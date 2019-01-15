0bb0ff55ffd356b0a5cd1209fd884491995d3625
Dan Adler <dadlerj@users.noreply.github.com>
2019-01-15 14:02:52 -0800

    Update advisors (#73)

--- website/src/pages/about.tsx
+++ website/src/pages/about.tsx
@@ -323,6 +323,14 @@ export default class About extends React.Component<any, any> {
                                     </div>
                                     <br />
                                     <div className="about__founders-advisors-item">
+                                        <strong>David Stewart</strong>, Business Advisor
+                                    </div>
+                                    <br />
+                                    <div className="about__founders-advisors-item">
+                                        <strong>Adam Frankl</strong>, Marketing Advisor
+                                    </div>
+                                    <br />
+                                    <div className="about__founders-advisors-item">
                                         <strong>Jens Pillgram-Larsen</strong>, Google
                                     </div>
                                     <br />
@@ -335,7 +343,15 @@ export default class About extends React.Component<any, any> {
                                     </div>
                                     <br />
                                     <div className="about__founders-advisors-item">
+                                        <strong>Mike Saparov</strong>, CoreOS
+                                    </div>
+                                    <br />
+                                    <div className="about__founders-advisors-item">
                                         <strong>Audrey Maclean</strong>, Entrepreneur
+                                    </div>
+                                    <br />
+                                    <div className="about__founders-advisors-item">
+                                        <strong>Joseph Malchow</strong>, HNVR
                                     </div>
                                     <br />
                                     <div className="about__founders-advisors-item">
@@ -343,7 +359,7 @@ export default class About extends React.Component<any, any> {
                                     </div>
                                     <br />
                                     <div className="about__founders-advisors-item">
-                                        <strong>John Vrionis</strong>, Lightspeed Venture Partners
+                                        <strong>John Vrionis</strong>, Unusual Ventures
                                     </div>
                                     <br />
                                     <div className="about__founders-advisors-item">
