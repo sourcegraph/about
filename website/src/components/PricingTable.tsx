80c7235a88ca952001d1e7bf0fd50dbddc579eaf
Christina Forney <forney.christina@gmail.com>
2019-08-02 13:46:44 -0700

    Remove reference to Slack and webhooks until we add back that feature

--- website/src/components/PricingTable.tsx
+++ website/src/components/PricingTable.tsx
@@ -131,7 +131,7 @@ const DATA: PricingItemCategory[] = [
         title: 'Code alerts & automation',
         items: [
             { name: 'Saved searches', plans: ENTERPRISE_PLAN },
-            { name: 'Email, Slack, and webhook notifications', plans: ENTERPRISE_PLAN },
+            { name: 'Email notifications', plans: ENTERPRISE_PLAN },
             {
                 name: 'Large-scale code modifications',
                 description: 'Only available to select customers in preview',
