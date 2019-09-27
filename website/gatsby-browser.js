62fc7bdeb443b0e8697374f49c38dcb0fb68b188
Ryan Blunden <ryan@sourcegraph.com>
2019-09-27 10:35:29 +1000

    Removed Intercom and Telligent (#278)
    * Removed Intercom
    * Remove Telligent
    * Updated last modified date on the privacy page

--- website/gatsby-browser.js
+++ website/gatsby-browser.js
@@ -1,20 +1,4 @@
 exports.onClientEntry = function() {
-  // Telligent
-  ;(function(t, r, a, c, k, e, d) {
-    if (!t[k]) {
-      t.GlobalTelligentNamespace = t.GlobalTelligentNamespace || []
-      t.GlobalTelligentNamespace.push(k)
-      t[k] = function() {
-        ;(t[k].q = t[k].q || []).push(arguments)
-      }
-      t[k].q = t[k].q || []
-      e = r.createElement(a)
-      d = r.getElementsByTagName(a)[0]
-      e.async = 1
-      e.src = c
-      d.parentNode.insertBefore(e, d)
-    }
-  })(window, document, 'script', 'https://storage.googleapis.com/telligent-artifacts/tracker/tel.js', 'telligent')
   // Google Analytics
   ;(function(i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r
@@ -32,38 +16,6 @@ exports.onClientEntry = function() {
 
   ga('create', 'UA-40540747-17', 'auto')
   ga('send', 'pageview')
-  // Intercom
-  window.intercomSettings = { app_id: 'wd8xxn47' }
-  ;(function() {
-    var w = window
-    var ic = w.Intercom
-    if (typeof ic === 'function') {
-      ic('reattach_activator')
-      ic('update', intercomSettings)
-    } else {
-      var d = document
-      var i = function() {
-        i.c(arguments)
-      }
-      i.q = []
-      i.c = function(args) {
-        i.q.push(args)
-      }
-      w.Intercom = i
-      function l() {
-        var s = d.createElement('script')
-        s.type = 'text/javascript'
-        s.async = true
-        s.src = 'https://widget.intercom.io/widget/wd8xxn47'
-        s.onload = () => {
-          initializeIntercom('SourcegraphWeb')
-        }
-        var x = d.getElementsByTagName('script')[0]
-        x.parentNode.insertBefore(s, x)
-      }
-      w.addEventListener('load', l, false)
-    }
-  })()
 }
 
 exports.onInitialClientRender = function() {
@@ -81,13 +33,11 @@ exports.onInitialClientRender = function() {
   setTimeout(function() {
     const appId = 'SourcegraphWeb'
     const env = window.location.origin === 'https://about.sourcegraph.com' ? 'production' : 'development'
-    initializeTelligent(appId, env, true)
     initializeGA()
   }, 0)
 }
 
 exports.onRouteUpdate = function({ location }) {
-  // HACK: Wait one second for onInitialClientRender to happen to ensure telligent tracker is available.
   // TODO: @attfarhan fix condition to ensure "tracker is configured"
 
   setTimeout(() => {
@@ -95,8 +45,4 @@ exports.onRouteUpdate = function({ location }) {
       window.twttr.widgets.load()
     }
   })
-
-  setTimeout(function() {
-    trackPageView()
-  }, 1000)
 }
