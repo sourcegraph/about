7b2c387bd4ec4f19d301d3da32d60019cece44f0
Ryan Blunden <ryan.blunden@gmail.com>
2019-10-21 14:53:07 -0700

    Add "Edit this post" button

--- website/src/pages/blog.tsx
+++ website/src/pages/blog.tsx
@@ -69,6 +69,7 @@ export const pageQuery = graphql`
                     fields {
                         slug
                     }
+                    fileAbsolutePath
                 }
             }
         }
