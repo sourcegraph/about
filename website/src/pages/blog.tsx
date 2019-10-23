cd94c61bbbb09fd1392c8de6dabb9cb39ba56741
Ryan Blunden <ryan.blunden@gmail.com>
2019-10-23 11:14:45 -0700

    Don't need `fileAbsolutePath` here

--- website/src/pages/blog.tsx
+++ website/src/pages/blog.tsx
@@ -69,7 +69,6 @@ export const pageQuery = graphql`
                     fields {
                         slug
                     }
-                    fileAbsolutePath
                 }
             }
         }
