3dfc43b68a6edfe77ea6552305ab96d36e57b682
Dan Adler <dadlerj@users.noreply.github.com>
2019-11-20 15:11:46 -0800

    fix: fix homepage js issues (#369)

--- website/src/components/TestimonialCarousel.tsx
+++ website/src/components/TestimonialCarousel.tsx
@@ -45,7 +45,7 @@ export const TestimonialCarousel: React.FunctionComponent<Props> = ({
                     <img src="/case-studies/quote.svg" className="testimonial-carousel__quote-icon mb-5" alt="" />
                     <Carousel controls={false}>
                         {testimonials.map(({ customer, logo, quote, author, cta }, i) => (
-                            <div className={`${slugify(customer).toLowerCase()} testimonial-carousel__testimonial`}>
+                            <div key={`testimonial-${i}`} className={`${slugify(customer).toLowerCase()} testimonial-carousel__testimonial`}>
                                 <img src={logo} className="testimonial-carousel__logo d-inline-block mb-4" />
                                 <blockquote className="testimonial-carousel__blockquote blockquote">
                                     <p className="mb-5">{quote}</p>
