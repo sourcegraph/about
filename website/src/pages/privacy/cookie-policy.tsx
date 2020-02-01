797da0f3f5fea16b3eb3e32e752a98d526b0cadb
aileenrose <aileenrose@users.noreply.github.com>
2020-01-31 17:07:19 -0800

    Move cookie below privacy

--- website/src/pages/privacy/cookie-policy.tsx
+++ website/src/pages/privacy/cookie-policy.tsx
@@ -1,7 +1,7 @@
 import ArrowRightIcon from 'mdi-react/ArrowRightIcon'
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
-import Layout from '../components/Layout'
+import Layout from '../../components/Layout'
 export default class CookiesPage extends React.Component<any, any> {
     constructor(props: any) {
         super(props)
@@ -23,7 +23,7 @@ export default class CookiesPage extends React.Component<any, any> {
             <Layout location={this.props.location}
                 meta={{
                     title:
-                        'Sourcegraph cookie policy',
+                        'Sourcegraph Cookie Policy',
                     description:
                         'About.sourcegraph.com uses cookies.'
                  }}
@@ -31,7 +31,7 @@ export default class CookiesPage extends React.Component<any, any> {
                 <div className="bg-white text-dark">
                     <section>
                         <div class="content-page__title">
-                            <h1>Sourcegraph cookie policy</h1>
+                            <h1>Sourcegraph Cookie Policy</h1>
                         </div>
                     </section>
                     <section>
