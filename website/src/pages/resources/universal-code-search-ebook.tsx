3b86e47bae820184a926a31eaf6a4c0089db4a09
Ryan Blunden <ryan@sourcegraph.com>
2020-03-03 08:19:24 +1000

    Move h1 to page header, replace automation with code change management (#618)

--- website/src/pages/resources/universal-code-search-ebook.tsx
+++ website/src/pages/resources/universal-code-search-ebook.tsx
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from 'react'
 import Layout from '../../components/Layout'
 import { eventLogger } from '../../EventLogger'
+import { ContentPage } from '../../components/content/ContentPage'
 
 interface HubSpotForm {
     portalId: string
@@ -44,43 +45,41 @@ export default ((props: any) => {
                 image: 'https://info.sourcegraph.com/hubfs/sourcegraph-universal-code-search-ebook-social.png'
             }}
         >
-            <script charSet="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js" />
-            <div className="landing-page bg-white text-dark">
-                <div className="container-lg py-6 px-5">
-                    <div className="row flex-wrap-reverse">
-                        <div className="col-md-12"> 
-                            <h1>Free ebook: Universal code search and intelligence</h1>
-                            <h3>How to increase programming productivity and improve code quality</h3>
-                            <p>&nbsp;</p>
-                        </div>
-                    </div>              
-                    <div className="row flex-wrap-reverse">
-                        <div className="col-md-4">
-                            <p>By Quinn Slack and Beyang Liu</p>
-                            <p>Highly productive engineering organizations make searching across massive codebases, comprehending unfamiliar code, and sharing institutional first-order concerns. To achieve this, engineering teams require a
-                            tool that provides universal code search.</p>
-                            
-                            <p>With this ebook, you will learn:</p>
-                            <ul>
-                                <li>What is universal code search</li>
-                                <li>Top reasons developers rely on Sourcegraph</li>
-                                <li>How to improve code reviews with code navigation</li>
-                                <li>Why engineering leaders need code search and intelligence for their teams</li>
-                            </ul>
-                        </div>
-                        <div className="col-md-4">
-                            <img src="/external-logos/sourcegraph-universal-code-search-ebook-v1.jpg" alt="Download: Universal Code Search and Intelligence" className="landing-page__img" />
-                        </div>                 
-                        <div className="col-md-4">
-                            <h3 className="font-weight-light text-sans-serif">Get your free ebook.</h3>
-                            <div className="form mt-5">
-                                <div id="hubspotContactForm" className="d-flex justify-center" />
-                            </div>
-                        </div>
+            <ContentPage
+                title="Universal Code Search"
+                description="Ship better software faster with Sourcegraph Universal Code Search with enhanced code search, review, and code change management."
+            >
+                <script charSet="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js" />
+                <div className="landing-page bg-white text-dark">
+                    <div className="container-lg py-6 px-5">
+                        <div className="row flex-wrap-reverse">
+                            <div className="col-md-4">
+                                <p>By Quinn Slack and Beyang Liu</p>
+                                <p>Highly productive engineering organizations make searching across massive codebases, comprehending unfamiliar code, and sharing institutional first-order concerns. To achieve this, engineering teams require a
+                                tool that provides universal code search.</p>
 
+                                <p>With this ebook, you will learn:</p>
+                                <ul>
+                                    <li>What is universal code search</li>
+                                    <li>Top reasons developers rely on Sourcegraph</li>
+                                    <li>How to improve code reviews with code navigation</li>
+                                    <li>Why engineering leaders need code search and intelligence for their teams</li>
+                                </ul>
+                            </div>
+                            <div className="col-md-4">
+                                <img src="/external-logos/sourcegraph-universal-code-search-ebook-v1.jpg" alt="Download: Universal Code Search and Intelligence" className="landing-page__img" />
+                            </div>
+                            <div className="col-md-4">
+                                <h3 className="font-weight-light text-sans-serif">Get your free ebook.</h3>
+                                <div className="form mt-5">
+                                    <div id="hubspotContactForm" className="d-flex justify-center" />
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>
+            </ContentPage>
         </Layout>
     )
 }) as React.FunctionComponent<any>
