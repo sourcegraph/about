56a5a66d91cb5313074328f1e35858248eab80e2
aileenrose <aileenrose@users.noreply.github.com>
2020-03-03 03:59:54 -0800

    Update page slug

--- website/src/components/ProductPopover.tsx
+++ website/src/components/ProductPopover.tsx
@@ -9,7 +9,7 @@ interface ProductFeatureOrUseCase {
 }
 
 export const PRODUCT_FEATURES: ProductFeatureOrUseCase[] = [
-    { text: 'Code discovery', to: '/product/code-search-navigation' },
+    { text: 'Code discovery', to: '/product/code-discovery' },
     { text: 'Code intelligence', to: '/product/code-intelligence' },    
     { text: 'Code change management', to: '/product/code-change-management' },
     { text: 'Code review', to: '/product/code-review' },
