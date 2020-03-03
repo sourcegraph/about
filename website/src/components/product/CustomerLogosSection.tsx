26dfabbbe66fda17e00d23afd31eb19d55b9b83a
aileenrose <aileenrose@users.noreply.github.com>
2020-03-02 22:40:53 -0800

    Add logos and remove test image on home

--- website/src/components/product/CustomerLogosSection.tsx
+++ website/src/components/product/CustomerLogosSection.tsx
@@ -7,12 +7,20 @@ const ITEMS: (
       }
     | { topN: number; description: string; className: string })[] = [
     {
+        name: 'Cloudflare',
+        url: '/external-logos/cloudflare-color-logo.svg',
+    },
+    {
         name: 'Uber',
         url: '/external-logos/uber.svg',
     },
     {
         name: 'Lyft',
         url: '/external-logos/lyft-logo.svg',
+    },
+    {
+        name: 'Qualtrics',
+        url: '/external-logos/qualtrics-logo.svg',
     },
     {
         name: 'Convoy',
@@ -28,12 +36,40 @@ const ITEMS: (
         className: 'customer-logos-section__item-logo-synthesized-0',
     },
     {
+        name: 'Adidas Running',
+        url: '/external-logos/adidas-running-logo.svg',
+    },
+    {
         name: 'Quantcast',
         url: '/external-logos/quantcast-logo.svg',
     },
     {
+        name: 'SoFi',
+        url: '/external-logos/sofi-logo.svg',
+    },
+    {
+        name: 'Collective Health',
+        url: '/external-logos/collective-health-logo.svg',
+    },
+    {
+        name: 'Apex Clearning',
+        url: '/external-logos/apex-clearing-logo.png',
+    },
+    {
         name: 'Plaid',
         url: '/external-logos/plaid-logo.svg',
+    },
+    {
+        name: 'Expanse',
+        url: '/external-logos/expanse-logo.svg',
+    },
+    {
+        name: 'Nubank',
+        url: '/external-logos/nubank-logo.svg',
+    },
+    {
+        name: 'Thought Machine',
+        url: '/external-logos/thought-machine-logo.svg',
     },
     {
         name: 'GetYourGuide',
@@ -51,13 +87,13 @@ export const CustomerLogosSection: React.FunctionComponent<{ trustWhat?: string;
     className = '',
 }) => (
     <div className={`customer-logos-section ${className}`}>
-        <h3 className="text-center font-weight-light">Every developer at these companies uses Sourcegraph Universal Code Search</h3>
+        <h3 className="text-center font-weight-light">Engineering teams at these companies use Sourcegraph Universal Code Search</h3>
         <div className="container text-center mt-4 d-flex flex-wrap justify-content-center align-items-center line-height-normal">
             {ITEMS.map((logo, i) =>
                 'name' in logo ? (
                     <div
                         key={i}
-                        className={`${logo.name.replace(' ', '-').toLowerCase()} customer-logos-section__item mx-2`}
+                        className={`${logo.name.replace(' ', '-').toLowerCase()} customer-logos-section__item mx-4`}
                     >
                         <img className="customer-logos-section__item-logo d-block mx-auto" src={logo.url} />
                     </div>
