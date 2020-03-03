4c78c6201d31f2756f0b3e9470f3ca3ff8d41cf3
adam <41576481+adaaaam@users.noreply.github.com>
2020-03-02 17:34:59 -0800

    Update universal-code-search.tsx

--- website/src/pages/universal-code-search.tsx
+++ website/src/pages/universal-code-search.tsx
@@ -8,6 +8,7 @@ import Layout from '../components/Layout'
 import { CustomerLogosSection } from '../components/product/CustomerLogosSection'
 import { GetSourcegraphNowActions } from '../css/components/actions/GetSourcegraphNowActions'
 import Helmet from 'react-helmet'
+import 'components/product/IntegratesWithSection'
 
 export default ((props: any) => (
     <Layout
