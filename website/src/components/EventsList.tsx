6ebaf2eb0f271cfc204dad0c322a4f5384e0e9c1
aileenrose <aileenrose@users.noreply.github.com>
2020-03-05 20:22:35 -0800

    Add Webinars

--- website/src/components/EventsList.tsx
+++ website/src/components/EventsList.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import EventData from "../data/events-listing"
-import NewsData from "../data/news-listing"
 
 class Conferences extends React.Component {
 	render() {		
@@ -9,9 +8,10 @@ class Conferences extends React.Component {
 				
 				{EventData.Conference.map((eventDetail, index)=> {
 					return ( 					
-						<div class="card col-sm-3">
-							<img class="card-img-top img-fluid" src={eventDetail.eventImage} />
-							<div class="card-body">
+						<div className="card col-sm-3 event__item">
+							<img className="card-img-top img-fluid" src={eventDetail.eventImage} />
+							<div className="card-body">
+								<h6>CONFERENCE</h6>
 								<h5>{eventDetail.eventTitle}</h5>
 								<p>{eventDetail.eventLocation}<br />
 								{eventDetail.eventDate}<br />
@@ -20,12 +20,15 @@ class Conferences extends React.Component {
 						</div>
 					)
 				})}
-				{NewsData.y2020.map((newsDetail, index)=> {
+				{EventData.Webinar.map((eventDetail, index)=> {
 					return ( 					
-						<div class="card col-sm-3">
-							<div class="card-body">
-								<h5>{newsDetail.newsTitle}</h5>
-								<p><a href={newsDetail.newsLink} ref="nofollow">Join us &rarr;</a></p>
+						<div className="card col-sm-3 event__item">
+							<img className="card-img-top img-fluid" src={eventDetail.eventImage} />
+							<div className="card-body">
+								<h6>WEBINAR</h6>
+								<h5>{eventDetail.eventTitle}</h5>
+								<p>{eventDetail.eventDate}<br />
+								<a href={eventDetail.eventLink} ref="nofollow">Sign up &rarr;</a></p>
 							</div>
 						</div>
 					)
