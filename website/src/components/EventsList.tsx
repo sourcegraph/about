01a6bab096ab2e69a7f15a0e8ebd5c774200e29a
aileenrose <aileenrose@users.noreply.github.com>
2020-03-09 17:22:46 -0700

    Fix formatting errors, prettier stuff

--- website/src/components/EventsList.tsx
+++ website/src/components/EventsList.tsx
@@ -2,41 +2,46 @@ import * as React from 'react'
 import EventData from '../data/events-listing'
 
 class Conferences extends React.Component {
-	render() {
-		return (
-			<div className="card-deck">
-				{EventData.Conference.map((eventDetail, index)=> {
-					return ( 					
-						<div className="card col-sm-3 event__item">
-							<img className="card-img-top img-fluid" src={eventDetail.eventImage} />
-							<div className="card-body">
-								<h6>CONFERENCE</h6>
-								<h5>{eventDetail.eventTitle}</h5>
-								<p>
-									{eventDetail.eventLocation}<br />
-									{eventDetail.eventDate}<br />
-									<a href={eventDetail.eventLink} ref="nofollow">Join us &rarr;</a>
-								</p>
-							</div>
-						</div>
-					)
-				})}
-				{EventData.Webinar.map((eventDetail, index)=> {
-					return ( 					
-						<div className="card col-sm-3 event__item">
-							<img className="card-img-top img-fluid" src={eventDetail.eventImage} />
-							<div className="card-body">
-								<h6>WEBINAR</h6>
-								<h5>{eventDetail.eventTitle}</h5>
-								<p>{eventDetail.eventDate}<br />
-								<a href={eventDetail.eventLink} ref="nofollow">Sign up &rarr;</a></p>
-							</div>
-						</div>
-					)
-				})}
-			</div>
-		)
-	}
+    public render(): JSX.Element {
+        return (
+            <div className="card-deck">
+                {EventData.Conference.map((eventDetail, index) => (
+                    <div className="card col-sm-3 event__item">
+                        <img className="card-img-top img-fluid" src={eventDetail.eventImage} />
+                        <div className="card-body">
+                            <h6>CONFERENCE</h6>
+                            <h5>{eventDetail.eventTitle}</h5>
+                            <p>
+                                {eventDetail.eventLocation}
+                                <br />
+                                {eventDetail.eventDate}
+                                <br />
+                                <a href={eventDetail.eventLink} rel="nofollow">
+                                    Join us &rarr;
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+                {EventData.Webinar.map((eventDetail, index) => (
+                    <div className="card col-sm-3 event__item">
+                        <img className="card-img-top img-fluid" src={eventDetail.eventImage} />
+                        <div className="card-body">
+                            <h6>WEBINAR</h6>
+                            <h5>{eventDetail.eventTitle}</h5>
+                            <p>
+                                {eventDetail.eventDate}
+                                <br />
+                                <a href={eventDetail.eventLink} rel="nofollow">
+                                    Sign up &rarr;
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
 
-export default Conferences;
+export default Conferences
--- website/src/pages/news.tsx
+++ website/src/pages/news.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
+import Helmet from 'react-helmet'
 import { ContentSection } from '../components/content/ContentSection'
-import News from '../components/NewsList'
 import Layout from '../components/Layout'
+import News from '../components/NewsList'
 
-export default class NewsPage extends React.Component < any, any > {
+// tslint:disable-next-line: no-any
+export default class NewsPage extends React.Component<any, any> {
+    // tslint:disable-next-line: no-any
     constructor(props: any) {
         super(props)
         this.state = {
@@ -18,69 +21,82 @@ export default class NewsPage extends React.Component < any, any > {
     }
 
     public render(): JSX.Element | null {
-        const desc =
-            'Sourcegraph in the news.'
+        const desc = 'The latest Sourcegraph news and press releases.'
         return (
-            <Layout location={this.props.location}
-                meta={{
-                    title:
-                    'Sourcegraph news and press releases',
-                    description:
-                    'Sourcegraph in the news.'
-                }}
-            >
-            <div className="news bg-white text-dark">
-                <section>
-                    <div>
-                        <ContentSection color="primary" className="hero-section text-center py-5">
-                            <h1>Sourcegraph in the news</h1>
-                            <p className="news__head-description">
-                                The latest news and press releases.
-                            </p>
-                        </ContentSection>
-                    </div>
-                </section>
-                <section>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col mt-5 mb-5 ">
-                                <h3>Press releases</h3>
-                                <ul>
-                                    <li>
-                                        <a href="/blog/press-release-sourcegraph-secures-series-b/?ref=news" >Sourcegraph Secures $23 Million Series B Round for Universal Code Search</a>
-                                        <span className="news__date">March 3, 2020</span>
-                                    </li>
-                                    <li>
-                                        <a href="/blog/press-release-sourcegraph-announces-new-gitlab-native-integration/?ref=news" >New GitLab Native Integration, Universal Code Search Engine, and Amazing Company Momentum</a>
-                                        <span className="news__date">November 12, 2019</span>
-                                    </li>
-                                </ul>
+            <Layout location={this.props.location}>
+                >
+                <Helmet>
+                    <title>Sourcegraph - News</title>
+                    <meta name="twitter:title" content="Sourcegraph in the news" />
+                    <meta property="og:title" content="Sourcegraph in the news" />
+                    <meta name="twitter:site" content="@srcgraph" />
+                    <meta name="twitter:image" content="https://about.sourcegraph.com/favicon.png" />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:description" content={desc} />
+                    <meta property="og:description" content={desc} />
+                    <meta name="description" content={desc} />
+                </Helmet>
+                <div className="news bg-white text-dark">
+                    <section>
+                        <div>
+                            <ContentSection color="primary" className="hero-section text-center py-5">
+                                <h1>Sourcegraph in the news</h1>
+                                <p className="news__head-description">The latest news and press releases.</p>
+                            </ContentSection>
+                        </div>
+                    </section>
+                    <section>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col mt-5 mb-5 ">
+                                    <h3>Press releases</h3>
+                                    <ul>
+                                        <li>
+                                            <a href="/blog/press-release-sourcegraph-secures-series-b/?ref=news">
+                                                Sourcegraph Secures $23 Million Series B Round for Universal Code Search
+                                            </a>
+                                            <span className="news__date">March 3, 2020</span>
+                                        </li>
+                                        <li>
+                                            <a href="/blog/press-release-sourcegraph-announces-new-gitlab-native-integration/?ref=news">
+                                                New GitLab Native Integration, Universal Code Search Engine, and Amazing
+                                                Company Momentum
+                                            </a>
+                                            <span className="news__date">November 12, 2019</span>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="container">
-                        <div className="row justify-content-start">
-                            <div className="col-sm-10 col-lg-10">
-                                <h3>News</h3>
-                                <News></News>
+                        <div className="container">
+                            <div className="row justify-content-start">
+                                <div className="col-sm-10 col-lg-10">
+                                    <h3>News</h3>
+                                    <News></News>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="container">
-                        <div className="row justify-content-md-center">
-                            <div className="col mt-5">
-                                <h3>Media contact</h3>
-                                <p>Tanya Carlsson<br />
-                                Offleash PR for Sourcegraph<br />
-                                <a href="mailto:tanya@offleashpr.com">tanya@offleashpr.com</a><br />
-                                <a href="tel:+17075296139">+1 707-529-6139</a><br />
-                                &nbsp;</p>
+                        <div className="container">
+                            <div className="row justify-content-md-center">
+                                <div className="col mt-5">
+                                    <h3>Media contact</h3>
+                                    <p>
+                                        Tanya Carlsson
+                                        <br />
+                                        Offleash PR for Sourcegraph
+                                        <br />
+                                        <a href="mailto:tanya@offleashpr.com">tanya@offleashpr.com</a>
+                                        <br />
+                                        <a href="tel:+17075296139">+1 707-529-6139</a>
+                                        <br />
+                                        &nbsp;
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-            </div>
+                    </section>
+                </div>
             </Layout>
         )
     }
