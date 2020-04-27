4a4f3449b068158a1ae6aaa8218d03b3dd9f2123
aileenrose <aileenrose@users.noreply.github.com>
2020-04-27 15:47:32 -0700

    Blue button wins AB test

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -109,7 +109,7 @@ export default class Header extends React.Component<HeaderProps, any> {
                                         </li>
                                         <li className="header__nav-item nav-item" role="presentation">
                                             <a
-                                                className="header__nav-link nav-link"
+                                                className="header__nav-link nav-link btn btn-primary mb-2"
                                                 href="https://sourcegraph.com/sign-up"
                                                 title="For public code only on Sourcegraph.com"
                                             >
