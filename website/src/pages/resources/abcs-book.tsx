0ac5e5830d3a9fccec5d4ef0e3c77e955765f438
aileenrose <aileenrose@users.noreply.github.com>
2020-05-28 15:13:59 -0700

    Update website/src/pages/resources/abcs-book.tsx
    Co-authored-by: adam <41576481+adaaaam@users.noreply.github.com>

--- website/src/pages/resources/abcs-book.tsx
+++ website/src/pages/resources/abcs-book.tsx
@@ -74,7 +74,7 @@ export default ((props: any) => (
                     </p>
                     <p>
                         We hope you enjoy the book and would love to get your feedback{' '}
-                        <a href="https://twitter.com/intent/tweet?text=For%20all%20children%20at%20home%20that%20wonder%20what%20their%20techie%20parents%20do%20all%20day%2C%20night%2C%20and%20some%20weekends%2C%20too%2C%20they%20need%20the%20%22Our%20ABCs%3A%20Always%20Be%20Coding%22%20book%20by%20@srcgraph%20-%20https%3A//about.sourcegraph.com/abc%20%23abcsbook%20%23TYCTWD%20%23TODASTW%20%23BringYourKidsToWorkDay">
+                        <a href="https://twitter.com/intent/tweet?text=For%20all%20children%20at%20home%20that%20wonder%20what%20their%20techie%20parents%20do%20all%20day%2C%20night%2C%20and%20some%20weekends%2C%20too%2C%20they%20need%20the%20%22Our%20ABCs%3A%20Always%20Be%20Coding%22%20book%20by%20@srcgraph%20-%20https%3A//about.sourcegraph.com/abc%20%23ABCsbook">
                             via Twitter
                         </a>{' '}
                         or <a href="mailto:hi+abc@sourcegraph.com">email</a>.
