88c71beff9247755715facfd21acb5967bf4c123
aileenrose <aileenrose@users.noreply.github.com>
2020-06-16 16:40:25 -0700

    - Clean up featured blog posts - Add AppLovin logo to customers - Prettier home page code

--- website/src/components/FeaturedBlogPosts.tsx
+++ website/src/components/FeaturedBlogPosts.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 export interface BlogPost {
     title: string
-    description: string
     thumbnail: string
     url: string
 }
@@ -14,7 +13,7 @@ interface Props {
 export const FeaturedBlogPosts: React.FunctionComponent<Props> = ({ posts }) => (
     <div className="container">
         <div className="row text-dark justify-content-center">
-            {posts.map(({ title, description, thumbnail, url, active }, i) => (
+            {posts.map(({ title, thumbnail, url }, i) => (
                 <div className="blog-posts-home__post col-md-4 px-2">
                     <div className="card">
                         <div className="text-center">
@@ -24,7 +23,6 @@ export const FeaturedBlogPosts: React.FunctionComponent<Props> = ({ posts }) => 
                         </div>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{description}</p>
                             <a href={url} className="btn btn-primary">
                                 Read more
                             </a>
