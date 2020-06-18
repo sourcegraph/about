1c0b7b90c10ec2961ea90fe9d0a1cf934d32abf3
adam <41576481+adaaaam@users.noreply.github.com>
2020-06-18 13:51:09 -0700

    Update code-learning.tsx

--- website/src/pages/product/code-learning.tsx
+++ website/src/pages/product/code-learning.tsx
@@ -10,6 +10,7 @@ import FileSearchOutlineIcon from 'mdi-react/FileSearchOutlineIcon'
 import GlobeIcon from 'mdi-react/GlobeIcon'
 import PlaylistAddCheckIcon from 'mdi-react/PlaylistAddCheckIcon'
 import StopwatchIcon from 'mdi-react/StopwatchIcon'
+import HeadSnowflakeIcon from 'mdi-react/HeadSnowflakeIcon'
 import React from 'react'
 import { ContentPage } from '../../components/content/ContentPage'
 import { ContentSection } from '../../components/content/ContentSection'
@@ -44,7 +45,7 @@ export default ((props: any) => (
                 </h2>
                 <div className="row">
                     <div className="col-md-4 mb-4">
-                        <IconItem color="red" icon={HelpIcon} className="py-3">
+                        <IconItem color="red" icon={HeadSnowflakeIcon} className="py-3">
                             <p className="text-sans-serif">
                                 Without contextual code intelligence, it’s difficult to find answers to code questions – for both new hires and existing team members. 
                             </p>
