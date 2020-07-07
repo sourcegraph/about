dfb3e5da31ee896d99a55ab69fe556ae8ff98702
aileenrose <aileenrose@users.noreply.github.com>
2020-07-07 10:57:47 -0700

    Update Handbook logo Update blog post header to fit style

--- website/src/pages/blog.tsx
+++ website/src/pages/blog.tsx
@@ -44,8 +44,8 @@ export default class BlogList extends React.Component<any, any> {
                             <BlogHeadLinks />
                         </div>
                     }
-                    className="bg-gradient-primary"
-                    titleClassName="display-2 font-weight-bold"
+                    className="text-dark"
+                    titleClassName="display-2 text-dark font-weight-bold"
                 >
                     <ContentSection color="white">
                         <div className="pt-4">
