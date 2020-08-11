6743b3b9c7d85938a9a8d1ffb299dca8436c097e
Adam Harvey <aharvey@sourcegraph.com>
2020-08-11 09:31:00 -0700

    Improve alt text.

--- website/src/components/BlogPosts.tsx
+++ website/src/components/BlogPosts.tsx
@@ -40,7 +40,7 @@ export default class BlogPosts extends React.Component<any, any> {
                                         <img
                                             className="blog-posts__post__image"
                                             src={post.node.frontmatter.heroImage}
-                                            aria-hidden="true"
+                                            alt={post.node.frontmatter.title}
                                         />
                                     </Link>
                                 </div>
