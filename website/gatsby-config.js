f5e744fbef259746ccbcdb9bd0bcb4c689706d43
Quinn Keast <qkeast@sourcegraph.com>
2020-09-15 19:48:20 +0200

    Tune up typography on blog posts

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-autolink-headers`,
           `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-unwrap-images`,
           {
             resolve: `gatsby-remark-images`,
             options: {
