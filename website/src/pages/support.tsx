3bae1996f1736897b2aa275c587dfd56d70e59d2
aileenrose <aileenrose@users.noreply.github.com>
2020-10-13 13:56:46 -0700

    Update to working hours

--- website/src/pages/support.tsx
+++ website/src/pages/support.tsx
@@ -40,7 +40,7 @@ export default ((props: any) => (
                                 <ol className="list-group support-plan-feature list-group-flush py-3">
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> Community support on our <Link to="https://github.com/sourcegraph/sourcegraph/issues">public issue tracker</Link></li>
                                     <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />Email support</li>
-                                    <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />Support during working hours (Pacific Time)</li>
+                                    <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />Support during working hours (all North American timezones)</li>
                                     <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />48 hour reponse time* to critical issues</li>
                                     <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />99.5% uptime (managed instances only)</li>
                                     <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />Initial implementation support on architecture, deployment, configuration, and rollout</li>
@@ -58,7 +58,7 @@ export default ((props: any) => (
                                 <ol className="list-group support-plan-feature list-group-flush py-3">
                                     <li className="list-group-item bg-transparent border-0"><CheckIcon className={checkClassName} /> Community support on our <Link to="https://github.com/sourcegraph/sourcegraph/issues">public issue tracker</Link></li>
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> Email support</li>
-                                    <li className={liClassName}><CheckIcon className={checkClassName} /> Support during working hours (Pacific Time)</li>
+                                    <li className={liClassName}><CheckIcon className={checkClassName} /> Support during working hours (all North American timezones)</li>
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> 48 hour reponse time* to critical issues</li>
                                     <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />99.5% uptime (managed instances only)</li>
                                     <li className={`${liClassName} pricing-plan-feature__value-false`}><CheckIcon className={nocheckClassName} />Initial implementation support on architecture, deployment, configuration, and rollout</li>
@@ -76,7 +76,7 @@ export default ((props: any) => (
                                 <ol className="list-group support-plan-feature list-group-flush py-3">
                                     <li className="list-group-item bg-transparent border-0"><CheckIcon className={checkClassName} /> Community support on our <Link to="https://github.com/sourcegraph/sourcegraph/issues">public issue tracker</Link></li>
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> Email support</li>
-                                    <li className={liClassName}><CheckIcon className={checkClassName} /> Support during working hours (Pacific Time)</li>
+                                    <li className={liClassName}><CheckIcon className={checkClassName} /> Support during working hours (all North American timezones)</li>
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> 24 hour reponse time* to critical issues</li>
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> 99.5% uptime (managed instances only)</li>
                                     <li className={liClassName}><CheckIcon className={checkClassName} /> Initial implementation support on architecture, deployment, configuration, and rollout</li>
