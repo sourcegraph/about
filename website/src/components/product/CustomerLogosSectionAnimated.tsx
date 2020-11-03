ebf225760463798ca74cd173c79a5608765e082c
aileenrose <aileenrose@users.noreply.github.com>
2020-11-02 17:42:39 -0800

    Add logos

--- website/src/components/product/CustomerLogosSectionAnimated.tsx
+++ website/src/components/product/CustomerLogosSectionAnimated.tsx
@@ -10,142 +10,150 @@ const ITEMS: {
         rel?: string
     }
 }[] = [
-    {
-        name: 'Collective Health',
-        url: '/external-logos/collective-health-logo.svg',
-    },
-    {
-        name: 'F5',
-        url: '/external-logos/f5-logo.svg',
-    },
-    {
-        name: 'Uber',
-        url: '/external-logos/uber.svg',
-    },
-    {
-        name: 'Lyft',
-        url: '/external-logos/lyft-logo.svg',
-        // link: {
-        //     url: '/case-studies/lyft-monolith-to-microservices',
-        // },
-    },
-    {
-        name: 'Amazon',
-        url: '/external-logos/amazon-logo.svg',
-    },
-    {
-        name: 'Qualtrics',
-        url: '/external-logos/qualtrics-logo.svg',
-    },
-    {
-        name: 'Toast',
-        url: '/external-logos/toast-logo.svg',
-    },
-    {
-        name: 'SoFi',
-        url: '/external-logos/sofi-logo.svg',
-        // link: {
-        //     url: '/case-studies/sofi-moves-fast-on-hundreds-of-microservices',
-        // },
-    },
-    {
-        name: 'Yelp',
-        url: '/external-logos/yelp.svg',
-        // link: {
-        //     url: 'https://engineeringblog.yelp.com/2019/11/winning-the-hackathon-with-sourcegraph.html',
-        //     target: '_blank',
-        //     rel: 'nofollow',
-        // },
-    },
-    {
-        name: 'Prezi',
-        url: '/external-logos/prezi-logo.svg',
-    },
-    {
-        name: 'Thought Machine',
-        url: '/external-logos/thought-machine-logo.svg',
-    },
-    {
-        name: 'Adidas Running',
-        url: '/external-logos/adidas-runtastic-sq-logo.svg',
-    },
-    {
-        name: 'Nutanix',
-        url: '/external-logos/nutanix-logo.svg',
-    },
-    {
-        name: 'Quantcast',
-        url: '/external-logos/quantcast-logo.svg',
-        // link: {
-        //     url: '/case-studies/quantcast-large-scale-refactoring',
-        // }
-    },
-    {
-        name: 'Criteo',
-        url: '/external-logos/criteo-logo.svg',
-    },
-    {
-        name: 'Paypal',
-        url: '/external-logos/paypal-logo.svg',
-    },
-    {
-        name: 'Convoy',
-        url: '/external-logos/convoy-logo.svg',
-        // link: {
-        //     url: '/case-studies/convoy-improved-on-boarding',
-        // },
-    },
-    {
-        name: 'Outreach',
-        url: '/external-logos/outreach-logo.svg',
-    },
-    {
-        name: 'GetYourGuide',
-        url: '/external-logos/gyg.svg',
-    },
-    {
-        name: 'Expanse',
-        url: '/external-logos/expanse-logo.svg',
-    },
-    {
-        name: 'Cloudflare',
-        url: '/external-logos/cloudflare-color-logo.svg',
-    },
-    {
-        name: 'Button',
-        url: '/external-logos/use-button-logo.svg',
-    },
-    {
-        name: 'Apex Clearing',
-        url: '/external-logos/apex-clearing-logo.png',
-    },
-    {
-        name: 'AppLovin',
-        url: '/external-logos/applovin-logo.svg',
-    },
-    {
-        name: 'Thorn',
-        url: '/external-logos/thorn-logo.svg',
-        // link: {
-        //     url: '/case-studies/we-are-thorn',
-        // },
-    },
-    {
-        name: 'Plaid',
-        url: '/external-logos/plaid-logo.svg',
-    },
-    {
-        name: 'Indeed',
-        url: '/external-logos/indeed-logo.svg',
-    },
-]
+        {
+            name: 'Collective Health',
+            url: '/external-logos/collective-health-logo.svg',
+        },
+        {
+            name: 'F5',
+            url: '/external-logos/f5-logo.svg',
+        },
+        {
+            name: 'Uber',
+            url: '/external-logos/uber.svg',
+        },
+        {
+            name: 'Lyft',
+            url: '/external-logos/lyft-logo.svg',
+            // link: {
+            //     url: '/case-studies/lyft-monolith-to-microservices',
+            // },
+        },
+        {
+            name: 'Amazon',
+            url: '/external-logos/amazon-logo.svg',
+        },
+        {
+            name: 'Qualtrics',
+            url: '/external-logos/qualtrics-logo.svg',
+        },
+        {
+            name: 'Toast',
+            url: '/external-logos/toast-logo.svg',
+        },
+        {
+            name: 'SoFi',
+            url: '/external-logos/sofi-logo.svg',
+            // link: {
+            //     url: '/case-studies/sofi-moves-fast-on-hundreds-of-microservices',
+            // },
+        },
+        {
+            name: 'Yelp',
+            url: '/external-logos/yelp.svg',
+            // link: {
+            //     url: 'https://engineeringblog.yelp.com/2019/11/winning-the-hackathon-with-sourcegraph.html',
+            //     target: '_blank',
+            //     rel: 'nofollow',
+            // },
+        },
+        {
+            name: 'Prezi',
+            url: '/external-logos/prezi-logo.svg',
+        },
+        {
+            name: 'Thought Machine',
+            url: '/external-logos/thought-machine-logo.svg',
+        },
+        {
+            name: 'Adidas Running',
+            url: '/external-logos/adidas-runtastic-sq-logo.svg',
+        },
+        {
+            name: 'Nutanix',
+            url: '/external-logos/nutanix-logo.svg',
+        },
+        {
+            name: 'Quantcast',
+            url: '/external-logos/quantcast-logo.svg',
+            // link: {
+            //     url: '/case-studies/quantcast-large-scale-refactoring',
+            // }
+        },
+        {
+            name: 'Criteo',
+            url: '/external-logos/criteo-logo.svg',
+        },
+        {
+            name: 'Paypal',
+            url: '/external-logos/paypal-logo.svg',
+        },
+        {
+            name: 'Convoy',
+            url: '/external-logos/convoy-logo.svg',
+            // link: {
+            //     url: '/case-studies/convoy-improved-on-boarding',
+            // },
+        },
+        {
+            name: 'Outreach',
+            url: '/external-logos/outreach-logo.svg',
+        },
+        {
+            name: 'GetYourGuide',
+            url: '/external-logos/gyg.svg',
+        },
+        {
+            name: 'Expanse',
+            url: '/external-logos/expanse-logo.svg',
+        },
+        {
+            name: 'Cloudflare',
+            url: '/external-logos/cloudflare-color-logo.svg',
+        },
+        {
+            name: 'Button',
+            url: '/external-logos/use-button-logo.svg',
+        },
+        {
+            name: 'Apex Clearing',
+            url: '/external-logos/apex-clearing-logo.png',
+        },
+        {
+            name: 'AppLovin',
+            url: '/external-logos/applovin-logo.svg',
+        },
+        {
+            name: 'Thorn',
+            url: '/external-logos/thorn-logo.svg',
+            // link: {
+            //     url: '/case-studies/we-are-thorn',
+            // },
+        },
+        {
+            name: 'Plaid',
+            url: '/external-logos/plaid-logo.svg',
+        },
+        {
+            name: 'Indeed',
+            url: '/external-logos/indeed-logo.svg',
+        },
+        {
+            name: 'TripActions',
+            url: '/external-logos/tripactions-logo.svg',
+        },
+        {
+            name: 'Handy',
+            url: '/external-logos/handy-logo-01.svg',
+        },
+    ]
 
 interface Props {
     showButton: boolean,
     className: String
 }
 
-export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, className}) => {
+export const CustomerLogosSectionAnimated: React.FC<Props> = ({ showButton, className }) => {
 
     const [buttonClass, setButtonClass] = useState('');
     const [windowWidth, setWindowWidth] = useState(0);
@@ -157,7 +165,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
     const firstLogoContainerRef = useRef(null);
     const secondLogoContainerRefClone = useRef(null);
     const thirdLogoContainerRefClone = useRef(null);
-    const [{x, y, scale}, set] = useSpring(() => ({x: 0, y: 0, scale: 0 }));
+    const [{ x, y, scale }, set] = useSpring(() => ({ x: 0, y: 0, scale: 0 }));
     const minDeviceWidth = 991;
     const animationTimeInSeconds = 25;
     let extraSpace = 0;
@@ -177,7 +185,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
         let promises = ITEMS.map((image) => {
             return new Promise((resolve, _) => {
                 const imageRef = new Image();
-                imageRef.onload = function() {
+                imageRef.onload = function () {
                     if (this.height && this.width) {
                         let calculatedWidth = (this.width / this.height) * 50;
                         if (calculatedWidth > 135) calculatedWidth = 135;
@@ -201,7 +209,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
             thirdLogoContainerRefClone.current = thirdLogoContainerClone;
             setReadyToScroll(true);
         });
-    },[]);
+    }, []);
 
     useEffect(() => {
         if (scrollAnimation) {
@@ -216,7 +224,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
             }, 500)
         };
     }, [readyToScroll])
-    
+
     useEffect(() => {
         if (firstLogoContainerRef.current && secondLogoContainerRefClone.current && thirdLogoContainerRefClone.current) {
             if (windowWidth > minDeviceWidth) {
@@ -251,7 +259,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
         logoContainerThree.style.transform = `translateX(${-totalWidth}px)`;
     };
 
-    function transitionEnd(e) { 
+    function transitionEnd(e) {
         if (e.target.id !== 'logo-container-reference') return;
         const logoContainerOne = firstLogoContainerRef.current!;
         const logoContainerTwo = secondLogoContainerRefClone.current!;
@@ -264,7 +272,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
         logoContainerThree.style.transform = `translateX(0px)`;
         setTimeout(() => {
             transitionStart();
-        },0);
+        }, 0);
     };
 
     function pauseAnimation() {
@@ -297,7 +305,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
 
         const pixelsPersecond = Math.abs(totalWidth) / animationTimeInSeconds;
         const newpositiontime = (Math.abs(newPosition) / pixelsPersecond).toString();
-        
+
         logoContainerOne.style.transition = `${newpositiontime}s linear`;
         logoContainerTwo.style.transition = `${newpositiontime}s linear`;
         logoContainerThree.style.transition = `${newpositiontime}s linear`;
@@ -310,20 +318,19 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
     function buttonFollowsMouse(e) {
         const containerRec = innerContainerRef.current?.getBoundingClientRect();
         if (
-            e.clientX - containerRec?.left! > 150 && 
-            e.clientY - containerRec?.top! > 60 && 
+            e.clientX - containerRec?.left! > 150 &&
+            e.clientY - containerRec?.top! > 60 &&
             (containerRec?.left! + innerContainerRef.current?.offsetWidth!) - e.clientX > 150 &&
             (containerRec?.top! + innerContainerRef.current?.offsetHeight!) - e.clientY > 60
-            )
-        {
+        ) {
             setButtonClass('');
-            set({scale: 1});
+            set({ scale: 1 });
         } else {
             setButtonClass('sourcegraph-cta-button-edge');
-            set({scale: 0.1});
+            set({ scale: 0.1 });
         };
         set({
-            x: (e.clientX - containerRec?.left!) + innerContainerRef.current?.scrollLeft!, 
+            x: (e.clientX - containerRec?.left!) + innerContainerRef.current?.scrollLeft!,
             y: e.clientY - containerRec?.top!
         });
     };
@@ -345,7 +352,7 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
     function handleMouseLeaveInnerArea() {
         if (readyToScroll) {
             pauseAnimation();
-            if (showButton) set({scale: 0});
+            if (showButton) set({ scale: 0 });
         };
     };
 
@@ -358,48 +365,48 @@ export const CustomerLogosSectionAnimated: React.FC<Props> = ({showButton, class
                 className="customer-container-outer"
                 ref={innerContainerRef}
             >
-            {(windowWidth > minDeviceWidth) && showButton &&
-                <a href="/customers">
-                    <animated.div className={"sourcegraph-cta-button " + buttonClass} style={{ left: x, top: y, transform: scale.interpolate(s => `scale(${s}) translate(-50%, -50%)`) }}>
-                        Learn how customers <br/> use Sourcegraph
+                {(windowWidth > minDeviceWidth) && showButton &&
+                    <a href="/customers">
+                        <animated.div className={"sourcegraph-cta-button " + buttonClass} style={{ left: x, top: y, transform: scale.interpolate(s => `scale(${s}) translate(-50%, -50%)`) }}>
+                            Learn how customers <br /> use Sourcegraph
                     </animated.div>
-                </a>
-            }
-            <div
-                id="logo-container-reference"
-                ref={firstLogoContainerRef}
-                style={{width: (imagesWidth / 3) + 30 + extraSpace}}
-                className="text-center mt-4 d-flex flex-wrap justify-content-between align-items-center line-height-normal customer-container-inner hidden">
-                {ITEMS.map((logo, i) => {
-                    return (
-                        <div
-                            key={i}
-                            className={`${logo.name.replace(' ', '-').toLowerCase()} customer-logos-section__item`}
-                        >
-                            {logo.link ? (
-                                <a href={logo.link.url} target={logo.link.target} rel={logo.link.rel}>
-                                    <img
-                                        className={"customer-logos-section__item-logo d-block mx-auto " + (showButton && (windowWidth > minDeviceWidth) ? 'customer-logos-section__item-logo-animation' : 'link-hover')}
-                                        src={logo.url}
-                                        alt={logo.name}
-                                    />
-                                </a>
-                            ) : (
-                                <img
-                                    className={"customer-logos-section__item-logo d-block mx-auto " + (showButton && (windowWidth > minDeviceWidth) ? 'customer-logos-section__item-logo-animation' : '')}
-                                    src={logo.url}
-                                    alt={logo.name}
-                                />
-                            )}
-                        </div>
-                    )
-                })}
-            </div>
+                    </a>
+                }
+                <div
+                    id="logo-container-reference"
+                    ref={firstLogoContainerRef}
+                    style={{ width: (imagesWidth / 3) + 30 + extraSpace }}
+                    className="text-center mt-4 d-flex flex-wrap justify-content-between align-items-center line-height-normal customer-container-inner hidden">
+                    {ITEMS.map((logo, i) => {
+                        return (
+                            <div
+                                key={i}
+                                className={`${logo.name.replace(' ', '-').toLowerCase()} customer-logos-section__item`}
+                            >
+                                {logo.link ? (
+                                    <a href={logo.link.url} target={logo.link.target} rel={logo.link.rel}>
+                                        <img
+                                            className={"customer-logos-section__item-logo d-block mx-auto " + (showButton && (windowWidth > minDeviceWidth) ? 'customer-logos-section__item-logo-animation' : 'link-hover')}
+                                            src={logo.url}
+                                            alt={logo.name}
+                                        />
+                                    </a>
+                                ) : (
+                                        <img
+                                            className={"customer-logos-section__item-logo d-block mx-auto " + (showButton && (windowWidth > minDeviceWidth) ? 'customer-logos-section__item-logo-animation' : '')}
+                                            src={logo.url}
+                                            alt={logo.name}
+                                        />
+                                    )}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             {(windowWidth < minDeviceWidth) && showButton &&
                 <div className={"sourcegraph-cta-bottom-container"}>
                     <a href="/customers" className={"sourcegraph-cta-link-bottom"}>
-                        <div className={"sourcegraph-cta-button-bottom"}>Learn how customers <br/> use Sourcegraph</div>
+                        <div className={"sourcegraph-cta-button-bottom"}>Learn how customers <br /> use Sourcegraph</div>
                     </a>
                 </div>
             }
