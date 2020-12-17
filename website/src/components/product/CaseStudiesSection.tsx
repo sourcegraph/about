9c7b4da86228bb54571e32e82466462c1da7c34e
aileenrose <aileenrose@users.noreply.github.com>
2020-12-17 08:18:06 -0800

    Add CERN case study

--- website/src/components/product/CaseStudiesSection.tsx
+++ website/src/components/product/CaseStudiesSection.tsx
@@ -10,6 +10,12 @@ const CASESTUDIES: {
     external?: boolean
 }[] = [
         {
+            name: 'CERN',
+            logo: '/external-logos/cern-badge-logo.jpg',
+            title: 'Criteo uses Sourcegraph universal code search to tackle Big Code',
+            url: '/case-studies/cern-reduces-technical-debt',
+        },
+        {
             name: 'Criteo',
             logo: '/external-logos/criteo-logo.svg',
             title: 'Criteo uses Sourcegraph universal code search to tackle Big Code',
