7cddd4e6463784a0c26c1c914ef7f6910e4d02c2
aileenrose <aileenrose@users.noreply.github.com>
2020-12-17 19:42:07 -0800

    Additional edits and fix logo

--- website/src/pages/case-studies/cern-reduces-technical-debt.tsx
+++ website/src/pages/case-studies/cern-reduces-technical-debt.tsx
@@ -35,7 +35,7 @@ export default ((props: any) => (
         >
             <ContentSection color="white" className="col-md-6 pb-5">
                 <div className="container">
-                    <p>Physicists at CERN use some of the world's most powerful particle accelerators to discover what the universe is made of and how it works. Over the last 18 years, the organization’s codebase for accelerator controls has grown to roughly 15 million lines of code. CERN operates within a self-contained software system that is developed and maintained by nearly 300 people, with all of its code in Sourcegraph. To achieve efficient development and safe upgrades, developers require a tool that lets them quickly and effectively search through their entire codebase so they can easily change and reuse code where necessary. </p>
+                    <p>Physicists at CERN use some of the world's most powerful particle accelerators to discover what the universe is made of and how it works. Over the last 18 years, the organization’s Java codebase for accelerator controls has grown to roughly 15 million lines of code. CERN operates within a self-contained software system that is developed and maintained by nearly 300 people, with all of its code in Sourcegraph. To achieve efficient development and safe upgrades, developers require a tool that lets them quickly and effectively search through their entire codebase so they can easily change and reuse code where necessary. </p>
                     <h2 className="pt-5 pb-1">Avoiding reinventing the wheel with universal code search</h2>
                     <p>Sourcegraph empowers developers at CERN to reuse code that already exists, avoiding duplication and saving developers countless hours redoing work that has already been done. It takes search one step further by &quot;understanding&quot; the structure of the code (as opposed to just &quot;seeing&quot; it as raw text), enabling the organization to do semantic searches that yield accurate results.</p>
                     <div className="row justify-content-md-center pt-3">
@@ -53,17 +53,17 @@ export default ((props: any) => (
                         author="Vito Baggiolini"
                     />
                     <div className="row pt-3">
-                        <div className="col-md-3 pt-3">
+                        <div className="col-md-3 pt-5">
                             <InContentImage
                                 src={chris}
                                 alt="Chris Roderick, Applications and Services Section Leader, CERN"
-                                caption="Chris Roderick, Applications and Services Section Leader, CERN"
                                 className="case-studies__img rounded-circle img-fluid mx-auto d-block mb-3"
                             />
                         </div>
                         <div className="col-md-9">
                             <InContentBlockquote
                                 quote="Sourcegraph helps us with technical debt reduction and the consolidation of our codebase by letting us avoid duplication, spot the usage of deprecated APIs or internal (non-API) library code, and identify general purpose code in specific projects (such as utility classes) that can be factored out and shared in a core library"
+                                author="Chris Roderick, Applications and Services Section Leader, CERN"
                             />
                         </div>
                     </div>
@@ -72,10 +72,10 @@ export default ((props: any) => (
                         quote="Sourcegraph lets us make informed decisions on how to evolve our codebase. For example, a library owner knows exactly how all other developers use their API, and can therefore make educated decisions on how to evolve it."
                         author="Chris Roderick"
                     />
-                    <p>The Large Hadron Collider (LHC) at CERN features five-year operational periods and the software must be stable during this time. Sourcegraph helps developers make small, backward-compatible changes and ensures that any change made by a given team to one part of the codebase doesn't break (or require adaptations of) dependent code written by other people. It’s essential that these changes are done correctly, as mistakes can be costly—if the LHC goes down, it costs up to $100K per hour.</p>
+                    <p>The Large Hadron Collider (LHC) at CERN features five-year operational periods and the software must be stable during this time. Sourcegraph helps developers make small, backward-compatible changes and ensures that any change made by a given team to one part of the codebase doesn't break (or require adaptations of) dependent code written by other people. It’s essential that these changes are done correctly, as mistakes can stop the operation of the CERN accelerators and waste precious time for physics research.</p>
                     <p>The LHC also features upgrade periods of 1-2 years, during which developers typically make more radical changes. During these periods, Sourcegraph lets users identify redundant code (e.g., utility methods) and assess the cost/benefit of doing breaking API changes.</p>
                     <InContentBlockquote
-                        quote="Our solution prior to Sourcegraph didn’t let us do searches on the method that belongs to a class. As a result, we were unable to identify whether the changes we were making would impact anything in a meaningful way. This put us in a position where we weren’t making necessary changes because we were unsure of the effect they would have."
+                        quote="Our solution prior to Sourcegraph didn’t let us do detailed and precise searches on how and where a method of a specific class is used. As a result, we were unable to measure the impact that a planned API change would have on the rest of our huge codebase. This put us in a position where we weren’t making necessary changes because we were unsure of the effect they would have."
                         author="Vito Baggiolini"
                     />
                     <p>Universal code search lets developers at CERN make changes to their code, including those with potentially costly implications if done incorrectly, with ease and confidence. As the organization continues to uncover fascinating insights into how our universe works, Sourcegraph serves as its go-to tool for code reuse and code change management. </p>
