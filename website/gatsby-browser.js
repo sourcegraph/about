9104f03cdc7d0f4582ea94b6da1cd7158e517b39
Farhan Attamimi <farhan@sourcegraph.com>
2021-02-02 14:51:57 +0800

    Read sourcegraphAnonymousUid cookie value into HubSpot forms #2443 (#2450)

--- website/gatsby-browser.js
+++ website/gatsby-browser.js
@@ -8,16 +8,22 @@ exports.onInitialClientRender = function () {
     }
   })
 
-  {
-    /* Required by HubSpot */
-  }
+/**
+ * HACK: jQuery is required for HubSpot embedded forms. We polyfill
+ * jQuery to access elements in jQuery forms directly, instead of
+ * working with jQuery collections.
+ * See https://www.unstack.com/blog/hubspot-on-form-submit-callbacks-without-jquery
+ * for more info.
+ *
+ */
   if (window) {
     window.jQuery =
-      window.jQuery ||
-      (() => ({
-        change: () => {},
-        trigger: () => {},
-      }))
+      window.jQuery || function(nodeOrSelector) {
+        if (typeof(nodeOrSelector) == 'string') {
+            return document.querySelector(s);
+        }
+        return nodeOrSelector;
+    };
   }
 }
 
