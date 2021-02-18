d96a0cdb99ca30419f7f338d3732de28a91aead4
aileenrose <aileenrose@users.noreply.github.com>
2021-02-18 12:14:48 -0800

    Add support for Google consent mode and Cookiebot's automatic blocking (#2308)
    * Add support for Google consent more and Cookiebot's automatic blocking
    
    * Remove extra closing brackets.
    
    * Set up only google tag manager
    
    * Make prettier
    
    * Edit directly to workaround prettier bug
    
    * Manually make prettier
    
    * Set Hubspot tracking code to marketing for Cookiebot
    
    * Additional configuration for Consent Mode
    - Remove Hubspot Tracking Code which will be managed in Google Tag Manager
    - Include snippet that enables Google Consent API integration

--- website/src/html.tsx
+++ website/src/html.tsx
@@ -32,15 +32,41 @@ export default class HTML extends React.Component<HtmlProps> {
                         rel="chrome-webstore-item"
                         href="https://chrome.google.com/webstore/detail/dgjhfomjieaadpoljlnidmbgkdffpack"
                     />
-                    <script
-                        type="text/javascript"
-                        id="hs-script-loader"
-                        async={true}
-                        defer={true}
-                        src="//js.hs-scripts.com/2762526.js"
+                    <meta name="google-site-verification" content="vRPkjcQnrXKgId0IyxVPHp0CGp3B7zaEFiTpyb8kPSQ" />
+                    <script data-cookieconsent="ignore" dangerouslySetInnerHTML={{
+                        __html: `window.dataLayer = window.dataLayer || [];
+                                function gtag() {
+                                    dataLayer.push(arguments);
+                                }
+                                gtag("consent", "default", {
+                                    ad_storage: "denied",
+                                    analytics_storage: "denied",
+                                wait_for_update: 500,
+                                });
+                                gtag("set", "ads_data_redaction", true);
+                    `,
+                    }}
                     />
+                    {/*  Google Tag Manager */}
+                    <script data-cookieconsent="ignore" dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-TB4NLS7');  
+                    `,
+                    }}
+                    />
+                    {/*  End Google Tag Manager */}
+                    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="fb31dc3e-afb3-4be8-ae84-7090bba7797d" data-blockingmode="auto" type="text/javascript"></script>
                 </head>
                 <body>
+                    {/*   Google Tag Manager (noscript) */}
+                    <style dangerouslySetInnerHTML={{ __html: `.gtm-hide { display:none;visibility:hidden" 0 !important}` }} />
+                    <noscript>
+                        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB4NLS7" className="gtm-hide" height="0" width="0"></iframe>
+                    </noscript>
+                    {/*  End Google Tag Manager (noscript) */}
                     <div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
                     {this.props.postBodyComponents}
                 </body>
