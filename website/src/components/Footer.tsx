2180b172715aa6f4fea07c2601db0a34bfeb773d
mgyorffy <1383381+mgyorffy@users.noreply.github.com>
2021-04-14 16:54:30 -1000

    add twitch icon to Footer (#2925)
    Co-authored-by: Mate Gyorffy <mgyorffy@oneteamtech.com>

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -3,6 +3,7 @@ import GithubIcon from 'mdi-react/GithubIcon'
 import LinkedinIcon from 'mdi-react/LinkedinIcon'
 import TwitterIcon from 'mdi-react/TwitterIcon'
 import YouTubeIcon from 'mdi-react/YoutubeIcon'
+import TwitchIcon from 'mdi-react/TwitchIcon'
 import * as React from 'react'
 
 export const Footer: React.FunctionComponent<{ minimal?: boolean }> = ({ minimal }) => (
@@ -125,6 +126,16 @@ export const Footer: React.FunctionComponent<{ minimal?: boolean }> = ({ minimal
                                         aria-label="YouTube"
                                     >
                                         <YouTubeIcon />
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a
+                                        href="https://www.twitch.tv/sourcegraph"
+                                        target="_blank"
+                                        rel="nofollow noopener"
+                                        aria-label="Twitch"
+                                    >
+                                        <TwitchIcon />
                                     </a>
                                 </li>
                             </ul>
