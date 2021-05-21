49235fce907547c16182f258e805e9ccb7875d6d
Mate Gyorffy <mgyorffy@oneteamtech.com>
2021-05-20 20:13:07 -0700

    add clearbit to the Header component of our website

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -19,6 +19,14 @@ export default class Header extends React.Component<HeaderProps, any> {
             isOpen: false,
         }
     }
+
+    componentDidMount() {
+        const script = document.createElement('script');
+        script.innerHTML = '!function(e){var o=document.getElementsByTagName("script")[0];if("object"==typeof e.ClearbitForHubspot)return console.log("Clearbit For HubSpot included more than once"),!1;e.ClearbitForHubspot={},e.ClearbitForHubspot.forms=[],e.ClearbitForHubspot.addForm=function(o){var t=o[0];"function"==typeof e.ClearbitForHubspot.onFormReady?e.ClearbitForHubspot.onFormReady(t):e.ClearbitForHubspot.forms.push(t)};var t=document.createElement("script");t.async=!0,t.src="https://hubspot.clearbit.com/v1/forms/pk_a66b9ed76e62c713c06aab39bfae7234/forms.js",o.parentNode.insertBefore(t,o),e.addEventListener("message",function(o){if("hsFormCallback"===o.data.type&&"onFormReady"===o.data.eventName)if(document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\').length>0)e.ClearbitForHubspot.addForm(document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\'));else if(document.querySelectorAll("iframe.hs-form-iframe").length>0){document.querySelectorAll("iframe.hs-form-iframe").forEach(function(t){t.contentWindow.document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\').length>0&&e.ClearbitForHubspot.addForm(t.contentWindow.document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\'))})}})}(window);';
+        script.async = true;
+        document.body.appendChild(script);
+    }
+
     public toggle(): void {
         this.setState({
             isOpen: !this.state.isOpen,
