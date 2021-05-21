8854843138d07ca7b5014f5fdf6fe783db08e896
Mate Gyorffy <mgyorffy@oneteamtech.com>
2021-05-20 20:41:59 -0700

    add scripts to the head instead of the body

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -24,12 +24,12 @@ export default class Header extends React.Component<HeaderProps, any> {
         const jquery = document.createElement('script');
         jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
         jquery.async = true;
-        document.body.appendChild(jquery);
+        document.head.appendChild(jquery);
 
         const script = document.createElement('script');
         script.innerHTML = '!function(e){var o=document.getElementsByTagName("script")[0];if("object"==typeof e.ClearbitForHubspot)return console.log("Clearbit For HubSpot included more than once"),!1;e.ClearbitForHubspot={},e.ClearbitForHubspot.forms=[],e.ClearbitForHubspot.addForm=function(o){var t=o[0];"function"==typeof e.ClearbitForHubspot.onFormReady?e.ClearbitForHubspot.onFormReady(t):e.ClearbitForHubspot.forms.push(t)};var t=document.createElement("script");t.async=!0,t.src="https://hubspot.clearbit.com/v1/forms/pk_a66b9ed76e62c713c06aab39bfae7234/forms.js",o.parentNode.insertBefore(t,o),e.addEventListener("message",function(o){if("hsFormCallback"===o.data.type&&"onFormReady"===o.data.eventName)if(document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\').length>0)e.ClearbitForHubspot.addForm(document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\'));else if(document.querySelectorAll("iframe.hs-form-iframe").length>0){document.querySelectorAll("iframe.hs-form-iframe").forEach(function(t){t.contentWindow.document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\').length>0&&e.ClearbitForHubspot.addForm(t.contentWindow.document.querySelectorAll(\'form[data-form-id="\'+o.data.id+\'"]\'))})}})}(window);';
         script.async = true;
-        document.body.appendChild(script);
+        document.head.appendChild(script);
     }
 
     public toggle(): void {
