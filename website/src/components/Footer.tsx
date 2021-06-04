da4db87fd5b26bc7ab29319674d487346167ed5b
Mate Gyorffy <mgyorffy@oneteamtech.com>
2021-06-04 16:45:06 -0700

    remove sticky announcement bar - temporarily; small fixes to resovle react errors and warnings in console

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -54,7 +54,7 @@ export const Footer: React.FunctionComponent<{ minimal?: boolean }> = ({ minimal
                                     <Link to="/podcast">Podcast</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link to="https://info.sourcegraph.com/emergence-of-big-code-2020-survey">Big code survey</Link>
+                                    <a href="https://info.sourcegraph.com/emergence-of-big-code-2020-survey">Big code survey</a>
                                 </li>
                             </ul>
                         </div>
