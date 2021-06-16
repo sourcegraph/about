8f4834d86b1e45923930e24d3edd807f0503ab4c
Erica Lindberg <73327004+content-hacker@users.noreply.github.com>
2021-06-16 16:50:36 -0600

    Update code-search-guide.tsx
    add paragraph tags

--- website/src/pages/code-search-guide.tsx
+++ website/src/pages/code-search-guide.tsx
@@ -53,25 +53,23 @@ export default ((props: any) => (
 
                 <div className="col-lg-7" id="1">
                     <h2 className="display-3">1 Introduction</h2>
-                    <p>
-                        The way the world creates code has shifted. As developers, we're working with rapidly growing codebases, a proliferating number of repositories, multiple languages and file formats, and a wide variety of developer tools. 
+                    <p> The way the world creates code has shifted. As developers, we're working with rapidly growing codebases, a proliferating number of repositories, multiple languages and file formats, and a wide variety of developer tools. </p>
                         
-                        With increased complexity, we still need to efficiently write and make changes to our code while meeting tight deadlines and stringent quality and security requirements. No pressure. 
+                    <p> With increased complexity, we still need to efficiently write and make changes to our code while meeting tight deadlines and stringent quality and security requirements. No pressure. </p>
                         
-                        On top of that, we're often dealing with knowledge silos, poor code quality, and manual processes that interrupt flow and stall productivity. Though we'd like to pretend we're impervious to those issues, let's be real. We're all familiar with the outcome: Development Delays (yes, with a capital D), which lead to late releases, poor quality, frustrated teams, unhappy customers, and uncompetitive products. 
+                    <p> On top of that, we're often dealing with knowledge silos, poor code quality, and manual processes that interrupt flow and stall productivity. Though we'd like to pretend we're impervious to those issues, let's be real. We're all familiar with the outcome: Development Delays (yes, with a capital D), which lead to late releases, poor quality, frustrated teams, unhappy customers, and uncompetitive products. </p>
                         
-                        Google and Facebook were among the first to start addressing this problem. Their solution? Invest hundreds of millions of dollars in their customized, proprietary code search infrastructure for internal use. 
+                    <p> Google and Facebook were among the first to start addressing this problem. Their solution? Invest hundreds of millions of dollars in their customized, proprietary code search infrastructure for internal use. </p>
                         
-                        But what about the rest of us?
+                    <p> But what about the rest of us? </p>
                         
-                        This is where universal code search (the topic of this guide) comes in. 
+                    <p> This is where universal code search (the topic of this guide) comes in. </p>
                         
-                        Companies like Uber, Lyft, Yelp, Qualtrics, and others adopted search technology to enhance developer productivity in the era of Big Code. Unfortunately, a lot of devs and companies don't know that a search engine for code even exists! We made this guide because we love code search, and we want everyone to know why and how to get started.
+                    <p> Companies like Uber, Lyft, Yelp, Qualtrics, and others adopted search technology to enhance developer productivity in the era of Big Code. Unfortunately, a lot of devs and companies don't know that a search engine for code even exists! We made this guide because we love code search, and we want everyone to know why and how to get started. </p>
                         
-                        In the spirit of Sourcegraph's [open source/open product/open company ethos](https://about.sourcegraph.com/handbook/company), we're sharing advice and best practices to help you navigate your code easier, no matter what tool(s) you may have access to. 
+                    <p> In the spirit of Sourcegraph's [open source/open product/open company ethos](https://about.sourcegraph.com/handbook/company), we're sharing advice and best practices to help you navigate your code easier, no matter what tool(s) you may have access to. </p>
                         
-                        Our intention with this guide is to help every dev and development team navigate their codebase with greater ease so you can spend more time creating and less time agonizing over unintended rabbit holes. Let's get started! 
-                    </p>
+                    <p> Our intention with this guide is to help every dev and development team navigate their codebase with greater ease so you can spend more time creating and less time agonizing over unintended rabbit holes. Let's get started! </p>
 
                     <div className="container my-4 video-embed embed-responsive embed-responsive-16by9 ">
                         <iframe
@@ -109,18 +107,16 @@ export default ((props: any) => (
 
             	<div className="col-lg-7 offset-lg-4" id="3">
                     <h2 className="display-3">3 Why code search?</h2>
-                    <p>
-                        Once developers have code search they wonder how they ever lived without it. Being able to quickly find, understand, and fix the code you’re looking for just makes sense when 75% of a developer’s time is spent reading and understanding code. Not surprisingly, this sentiment is quite common and motivating among people who use and build code search tools. Han-Wen Nienhuys, the creator of the Zoekt code search engine, says:
+                    <p> Once developers have code search they wonder how they ever lived without it. Being able to quickly find, understand, and fix the code you’re looking for just makes sense when 75% of a developer’s time is spent reading and understanding code. Not surprisingly, this sentiment is quite common and motivating among people who use and build code search tools. Han-Wen Nienhuys, the creator of the Zoekt code search engine, says:</p>
                         
                         *"Software engineering is more about reading code than writing it, and part of this process is finding the code that you should read."*
 	                                 *– Han-Wen Nienhuys, "[Going beyond grep for searching source code](https://medium.com/hotels-com-technology/going-beyond-grep-for-searching-source-code-zoekt-e7da88ac7b2e)"*
                         
-                        Developers at local business listing site Yelp [use code search to scan through tens of thousands of lines of code](https://engineeringblog.yelp.com/2019/11/winning-the-hackathon-with-sourcegraph.html#shipping-code-faster-with-sourcegraph) before adding new parameters that could unintentionally break something elsewhere on the site. Code search empowers them to ship code fast and more reliably. 
+                    <p> Developers at local business listing site Yelp [use code search to scan through tens of thousands of lines of code](https://engineeringblog.yelp.com/2019/11/winning-the-hackathon-with-sourcegraph.html#shipping-code-faster-with-sourcegraph) before adding new parameters that could unintentionally break something elsewhere on the site. Code search empowers them to ship code fast and more reliably. </p>
                         
-                        For businesses, code search is critical to maintaining development velocity. As code bases become more complex, more interconnected, and harder to navigate, development velocity stalls. It takes longer to find answers, and constant context switching squashes productive workflows. Tech giants like Microsoft, Google, and Facebook are also keenly aware of how important good code search is to development velocity and have invested millions of dollars into building internal code search tools. 
+                    <p> For businesses, code search is critical to maintaining development velocity. As code bases become more complex, more interconnected, and harder to navigate, development velocity stalls. It takes longer to find answers, and constant context switching squashes productive workflows. Tech giants like Microsoft, Google, and Facebook are also keenly aware of how important good code search is to development velocity and have invested millions of dollars into building internal code search tools. </p>
                         
-                        Code search improves the experience and velocity of development by acting as a search bar that can point devs in the right direction when they get stuck or need answers. It’s an integral part of an efficient dev workflow as it allows you to find code across all your repositories to find answers, references, and examples. If you are in the business of writing and shipping code, you should know about code search and how to use it. 
-                    </p>
+                    <p> Code search improves the experience and velocity of development by acting as a search bar that can point devs in the right direction when they get stuck or need answers. It’s an integral part of an efficient dev workflow as it allows you to find code across all your repositories to find answers, references, and examples. If you are in the business of writing and shipping code, you should know about code search and how to use it. </p>
 
                     <div className="container my-4 video-embed embed-responsive embed-responsive-16by9 ">
                         <iframe
