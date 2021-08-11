1c3f37c8f2119de5f0026246dee9d01a2981fe8d
mgyorffy <mate.gyorffy@sourcegraph.com>
2021-08-11 13:48:24 -0700

    add Triblio scripts to about site

--- website/src/html.tsx
+++ website/src/html.tsx
@@ -38,6 +38,10 @@ export default class HTML extends React.Component<HtmlProps> {
                     <link rel="stylesheet" href="https://use.typekit.net/ngk3rlb.css"/>
 
                     <meta name="google-site-verification" content="vRPkjcQnrXKgId0IyxVPHp0CGp3B7zaEFiTpyb8kPSQ" />
+                    
+                    {/* Add Triblio "Webpage Personalization Script" to support Sales/Markting goals */}
+                    <script type='text/javascript' data-cfasync='false' src='https://tribl.io/h.js?orgId=Yee6bMKj7QSARqAePdE8'></script>
+
                     <script data-cookieconsent="ignore" dangerouslySetInnerHTML={{
                         __html: `window.dataLayer = window.dataLayer || [];
                                 function gtag() {
@@ -52,6 +56,7 @@ export default class HTML extends React.Component<HtmlProps> {
                     `,
                     }}
                     />
+
                     {/* Google Tag Manager */}
                     <script data-cookieconsent="ignore" dangerouslySetInnerHTML={{
                         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -62,6 +67,7 @@ export default class HTML extends React.Component<HtmlProps> {
                     `,
                     }}
                     />
+
                     {/* End Google Tag Manager */}
                     <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="fb31dc3e-afb3-4be8-ae84-7090bba7797d" data-blockingmode="auto" type="text/javascript"></script>
 
@@ -70,10 +76,11 @@ export default class HTML extends React.Component<HtmlProps> {
 
                     {/* Add Clearbit script for Hubspot form enrichment */}
                     <script type="text/javascript" dangerouslySetInnerHTML={{
-                        __html: `!function(e){var o=document.getElementsByTagName("script")[0];if("object"==typeof e.ClearbitForHubspot)return console.log("Clearbit For HubSpot included more than once"),!1;e.ClearbitForHubspot={},e.ClearbitForHubspot.forms=[],e.ClearbitForHubspot.addForm=function(o){var t=o[0];"function"==typeof e.ClearbitForHubspot.onFormReady?e.ClearbitForHubspot.onFormReady(t):e.ClearbitForHubspot.forms.push(t)};var t=document.createElement("script");t.async=!0,t.src="https://hubspot.clearbit.com/v1/forms/pk_a66b9ed76e62c713c06aab39bfae7234/forms.js",o.parentNode.insertBefore(t,o),e.addEventListener("message",function(o){if("hsFormCallback"===o.data.type&&"onFormReady"===o.data.eventName)if(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0)e.ClearbitForHubspot.addForm(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'));else if(document.querySelectorAll("iframe.hs-form-iframe").length>0){document.querySelectorAll("iframe.hs-form-iframe").forEach(function(t){t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0&&e.ClearbitForHubspot.addForm(t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'))})}})}(window);
+                            __html: `!function(e){var o=document.getElementsByTagName("script")[0];if("object"==typeof e.ClearbitForHubspot)return console.log("Clearbit For HubSpot included more than once"),!1;e.ClearbitForHubspot={},e.ClearbitForHubspot.forms=[],e.ClearbitForHubspot.addForm=function(o){var t=o[0];"function"==typeof e.ClearbitForHubspot.onFormReady?e.ClearbitForHubspot.onFormReady(t):e.ClearbitForHubspot.forms.push(t)};var t=document.createElement("script");t.async=!0,t.src="https://hubspot.clearbit.com/v1/forms/pk_a66b9ed76e62c713c06aab39bfae7234/forms.js",o.parentNode.insertBefore(t,o),e.addEventListener("message",function(o){if("hsFormCallback"===o.data.type&&"onFormReady"===o.data.eventName)if(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0)e.ClearbitForHubspot.addForm(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'));else if(document.querySelectorAll("iframe.hs-form-iframe").length>0){document.querySelectorAll("iframe.hs-form-iframe").forEach(function(t){t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0&&e.ClearbitForHubspot.addForm(t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'))})}})}(window);
                         `,
-                    }}
+                        }}
                     />
+
                 </head>
                 <body>
                     {/*   Google Tag Manager (noscript) */}
@@ -82,6 +89,10 @@ export default class HTML extends React.Component<HtmlProps> {
                         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TB4NLS7" className="gtm-hide" height="0" width="0"></iframe>
                     </noscript>
                     {/*  End Google Tag Manager (noscript) */}
+
+                    {/* Add Triblio "Analytics and Overlay Cards script" to support Sales/Markting goals */}
+                    <script type='text/javascript' data-cfasync='false' src='https://tribl.io/footer.js?orgId=Yee6bMKj7QSARqAePdE8'></script>
+
                     <div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
                     {this.props.postBodyComponents}
                 </body>
