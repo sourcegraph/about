fe708e5b77b2fc1e1b275cbd61a72f929eebfec9
Rebecca Dodd <rebecca@sourcegraph.com>
2021-08-20 12:07:44 +0100

    Update header

--- website/src/components/blog/postTypes.tsx
+++ website/src/components/blog/postTypes.tsx
@@ -99,10 +99,15 @@ export interface BlogTypeInfo {
 
 export const BLOG_TYPE_TO_INFO: Record<BlogType, BlogTypeInfo> = {
     blog: {
-        title: 'Blog',
+        title: 'strings',
         baseUrl: '/blog',
+        belowTitle: (
+            <>
+                <p className="mb-1"><i>A collection of characters, stories, and other elements</i> <small>from Sourcegraph</small></p>
+            </>
+        ),
         meta: {
-            title: 'Sourcegraph Blog',
+            title: 'strings: the Sourcegraph blog',
             description:
                 "News from Sourcegraph: our changelog, announcements, tech blog posts, and anything else we think you'll find interesting.",
         },
