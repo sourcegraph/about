e4adf0ae8f067684e7121438a247040bac322497
mgyorffy <mgyorffy@users.noreply.github.com>
2021-09-02 01:10:55 +0000

    squash! Prettier

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -21,12 +21,12 @@ export default class Header extends React.Component<HeaderProps, any> {
         this.dropdownToggle = this.dropdownToggle.bind(this)
         this.state = {
             isOpen: false,
-            menuOpen: false
+            menuOpen: false,
         }
     }
 
-    public dropdownToggle(newValue){
-        this.setState({ menuOpen: newValue });
+    public dropdownToggle(newValue) {
+        this.setState({ menuOpen: newValue })
     }
 
     public toggle(toggle): void {
@@ -38,7 +38,10 @@ export default class Header extends React.Component<HeaderProps, any> {
     public render(): JSX.Element | null {
         return (
             <>
-                <nav expand="lg" className={`header navbar navbar-expand-md py-3 ${this.props.className || 'navbar-light'}`}>
+                <nav
+                    expand="lg"
+                    className={`header navbar navbar-expand-md py-3 ${this.props.className || 'navbar-light'}`}
+                >
                     <div className="container-lg px-0 px-lg-3">
                         <Navbar.Brand className="navbar-brand header__logo" href="/">
                             <span role="img" aria-label="Sourcegraph - Universal code search">
@@ -64,33 +67,32 @@ export default class Header extends React.Component<HeaderProps, any> {
                                     id="navcol-1"
                                 >
                                     <Nav className="me-auto">
-                                        <NavDropdown
-                                            onToggle={val => this.dropdownToggle(val)}
-                                            title="Product"
-                                        >
+                                        <NavDropdown onToggle={val => this.dropdownToggle(val)} title="Product">
                                             <NavDropdown.Item href="/code-search">Code Search</NavDropdown.Item>
                                             <NavDropdown.Item href="/batch-changes">Batch Changes</NavDropdown.Item>
-                                            <NavDropdown.Item href="https://docs.sourcegraph.com/code_intelligence">Code Intelligence</NavDropdown.Item>
+                                            <NavDropdown.Item href="https://docs.sourcegraph.com/code_intelligence">
+                                                Code Intelligence
+                                            </NavDropdown.Item>
                                         </NavDropdown>
-                                        <NavDropdown
-                                            title="Resources"
-                                            onToggle={this.dropdownToggle}
-                                        >
+                                        <NavDropdown title="Resources" onToggle={this.dropdownToggle}>
                                             <NavDropdown.Item href="/blog">Blog</NavDropdown.Item>
-                                            <NavDropdown.Item href="https://learn.sourcegraph.com/" target="_blank">Learn</NavDropdown.Item>
-                                            <NavDropdown.Item href="https://www.youtube.com/playlist?list=PL6zLuuRVa1_iDEP4EicZ8972RgyccCRGF">Dev Tool Time</NavDropdown.Item>
+                                            <NavDropdown.Item href="https://learn.sourcegraph.com/" target="_blank">
+                                                Learn
+                                            </NavDropdown.Item>
+                                            <NavDropdown.Item href="https://www.youtube.com/playlist?list=PL6zLuuRVa1_iDEP4EicZ8972RgyccCRGF">
+                                                Dev Tool Time
+                                            </NavDropdown.Item>
                                             <NavDropdown.Item href="/podcast/">Sourcegraph Podcast</NavDropdown.Item>
                                         </NavDropdown>
-                                        <NavDropdown
-                                            title="Customers"
-                                            onToggle={this.dropdownToggle}
-                                        >
+                                        <NavDropdown title="Customers" onToggle={this.dropdownToggle}>
                                             <NavDropdown.Item href="/case-studies">Case studies</NavDropdown.Item>
                                             <NavDropdown.Item href="/customers">Use cases</NavDropdown.Item>
                                             <NavDropdown.Item href="/contact/sales">Become one</NavDropdown.Item>
                                         </NavDropdown>
                                         <Nav.Link href="/pricing">Pricing</Nav.Link>
-                                        <Nav.Link href="https://docs.sourcegraph.com" target="_blank">Docs</Nav.Link>
+                                        <Nav.Link href="https://docs.sourcegraph.com" target="_blank">
+                                            Docs
+                                        </Nav.Link>
                                         <Nav.Link
                                             href="https://sourcegraph.com/sign-in"
                                             title="Search public code with Sourcegraph Cloud"
