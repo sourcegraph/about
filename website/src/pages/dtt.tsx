c003eb60b807563dffa8445b68260bf5d189fbce
Scott Bailey <csb5t@virginia.edu>
2021-11-08 10:35:08 -0500

    Remove ContenSection component to fix styling

--- website/src/pages/dtt.tsx
+++ website/src/pages/dtt.tsx
@@ -5,7 +5,6 @@ import { PostsList } from '../components/blog/PostsList'
 import { BlogType, BLOG_TYPE_TO_INFO, urlToPost } from '../components/blog/postTypes'
 import { BlogHeader } from '../components/blog/BlogHeader'
 import { FeaturedEpisode } from '../components/blog/FeaturedEpisode'
-import { ContentSection } from '../components/content/ContentSection'
 import HubspotForm from '../components/HubspotForm'
 
 export const DttPage: React.FunctionComponent<PageProps<{ allMarkdownRemark: any }>> = props => {
@@ -41,7 +40,7 @@ export const DttPage: React.FunctionComponent<PageProps<{ allMarkdownRemark: any
                     {/* Given the profusion dtt logos, we may want to remove this banner. */}
                     {/* <img src="/dtt_landing_page.jpg" alt="Dev Tool Time logo" style={{ maxWidth: '100%' }} /> */}
                 </div>
-                <ContentSection className="py-5">
+                <section className="py-5">
                     {' '}
                     <h2>Recent episodes:</h2>
                     <FeaturedEpisode
@@ -65,14 +64,14 @@ export const DttPage: React.FunctionComponent<PageProps<{ allMarkdownRemark: any
                             />
                         </div>
                     </div>
-                </ContentSection>
+                </section>
                 <section>
                     <h2>Other episodes:</h2>
-                    <PostsList blogInfo={BLOG_TYPE_TO_INFO[BlogType.DTT]} posts={posts.slice(2, -1)} />
                     <p>
                         You can view the full archive of Dev Tool Time episodes on{' '}
                         <a href="https://www.youtube.com/playlist?list=PL6zLuuRVa1_iDEP4EicZ8972RgyccCRGF">Youtube</a>.
                     </p>
+                    <PostsList blogInfo={BLOG_TYPE_TO_INFO[BlogType.DTT]} posts={posts.slice(2, -1)} />
                 </section>
             </div>
         </Layout>
