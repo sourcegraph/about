280175f85c5aa34d3a61e2fdff28b42f515f724d
mgyorffy <mgyorffy@users.noreply.github.com>
2021-12-21 19:41:36 +0000

    squash! Prettier

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -66,9 +66,7 @@ export default class Header extends React.Component<HeaderProps, any> {
     public render(): JSX.Element | null {
         return (
             <>
-                <nav
-                    className={`header navbar py-3 ${this.props.className || 'navbar-light'}`}
-                >
+                <nav className={`header navbar py-3 ${this.props.className || 'navbar-light'}`}>
                     <div className="container-lg">
                         <Navbar.Brand className="header__logo" href="/" onContextMenu={this.handleRightClick}>
                             <span role="img" aria-label="Sourcegraph - Universal code search">
