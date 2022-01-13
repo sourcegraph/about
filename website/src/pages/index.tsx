46e452bb85a60ac8f847eecf579874c66ff70fbe
iskyOS <iskyOS@users.noreply.github.com>
2022-01-13 21:07:38 +0000

    squash! Prettier

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -88,8 +88,8 @@ const Index: React.FunctionComponent = (props: any) => (
                     <div className="col-lg-5">
                         <h2 className="display-3 font-weight-bold">Search open source code, too.</h2>
                         <p>
-                            Sourcegraph is indexing millions of open source repositories across several code hosts. You'll
-                            be able to search a universe of code — currently over 2 million repositories and
+                            Sourcegraph is indexing millions of open source repositories across several code hosts.
+                            You'll be able to search a universe of code — currently over 2 million repositories and
                             counting — in milliseconds. Explore, learn from, and reference the best community-developed
                             code out there.
                         </p>
