58345d90958ad8be134b3813debe42cd4dd8af3f
mgyorffy <mgyorffy@users.noreply.github.com>
2022-01-18 00:45:34 +0000

    squash! Prettier

--- website/src/pages/get-started.tsx
+++ website/src/pages/get-started.tsx
@@ -196,9 +196,7 @@ export const GetStartedPage: React.FunctionComponent<PageProps> = props => {
                                 <span>Deployment Options</span>
                             </div>
                             <h1 className="display-2 title">Sourcegraph Cloud</h1>
-                            <p>
-                                Sync your code from GitHub.com or GitLab.com. No technical setup is required.
-                            </p>
+                            <p>Sync your code from GitHub.com or GitLab.com. No technical setup is required.</p>
                             <div className="small-title">Best For</div>
                             <p>Individual developers (small teams coming soon)</p>
                             <p>Search across your repositories and the open-source universe.</p>
@@ -223,14 +221,9 @@ export const GetStartedPage: React.FunctionComponent<PageProps> = props => {
 
                         <div className="new-container d-none" ref={node => (newContainerRightRef.current = node)}>
                             <div className="bg-white login-section">
-                                <h2 className="get-started-page__search-headings">
-                                    Search open source code
-                                </h2>
+                                <h2 className="get-started-page__search-headings">Search open source code</h2>
                                 <p>No account required.</p>
-                                <a
-                                    href="https://sourcegraph.com/search"
-                                    className="btn btn-primary"
-                                >
+                                <a href="https://sourcegraph.com/search" className="btn btn-primary">
                                     Start searching now <ArrowRightIcon />
                                 </a>
                             </div>
@@ -242,14 +235,24 @@ export const GetStartedPage: React.FunctionComponent<PageProps> = props => {
                                     href="https://sourcegraph.com/.auth/github/login?pc=https%3A%2F%2Fgithub.com%2F%3A%3Ae917b2b7fa9040e1edd4&redirect=%2Fwelcome"
                                     className="btn github"
                                 >
-                                    <img src="../external-logos/GitHub-Mark-Light-32px.png" width="30" height="30" className="mr-3"/>
+                                    <img
+                                        src="../external-logos/GitHub-Mark-Light-32px.png"
+                                        width="30"
+                                        height="30"
+                                        className="mr-3"
+                                    />
                                     Continue with GitHub
                                 </a>
                                 <a
                                     href="https://sourcegraph.com/.auth/gitlab/login?pc=https%3A%2F%2Fgitlab.com%2F%3A%3A013395a61a639f4c3eb3668b89c96039637a86ebb6831f1e141627df3d55384d&redirect=%2Fwelcome"
                                     className="btn gitlab"
                                 >
-                                    <img src="../external-logos/gitlab-mark.svg" width="32" height="32" className="mr-3"/>
+                                    <img
+                                        src="../external-logos/gitlab-mark.svg"
+                                        width="32"
+                                        height="32"
+                                        className="mr-3"
+                                    />
                                     Continue with GitLab
                                 </a>
                                 <p className="small-font">
