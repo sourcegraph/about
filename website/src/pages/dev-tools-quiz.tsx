4762eed4bb26bab2ebdcd0248e5cf0ad884af3be
mgyorffy <mgyorffy@users.noreply.github.com>
2022-01-19 01:29:32 +0000

    squash! Prettier

--- website/src/pages/dev-tools-quiz.tsx
+++ website/src/pages/dev-tools-quiz.tsx
@@ -1,9 +1,15 @@
 import { PageProps } from 'gatsby'
 import * as React from 'react'
 
-export const DevToolsQuizPage: React.FunctionComponent<PageProps> = (props) => (
+export const DevToolsQuizPage: React.FunctionComponent<PageProps> = props => (
     <div>
-        <body id="wrapper" data-tf-widget="bSiNOCmx" data-tf-inline-on-mobile data-tf-medium="snippet" data-tf-hidden="utm_source=xxxxx,utm_medium=xxxxx,utm_campaign=xxxxx,utm_term=xxxxx,utm_content=xxxxx"></body>
+        <body
+            id="wrapper"
+            data-tf-widget="bSiNOCmx"
+            data-tf-inline-on-mobile
+            data-tf-medium="snippet"
+            data-tf-hidden="utm_source=xxxxx,utm_medium=xxxxx,utm_campaign=xxxxx,utm_term=xxxxx,utm_content=xxxxx"
+        ></body>
         <script src="//embed.typeform.com/next/embed.js"></script>
     </div>
 )
