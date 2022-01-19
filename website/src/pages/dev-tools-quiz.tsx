f49d50700e6e38cce6045e70eb3b481c37f45550
mgyorffy <mate.gyorffy@sourcegraph.com>
2022-01-19 11:37:43 -0800

    add standard typeform instead of full page

--- website/src/pages/dev-tools-quiz.tsx
+++ website/src/pages/dev-tools-quiz.tsx
@@ -1,17 +1,24 @@
 import { PageProps } from 'gatsby'
 import * as React from 'react'
+import Layout from '../components/Layout'
+import { Helmet } from 'react-helmet'
 
 export const DevToolsQuizPage: React.FunctionComponent<PageProps> = props => (
-    <div>
-        <body
-            id="wrapper"
-            data-tf-widget="bSiNOCmx"
-            data-tf-inline-on-mobile
-            data-tf-medium="snippet"
-            data-tf-hidden="utm_source=xxxxx,utm_medium=xxxxx,utm_campaign=xxxxx,utm_term=xxxxx,utm_content=xxxxx"
-        ></body>
-        <script src="//embed.typeform.com/next/embed.js"></script>
-    </div>
+    <Layout
+        location={props.location}
+        meta={{
+            title: 'New Year, New Dev Tools - Take the Quiz - Sourcegraph',
+            description:
+                'Dev tools have made huge strides recently. This is a chance to think about your go-to tools for searching, reading, and understanding code and discover some new ones along the way.',
+            image: 'https://about.sourcegraph.com/sourcegraph-og.png',
+        }}
+        className="dev-tools-quiz-page"
+    >
+        <Helmet>
+            <script src="//embed.typeform.com/next/embed.js"></script>
+        </Helmet>
+        <body data-tf-widget="bSiNOCmx" data-tf-iframe-props="title=Dev Tools Quiz" data-tf-hidden="utm_source=xxxxx,utm_medium=xxxxx,utm_campaign=xxxxx,utm_term=xxxxx,utm_content=xxxxx"></body>
+    </Layout>
 )
 
 export default DevToolsQuizPage
