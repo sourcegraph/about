3ea743442b004364946d2c531c583b8ad527febd
Kathleen Juell <kathleenjuell@gmail.com>
2022-02-01 18:27:43 -0500

    Add wrapper class to control vh on typeform

--- website/src/pages/dev-tools-quiz.tsx
+++ website/src/pages/dev-tools-quiz.tsx
@@ -17,7 +17,7 @@ export const DevToolsQuizPage: React.FunctionComponent<PageProps> = props => (
         <Helmet>
             <script src="//embed.typeform.com/next/embed.js"></script>
         </Helmet>
-        <body
+        <body className="wrapper"
             data-tf-widget="bSiNOCmx"
             data-tf-iframe-props="title=Dev Tools Quiz"
             data-tf-hidden="utm_source=xxxxx,utm_medium=xxxxx,utm_campaign=xxxxx,utm_term=xxxxx,utm_content=xxxxx"
