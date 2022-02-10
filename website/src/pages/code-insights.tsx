942ff8dc4d84aa9b14472ead6192590c7c9a51f7
Kathleen Juell <kathleenjuell@gmail.com>
2022-02-10 14:37:27 -0500

    Add Code Insights page, stylesheet, hero

---
+++ website/src/pages/code-insights.tsx
@@ -0,0 +1,255 @@
+import { Link, PageProps } from 'gatsby'
+import * as React from 'react'
+import { ContentSection } from '../components/content/ContentSection'
+import Layout from '../components/Layout'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
+
+export const CodeInsightsPage: React.FunctionComponent<PageProps> = props => (
+    <Layout
+        location={props.location}
+        meta={{
+            title: 'Sourcegraph | Code Insights',
+            description:
+                'Draw insights from your codebase about how different initiatives are tracking over time. Code Insights is now generally available for teams of all sizes.',
+            image: 'https://about.sourcegraph.com/sourcegraph-og.png',
+        }}
+        heroAndHeaderClassName="code-insights-page__hero-and-header"
+        hero={
+            <div className="container pb-4">
+                <div className="row">
+                    <div className="col-lg-6 mb-lg-6 mt-6">
+                        <h6 className="text-uppercase mb-2">Code Insights</h6>
+                        <h1 className="display-3 font-weight-bold mb-5">Track what really matters to you and your team.</h1>
+                        <h6>Effortlessly track and visualize trends in your entire codebase — kept automatically up to date.</h6>
+                        <p className="mb-3 display-5">
+                            Code Insights transforms your entire codebase into a queryable database so you can create customizable, visual dashboards in seconds
+                            and get real-time results. Accurately understand how different initiatives are progressing over time and answer questions that used 
+                            to be difficult or impossible to answer.
+                        </p>
+                        <div className="pt-1">
+                            <Link
+                                className="btn btn-primary mr-3"
+                                to="/contact/request-demo"
+                                title="Request a Demo of Code Insights."
+                            >
+                                Request a demo
+                            </Link>
+                            <a
+                                className="btn btn-outline-primary my-3"
+                                href="/get-started"
+                                title="Use this if you want to search your (or your company's) code, invite teammates, and try all the features."
+                            >
+                                Try it on Sourcegraph
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        }
+    >
+        <ContentSection className="py-5 text-center">
+            <h2 className="display-3 font-weight-bold mt-5 mb-3">Get up to speed</h2>
+            <div className="row justify-content-center">
+                <p className="col-md-8">
+                    We can't give you a 25-hour day, but here is a <a href="http://srcgr.ph/cheatsheet">speed sheet</a>{' '}
+                    with the most useful Sourcegraph shortcuts. Need even more speed? Fasten your seat belt and dive
+                    into our curated
+                    <a href="https://learn.sourcegraph.com/tags/sourcegraph"> tutorials</a> &#38; other{' '}
+                    <a href="https://docs.sourcegraph.com/">tip documents</a>.
+                </p>
+            </div>
+            <div className="row">
+                <div className="col-12 mt-4">
+                    <a href="http://srcgr.ph/cheatsheet">
+                        <figure>
+                            <img
+                                className="cheatsheet_img"
+                                src="/community/cheatsheet_top.png"
+                                alt="Sourcegraph cheatsheet"
+                            />
+                        </figure>
+                    </a>
+                </div>
+            </div>
+        </ContentSection>
+
+        <div className="bg-gradient-green-blue py-5">
+            <ContentSection className="py-5">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h2 className="display-3 font-weight-bold mb-3">Sourcegraph Champions Program</h2>
+                        <p>
+                            In the spirit of collaboration, we created the Sourcegraph Champions Program to serve the
+                            developer community, create a friendly networking space and share knowledge among each
+                            other.
+                        </p>
+                        <p>
+                            We believe that if we create the right environment and provide equal resources for all,
+                            everyone can learn how to code. This is our mission. And if you share the same idea you are
+                            a "champion" in our eyes.
+                        </p>
+                        <p>
+                            We can't wait to meet you! And send you really cool custom swag ;)
+                            <br />
+                            <a
+                                className="btn btn-primary mt-3"
+                                href="https://handbook.sourcegraph.com/marketing/becoming_a_sourcegraph_champion"
+                            >
+                                Become a Sourcegraph Champion
+                            </a>
+                        </p>
+                        <p>
+                            Or if you know someone that should be a Sourcegraph Champion, please{' '}
+                            <a href="https://forms.gle/QP6BBCpN1TwQfHzo6">nominate them</a>.
+                        </p>
+                    </div>
+                    <div className="col-lg-6">
+                        <img
+                            className="sg_champion_img mt-6"
+                            src="/community/SG_Robots_Trophy.png"
+                            alt="Become a Sourcegraph Champion!"
+                        />
+                    </div>
+                </div>
+            </ContentSection>
+        </div>
+
+        <ContentSection className="py-6">
+            <div className="row">
+                <div className="col-lg-6">
+                    <h2 className="display-3 font-weight-bold mt-5 mb-3">DM us on Slack. We're here.</h2>
+                    What you seek is seeking you — come, chat and collaborate with inspiring engineers like you.
+                    <ul className="list-spaced">
+                        <li>Ask questions - any questions: what is the best sit-stand desk?</li>
+                        <li>Reach out to the community, discover new ideas &#38; seek or give mentorship</li>
+                        <li>Share the road less traveled so that everyone can learn</li>
+                    </ul>
+                    <a
+                        className="btn btn-primary"
+                        href={
+                            'https://join.slack.com/t/sourcegraph-community/shared_invite/zt-w11gottx-c0PYTK69YVW_06tpJZ0bOQ'
+                        }
+                        title="Join us on Slack"
+                    >
+                        Join us on Slack
+                    </a>
+                </div>
+                <div className="col-lg-5">
+                    <img
+                        className="sg_champion_img mt-6"
+                        src="/community/SG_DM_us_on_slack.png"
+                        alt="DM us on Slack!"
+                    />
+                </div>
+            </div>
+        </ContentSection>
+
+        <ContentSection className="py-4">
+            <h2 className="display-3 font-weight-bold mb-5 text-center">What's next?</h2>
+            <Tabs defaultActiveKey="configuration" id="use-cases" className="justify-content-center">
+                <Tab eventKey="configuration" title="Events">
+                    <div className="row mt-5 justify-content-center">
+                        <div className="col-lg-8">
+                            <p>
+                                Keynote speakers. Job opportunities and partnerships. You can find us in every major
+                                industry event. Give us an air-hug if you see us.
+                            </p>
+                            <ul>
+                                <li>
+                                    <a href="https://events.linuxfoundation.org/open-source-summit-north-america/register/">
+                                        Open Source Summit
+                                    </a>{' '}
+                                    - 9/27 - 9/30
+                                </li>
+                                <li>
+                                    <a href="https://reactadvanced.com/">React Advanced London</a> - 10/22
+                                </li>
+                                <li>
+                                    <a href="https://africa.jsworldconference.com/">JSWorld Africa</a> - 10/30
+                                </li>
+                                <li>
+                                    <a href="https://festival.oscafrica.org/">Open Source Festival</a> - 11/11 - 11/13
+                                </li>
+                                <li>
+                                    <a href="https://www.gophercon.com/">GopherCon</a> - 12/5 - 12/8
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </Tab>
+                <Tab eventKey="refactoring" title="Dev Tool Time">
+                    <div className="row mt-5 justify-content-center">
+                        <div className="col-lg-8">
+                            <p>
+                                Cool hardware. Most-wanted guests. And hot topics. Check our{' '}
+                                <a href="https://srcgr.ph/dev-tool-time-playlist">YouTube channel</a> and subscribe to
+                                keep up with new episodes.
+                            </p>
+                            <a href="https://srcgr.ph/dev-tool-time-playlist">
+                                <img
+                                    className="w-100 h-auto mt-4"
+                                    width="850"
+                                    height="380"
+                                    src="/community/DTT_module.jpg"
+                                    alt="Dev Tool Time"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </Tab>
+                <Tab eventKey="security" title="Podcast">
+                    <div className="row mt-5 justify-content-center">
+                        <div className="col-lg-8">
+                            <p>
+                                Tune into our developer convos wherever you listen to your favorite podcasts. Every
+                                episode is an inspiration.
+                            </p>
+                            <a href="https://about.sourcegraph.com/podcast">
+                                <img
+                                    className="w-100 h-auto mt-4"
+                                    width="750"
+                                    height="472"
+                                    src="/community/Podcast_module.png"
+                                    alt="Podcasts"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </Tab>
+            </Tabs>
+        </ContentSection>
+
+        <ContentSection className="py-5">
+            <div className="row">
+                <div className="col-lg-7">
+                    <h2 className="display-3 font-weight-bold mb-3">We’d love to hear from you!</h2>
+                    <br />
+                    Connect with us on the Sourcegraph Community Slack group, direct message us on Twitter, LinkedIn, or
+                    email us at <a href="mailto:community@sourcegraph.com">community@sourcegraph.com</a>.
+                </div>
+                <div className="col-lg-5 mt-3">
+                    <a
+                        className="btn btn-secondary"
+                        href={
+                            'https://join.slack.com/t/sourcegraph-community/shared_invite/zt-w11gottx-c0PYTK69YVW_06tpJZ0bOQ'
+                        }
+                        title="Join us on Slack"
+                    >
+                        Join us on Slack
+                    </a>
+                    <br />
+                    <a
+                        className="btn btn-primary mt-3"
+                        href={'mailto:community@sourcegraph.com'}
+                        title="Send us an email"
+                    >
+                        Send us an email
+                    </a>
+                </div>
+            </div>
+        </ContentSection>
+    </Layout>
+)
+
+export default CodeInsightsPage
