66cd46e2b22d81ef387c31a86501cb742b62fcc7
Kathleen Juell <kathleenjuell@gmail.com>
2022-02-22 11:07:44 -0500

    New illustration and other QA changes

--- website/src/pages/code-insights.tsx
+++ website/src/pages/code-insights.tsx
@@ -261,7 +261,7 @@ const blogListItems = [
         description: 'Learn everything you need to know about Code Insights.',
         type: 'Docs',
         image: 'https://sourcegraphstatic.com/blog/indexing-the-oss-universe-update.png',
-        href: 'https://docs.sourcegraph.com',
+        href: 'https://docs.sourcegraph.com/code_insights',
     },
 ]
 
@@ -482,7 +482,7 @@ export const CodeInsightsPage: React.FunctionComponent<PageProps> = props => (
                 <div className="w-100 d-flex justify-content-center">
                     <a
                         className="btn btn-link px-0 py-4 col-7 docs-cta font-weight-bold"
-                        href="https://docs.sourcegraph.com/batch_changes"
+                        href="https://docs.sourcegraph.com/code_insights"
                         title="Code Insights on docs."
                     >
                         See Docs for more use cases
