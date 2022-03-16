09f0c6b510965334e30b9eb1e4ac547a5c9d4bde
bretthayes <bretthayes@users.noreply.github.com>
2022-03-16 21:03:46 +0000

    squash! Prettier

--- website/src/pages/guides/continuous-developer-onboarding/index.tsx
+++ website/src/pages/guides/continuous-developer-onboarding/index.tsx
@@ -15,27 +15,51 @@ const ContinuousDevOnboarding: FunctionComponent<PageProps> = props => {
             location={props.location}
             meta={{
                 title: 'Guide to Developer Onboarding | Accelerate Dev Onboarding',
-                description: 'Continuous onboarding shifts traditional onboarding practices to cultivate an environment that enables developers to build themselves as flexible and resilient engineers.'
+                description:
+                    'Continuous onboarding shifts traditional onboarding practices to cultivate an environment that enables developers to build themselves as flexible and resilient engineers.',
             }}
             hero={<div className={styles.hero} />}
         >
             <div className="container py-lg-6 py-4">
                 <div className="row">
                     <div className="col-lg-6 pr-lg-6">
-                        <h3 className="display-3">Continuous developer onboarding: A guide to cultivating a culture of professional growth in your engineering organization</h3>
+                        <h3 className="display-3">
+                            Continuous developer onboarding: A guide to cultivating a culture of professional growth in
+                            your engineering organization
+                        </h3>
 
-                        <p>Companies often define developer onboarding as a process exclusive to new hires.  But developer onboarding continues far past a developer's first few months. Engineers want to explore new parts of a codebase, learn new skills, and build new things as they switch teams, take on new features and own different parts of a codebase. Successful engineers are those who have the capacity to make these transitions in a largely self-guided way; they continuously onboard.</p>
+                        <p>
+                            Companies often define developer onboarding as a process exclusive to new hires. But
+                            developer onboarding continues far past a developer's first few months. Engineers want to
+                            explore new parts of a codebase, learn new skills, and build new things as they switch
+                            teams, take on new features and own different parts of a codebase. Successful engineers are
+                            those who have the capacity to make these transitions in a largely self-guided way; they
+                            continuously onboard.
+                        </p>
 
-                        <p>How do companies set developers up to continuously onboard though? This guide describes one approach that focuses on onboarding processes and an engineering culture that nourishes engineers, and provides them with the flexible, supported room to develop their own learning capacities.</p>
+                        <p>
+                            How do companies set developers up to continuously onboard though? This guide describes one
+                            approach that focuses on onboarding processes and an engineering culture that nourishes
+                            engineers, and provides them with the flexible, supported room to develop their own learning
+                            capacities.
+                        </p>
 
                         <p>Download this guide to learn:</p>
                         <ul>
-                            <li>How to recontextualize traditional onboarding practices with long-term developer growth in mind</li>
-                            <li>How to cultivate an environment that enables developers to be flexible and resilient</li>
-                            <li>How to approach people, knowledge, and tools as fundamental aspects of engineering culture</li>
+                            <li>
+                                How to recontextualize traditional onboarding practices with long-term developer growth
+                                in mind
+                            </li>
+                            <li>
+                                How to cultivate an environment that enables developers to be flexible and resilient
+                            </li>
+                            <li>
+                                How to approach people, knowledge, and tools as fundamental aspects of engineering
+                                culture
+                            </li>
                         </ul>
                     </div>
-                    
+
                     <div
                         className="col-lg-6"
                         // TODO: This is temporary as a visual guide.
