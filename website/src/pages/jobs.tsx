d6ad48ea59203ccb02d21218e0e42cb94585c2c4
st0nebraker <st0nebraker@users.noreply.github.com>
2022-03-25 18:59:33 +0000

    squash! Prettier

--- website/src/pages/jobs.tsx
+++ website/src/pages/jobs.tsx
@@ -310,8 +310,14 @@ const CareersPage = props => {
                     nextIcon={<ArrowRightIcon color="#808080" />}
                 >
                     {new Array(6).fill(null).map((el, i) => (
-                        <Carousel.Item key={`review-${i}`} className="pt-sm-6 pt-5 height-800 height-md-650 height-sm-575">
-                            <img src={`/careers/review${i + 1}.png`} className="d-block m-auto mw-lg-600 w-sm-90 mw-sm-500" />
+                        <Carousel.Item
+                            key={`review-${i}`}
+                            className="pt-sm-6 pt-5 height-800 height-md-650 height-sm-575"
+                        >
+                            <img
+                                src={`/careers/review${i + 1}.png`}
+                                className="d-block m-auto mw-lg-600 w-sm-90 mw-sm-500"
+                            />
                         </Carousel.Item>
                     ))}
                 </Carousel>
