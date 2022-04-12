745496d65ce409fe9ed2b35e8cd44f956f432b8d
bretthayes <bretthayes@users.noreply.github.com>
2022-04-12 19:43:50 +0000

    squash! Prettier

--- website/src/components/QuoteCarousel/index.tsx
+++ website/src/components/QuoteCarousel/index.tsx
@@ -32,7 +32,11 @@ export const QuoteCarousel: FunctionComponent<QuoteCarouselProps> = ({ items, au
     return (
         <div className="d-flex flex-lg-row align-items-lg-center flex-column justify-content-center text-center h-xl-450 h-lg-450 h-md-450 h-sm-550 position-relative">
             <div className="d-lg-flex d-none align-items-center btn">
-                <ArrowLeftIcon className="mr-4" color="#808080" onClick={() => carouselHook.moveCarousel('decrement')} />
+                <ArrowLeftIcon
+                    className="mr-4"
+                    color="#808080"
+                    onClick={() => carouselHook.moveCarousel('decrement')}
+                />
             </div>
             <div className="px-lg-7">
                 {carouselItems.map(item => (
@@ -60,7 +64,11 @@ export const QuoteCarousel: FunctionComponent<QuoteCarouselProps> = ({ items, au
                 className={`d-lg-none d-flex justify-content-center mt-4 w-100 position-absolute ${styles.positionNav}`}
             >
                 <div>
-                    <ArrowLeftIcon className="mr-4" color="#808080" onClick={() => carouselHook.moveCarousel('decrement')} />
+                    <ArrowLeftIcon
+                        className="mr-4"
+                        color="#808080"
+                        onClick={() => carouselHook.moveCarousel('decrement')}
+                    />
                 </div>
                 <div>
                     <ArrowRightIcon className="ml-4" color="#808080" onClick={() => carouselHook.moveCarousel()} />
