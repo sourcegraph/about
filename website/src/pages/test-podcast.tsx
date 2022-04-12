6edd1ad7f7d844318422372cdf1466f403c366cf
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.lan>
2022-04-12 16:35:10 -0600

    Mobile responsiveness

--- website/src/pages/test-podcast.tsx
+++ website/src/pages/test-podcast.tsx
@@ -4,19 +4,19 @@ import { Link, PageProps } from 'gatsby'
 import Layout from '../components/Layout'
 import { BlogListItem } from '../components/BlogListItem'
 
-const blogListItems = [
+const resourceItems = [
     {
-        title: '8 ways to implement better onboarding',
+        title: 'Continuous developer onboarding: A guide to cultivating a culture of professional growth',
         description:
-            'We spoke to engineering leaders and engineers to learn their secrets for successful, repeatable, and scalable onboarding.',
+            'Download the guide to developer onboarding and learn how to shift to a culture of continuous onboarding in your engineering organization.',
         type: 'Guide',
-        image: 'https://sourcegraphstatic.com/better-onboarding-prevent-codebase-overwhelm.png',
-        href: '/blog/better-onboarding-how-to-prevent-codebase-overwhelm',
+        image: '/guides/dev-onboarding/hero.png',
+        href: '/guides/continuous-developer-onboarding',
     },
     {
-        title: 'How we built our software engineering career framework',
+        title: 'Nutanix fixed Log4j quickly and confidently with Sourcegraph',
         description:
-            'Learn how Sourcegraph built an engineering career development framework to align the entire engineering organization and create consistent, fair, and scalalabe conversations about growth.',
+            'See how Nutanix was able to confidently identify every instance of Log4j across its sprwaling codebase and deliver patches to its customers that fully remediated the vulnerability within 4 days.',
         type: 'Case study',
         image: 'https://storage.googleapis.com/sourcegraph-assets/blog/engineering-framework-images/Engineering%20career%20framework%20hero%20FINAL.png',
         href: '/blog/software-engineer-career-ladder',
@@ -33,31 +33,33 @@ const PartnerPodcastPage: FunctionComponent<PageProps> = props => {
                     'Address security risks, onboard to a new codebase, identify the root cause of incidents, promote code reuse, improve code health, and more with Sourcegraph.',
             }}
         >
-            <div className="bg-gradient-fog-onahau py-8">
+            <div className="bg-gradient-fog-onahau py-md-8 py-6">
                 <section className="container">
                     <h1 className="display-2 font-weight-bold max-w-600">
                         Accelerate engineering velocity with Sourcegraph
                     </h1>
                     <p className="py-4">See why over 1.2M engineers use Sourcegraph to build software you rely on</p>
-                    <Link to="/demo" className="btn btn-purple">
-                        Request a demo
-                    </Link>
-                    <Link to="/get-started" className="btn btn-outline-purple ml-4">
-                        Get started
-                    </Link>
+                    <div className="d-flex flex-column flex-md-row">
+                        <Link to="/demo" className="btn btn-purple max-w-200">
+                            Request a demo
+                        </Link>
+                        <Link to="/get-started" className="btn btn-outline-purple max-w-150 ml-md-4 mt-4 mt-md-0">
+                            Get started
+                        </Link>
+                    </div>
                 </section>
             </div>
 
             {/* TODO: Move fast, case study list */}
             <div className="container my-6">
-                <h1 className="font-weight-bold">Move fast — even in big code bases</h1>
+                <h1 className="display-4 font-weight-bold">Move fast — even in big code bases</h1>
                 <p>Learn how these software companies used Sourcegraph</p>
             </div>
 
-            <div className="bg-gradient-onahau-green py-3">
+            <div className="bg-gradient-onahau-green py-6">
                 <section className="container text-center">
-                    <h1 className="font-weight-bold">Want to use Sourcegraph at your company?</h1>
-                    <p className="py-4 mx-auto max-w-500">
+                    <h1 className="display-4 font-weight-bold pt-2">Want to use Sourcegraph at your company?</h1>
+                    <p className="pt-4 mx-auto max-w-500">
                         <span>
                             <Link to="/get-started">Get started </Link>
                         </span>
@@ -70,13 +72,25 @@ const PartnerPodcastPage: FunctionComponent<PageProps> = props => {
                 </section>
             </div>
 
-            <div className="container mt-8">
+            <div className="container my-md-8 my-6">
                 <div className="col-lg-6">
                     <h1 className="mb-5 font-weight-bold">Related resources</h1>
                 </div>
-                {blogListItems.map(item => (
+                {resourceItems.map(item => (
                     <BlogListItem key={item.title} blog={item} />
                 ))}
+            </div>
+
+            <div className="py-5 d-flex flex-md-row flex-column align-items-center justify-content-center bg-purple text-white font-weight-bold">
+                <p className="max-w-200 max-w-md-400 text-center my-auto pr-md-4">
+                    Looking for our changelog? Look no further
+                </p>
+                <a
+                    href="https://sourcegraph.com/github.com/sourcegraph/sourcegraph/-/blob/CHANGELOG.md"
+                    className="rounded btn btn-outline-purple mt-md-0 mt-3"
+                >
+                    Changelog
+                </a>
             </div>
         </Layout>
     )
