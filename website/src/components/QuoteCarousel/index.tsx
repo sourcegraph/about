cdabd654439c888b63374976b93c7740b6fd760b
Kathleen Juell <kathleenjuell@gmail.com>
2022-04-19 14:06:32 -0400

    Fixes for carousels

--- website/src/components/QuoteCarousel/index.tsx
+++ website/src/components/QuoteCarousel/index.tsx
@@ -30,7 +30,7 @@ export const QuoteCarousel: FunctionComponent<QuoteCarouselProps> = ({ items, au
     const carouselItems = carouselHook.carouselItems.items as Blockquote[]
 
     return (
-        <div className="d-flex flex-lg-row align-items-lg-center flex-column justify-content-center text-center h-xl-450 h-lg-450 h-md-450 h-sm-550 h-550 position-relative">
+        <div className="d-flex flex-lg-row align-items-lg-center flex-column justify-content-center text-center h-xl-450 h-lg-450 h-md-450 h-sm-500 h-500 position-relative">
             <div className="d-lg-flex d-none align-items-center btn">
                 <ArrowLeftIcon
                     className="mr-4"
@@ -42,7 +42,7 @@ export const QuoteCarousel: FunctionComponent<QuoteCarouselProps> = ({ items, au
                 {carouselItems.map(item => (
                     <div
                         key={item.quote}
-                        className={item === carouselHook.carouselItems.currentItem ? 'd-block' : 'd-none'}
+                        className={item === carouselHook.carouselItems.currentItem ? 'd-block h-600' : 'd-none'}
                     >
                         <BlockquoteWithLogoBottom
                             quote={item.quote}
