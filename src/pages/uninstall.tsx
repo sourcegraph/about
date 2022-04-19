c55492ca2a5e8c9094bc0758c5fcac963c9fff2e
katjuell <kathleenjuell@gmail.com>
2022-04-19 14:17:36 -0400

    Add uninstall page (#117)
    * Add uninstall page
    
    * Remove unused styles

---
+++ src/pages/uninstall.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent } from 'react'
+
+import { Layout } from '@components'
+import { useHubSpot } from '@hooks'
+
+const Uninstall: FunctionComponent = () => {
+    useHubSpot({
+        portalId: '2762526',
+        formId: 'a1bc77c9-019c-4f83-b5ba-327949f8e587',
+        targetId: 'hubspotEditorForm',
+        chiliPiper: false,
+    })
+
+    return (
+        <Layout>
+            <div>
+                <section className="text-center py-4 bg-dark text-light">
+                    <h1>Thank you for using Sourcegraph</h1>
+                </section>
+                <section className="p-4 mx-auto max-w-500 bg-white text-dark">
+                    <div className="measure-wide center">
+                        <div className="d-flex flex-column p-4 mx-auto">
+                            <h3>Why did you uninstall the Sourcegraph browser extension? </h3>
+                            <p>Your feedback helps us improve the product for everyone.</p>
+                            <div id="hubspotEditorForm" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </Layout>
+    )
+}
+
+export default Uninstall
