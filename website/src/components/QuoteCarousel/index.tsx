1b3166fc9ba75fb68f04e0edff89ff17d667592b
Tim Zielonko <tim.zielonko@sourcegraph.com>
2022-04-20 10:00:10 -0500

    change spacing increments and add height for extra small screens

--- website/src/components/QuoteCarousel/index.tsx
+++ website/src/components/QuoteCarousel/index.tsx
@@ -42,7 +42,11 @@ export const QuoteCarousel: FunctionComponent<QuoteCarouselProps> = ({ items, au
                 {carouselItems.map(item => (
                     <div
                         key={item.quote}
-                        className={item === carouselHook.carouselItems.currentItem ? 'd-block h-md-auto h-600' : 'd-none'}
+                        className={
+                            item === carouselHook.carouselItems.currentItem
+                                ? 'd-block h-md-auto h-sm-600 h-625'
+                                : 'd-none'
+                        }
                     >
                         <BlockquoteWithLogoBottom
                             quote={item.quote}
