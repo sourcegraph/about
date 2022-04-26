e25168c69e821946cc180ac25a7f3a6e50002def
Tim Zielonko <tim.zielonko@sourcegraph.com>
2022-04-26 13:31:12 -0500

    new guide page for developer onboarding ebook, removed grey bg on hubspot form

---
+++ website/src/pages/guides/dev-onboarding-how-is-it-unique/index.tsx
@@ -0,0 +1,74 @@
+import React, { FunctionComponent } from 'react'
+import { PageProps } from 'gatsby'
+
+import Layout from '../../../components/Layout'
+import { useHubSpot } from '../../../hooks/hubSpot'
+import styles from './index.module.scss'
+
+const DevOnboardingUnique: FunctionComponent<PageProps> = props => {
+    useHubSpot({
+        portalId: '2762526',
+        formId: '25249bac-6544-4f50-9fc0-559745de5334',
+        targetId: 'form',
+        chiliPiper: false,
+        onFormSubmitted: () => window.open('/guides/dev-onboarding-unique/sg-dev-onboarding-what-makes-it-unique.pdf'),
+    })
+
+    return (
+        <Layout
+            location={props.location}
+            meta={{
+                title: 'Developer onboarding: How is it unique? | Sourcegraph',
+                description:
+                    'A guide to understanding how to create a developer onboarding program that differs from general employee onboarding and creates an engaged development team.',
+                image: 'https://about.sourcegraph.com/sourcegraph-og.png',
+            }}
+            hero={<div className={styles.hero} />}
+            className="bg-white"
+        >
+            <div className="container py-lg-6 py-4">
+                <div className="row">
+                    <div className="col-lg-6 pr-lg-6">
+                        <h3 className="display-3 mb-5">
+                            Developer onboarding: What makes it unique? A guide to creating an effective developer
+                            onboarding programs
+                        </h3>
+
+                        <p className="mb-4">
+                            Your organization’s ability to onboard new developers may be the difference between high
+                            attrition rates and an engaged, productive development team. But designing effective,
+                            delightful developer onboarding programs can be challenging.
+                        </p>
+
+                        <p className="mb-4">
+                            Why? Developer onboarding and general employee onboarding are two distinct programs. When
+                            companies don't understand how they differ, their developer onboarding experience can
+                            suffer.{' '}
+                            <strong>So, how can companies design a developer onboarding program that works?</strong>
+                        </p>
+
+                        <p>Download the developer onboarding guide to learn:</p>
+                        <ul>
+                            <li>
+                                How developer onboarding differs from general employee onboarding, and how to offer the
+                                best of both
+                            </li>
+                            <li>
+                                How to build the foundational <i>and</i> exceptional components of a developer
+                                onboarding program
+                            </li>
+                            <li>Examples of how 3 major tech companies onboard their developers</li>
+                            <li>And more!</li>
+                        </ul>
+                    </div>
+
+                    <div className="col-lg-6 p-5 p-lg-6 mt-5 mt-lg-0">
+                        <div id="form" />
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default DevOnboardingUnique
