2f60519128da1216b193d6a9981f0f5c7ae5dcfd
Brett Hayes <bretthayes@live.ca>
2022-04-26 11:55:01 -0400

    link font weight 600

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -119,7 +119,7 @@ export default class Header extends React.Component<HeaderProps, any> {
                                 <Nav className="right-nav justify-content-lg-end">
                                     {!this.props.hideGetStartedButton && (
                                         <Nav.Link
-                                            className="btn btn-simple px-2 py-2 font-weight-bold"
+                                            className="btn btn-simple px-2 py-2 font-weight-bolder"
                                             href="https://sourcegraph.com/search"
                                             title="Search code"
                                         >
@@ -128,7 +128,7 @@ export default class Header extends React.Component<HeaderProps, any> {
                                     )}
 
                                     <Nav.Link
-                                        className="btn btn-outline-primary ml-3 px-5 py-2 font-weight-bold"
+                                        className="btn btn-outline-primary ml-3 px-5 py-2 font-weight-bolder"
                                         data-button-style={buttonStyle.outline}
                                         data-button-location={buttonLocation.nav}
                                         data-button-type="cta"
@@ -140,7 +140,7 @@ export default class Header extends React.Component<HeaderProps, any> {
 
                                     {!this.props.hideGetStartedButton && (
                                         <Nav.Link
-                                            className="btn btn-primary ml-3 px-5 py-2 font-weight-bold"
+                                            className="btn btn-primary ml-3 px-5 py-2 font-weight-bolder"
                                             data-button-style={buttonStyle.primary}
                                             data-button-location={buttonLocation.nav}
                                             data-button-type="cta"
