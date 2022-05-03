208a2c90c9ca28fe5d8c6e36d5662215694a43ac
Brett Hayes <bretthayes@users.noreply.github.com>
2022-05-03 12:38:15 -0400

    port over event logger to react hook (#139)

---
+++ src/hooks/eventLogger.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+
+import { EventLogger } from '@sourcegraph/event-logger'
+
+interface EventArguments {
+    path: string
+    [key: string]: string | null
+}
+
+export const useEventLogger = (): void => {
+    useEffect(() => {        
+        const eventArguments: EventArguments = { path: location.pathname }
+        const urlSearchParameters = new URLSearchParams(location.search)
+        const utmParameters: string[] = ['utm_source', 'utm_campaign', 'utm_medium', 'utm_term', 'utm_content']
+        
+        for (const parameter of utmParameters) {
+            if (urlSearchParameters.has(parameter)) {
+                eventArguments[parameter] = urlSearchParameters.get(parameter)
+            }
+        }
+        
+        const eventLogger = new EventLogger('https://sourcegraph.com')
+        eventLogger?.log('ViewStaticPage', eventArguments, eventArguments)
+    })
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,28 @@
 import '@styles/globals.scss'
+
+import { ReactNode } from 'react'
+
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
 import SSRProvider from 'react-bootstrap/SSRProvider'
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => (
-    <>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+import { useEventLogger } from '@hooks'
 
-        {/* // Import all top-level scripts here: https://nextjs.org/docs/messages/no-script-in-document-page */}
+const App = ({ Component, pageProps }: AppProps): ReactNode => {
+    useEventLogger()
 
-        {/* Add Triblio "Webpage Personalization Script" for Sales/Marketing */}
-        <Script data-cfasync="false" type="text/javascript" src="https://tribl.io/h.js?orgId=Yee6bMKj7QSARqAePdE8" />
+    return (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        {/* Google Analytics */}
-        <Script data-cookieconsent="ignore" id="track-ga">
-            {`
+            {/* // Import all top-level scripts here: https://nextjs.org/docs/messages/no-script-in-document-page */}
+
+            {/* Add Triblio "Webpage Personalization Script" for Sales/Marketing */}
+            <Script data-cfasync="false" type="text/javascript" src="https://tribl.io/h.js?orgId=Yee6bMKj7QSARqAePdE8" />
+
+            {/* Google Analytics */}
+            <Script data-cookieconsent="ignore" id="track-ga">
+                {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag() {
                     dataLayer.push(arguments);
@@ -26,42 +34,42 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => (
                 });
                 gtag("set", "ads_data_redaction", true);
             `}
-        </Script>
+            </Script>
 
-        {/* Google Tag Manager */}
-        <Script data-cookieconsent="ignore" id="track-gtm">
-            {`
+            {/* Google Tag Manager */}
+            <Script data-cookieconsent="ignore" id="track-gtm">
+                {`
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','GTM-TB4NLS7');  
             `}
-        </Script>
+            </Script>
 
-        {/* Cookiebot */}
-        <Script
-            id="Cookiebot"
-            src="https://consent.cookiebot.com/uc.js"
-            data-cbid="fb31dc3e-afb3-4be8-ae84-7090bba7797d"
-            data-blockingmode="auto"
-            type="text/javascript"
-        />
+            {/* Cookiebot */}
+            <Script
+                id="Cookiebot"
+                src="https://consent.cookiebot.com/uc.js"
+                data-cbid="fb31dc3e-afb3-4be8-ae84-7090bba7797d"
+                data-blockingmode="auto"
+                type="text/javascript" />
 
-        {/* Add jQuery as a dependency for the Clearbit script - without this Clearbit doesn't work*/}
-        <Script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
+            {/* Add jQuery as a dependency for the Clearbit script - without this Clearbit doesn't work*/}
+            <Script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
 
-        {/* Add Clearbit script for Hubspot form enrichment */}
-        <Script
-            type="text/javascript"
-            id="clearbit-hubspot"
+            {/* Add Clearbit script for Hubspot form enrichment */}
+            <Script
+                type="text/javascript"
+                id="clearbit-hubspot"
             // eslint-disable-next-line quotes
-        >{`!function(e){var o=document.getElementsByTagName("script")[0];if("object"==typeof e.ClearbitForHubspot)return console.log("Clearbit For HubSpot included more than once"),!1;e.ClearbitForHubspot={},e.ClearbitForHubspot.forms=[],e.ClearbitForHubspot.addForm=function(o){var t=o[0];"function"==typeof e.ClearbitForHubspot.onFormReady?e.ClearbitForHubspot.onFormReady(t):e.ClearbitForHubspot.forms.push(t)};var t=document.createElement("script");t.async=!0,t.src="https://hubspot.clearbit.com/v1/forms/pk_a66b9ed76e62c713c06aab39bfae7234/forms.js",o.parentNode.insertBefore(t,o),e.addEventListener("message",function(o){if("hsFormCallback"===o.data.type&&"onFormReady"===o.data.eventName)if(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0)e.ClearbitForHubspot.addForm(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'));else if(document.querySelectorAll("iframe.hs-form-iframe").length>0){document.querySelectorAll("iframe.hs-form-iframe").forEach(function(t){t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0&&e.ClearbitForHubspot.addForm(t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'))})}})}(window);`}</Script>
+            >{`!function(e){var o=document.getElementsByTagName("script")[0];if("object"==typeof e.ClearbitForHubspot)return console.log("Clearbit For HubSpot included more than once"),!1;e.ClearbitForHubspot={},e.ClearbitForHubspot.forms=[],e.ClearbitForHubspot.addForm=function(o){var t=o[0];"function"==typeof e.ClearbitForHubspot.onFormReady?e.ClearbitForHubspot.onFormReady(t):e.ClearbitForHubspot.forms.push(t)};var t=document.createElement("script");t.async=!0,t.src="https://hubspot.clearbit.com/v1/forms/pk_a66b9ed76e62c713c06aab39bfae7234/forms.js",o.parentNode.insertBefore(t,o),e.addEventListener("message",function(o){if("hsFormCallback"===o.data.type&&"onFormReady"===o.data.eventName)if(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0)e.ClearbitForHubspot.addForm(document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'));else if(document.querySelectorAll("iframe.hs-form-iframe").length>0){document.querySelectorAll("iframe.hs-form-iframe").forEach(function(t){t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]').length>0&&e.ClearbitForHubspot.addForm(t.contentWindow.document.querySelectorAll('form[data-form-id="'+o.data.id+'"]'))})}})}(window);`}</Script>
 
-        <SSRProvider>
-            <Component {...pageProps} />
-        </SSRProvider>
-    </>
-)
+            <SSRProvider>
+                <Component {...pageProps} />
+            </SSRProvider>
+        </>
+    )
+}
 
 export default App
