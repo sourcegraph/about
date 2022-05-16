83fb79706497fa193a0dbdc8d76d8e364239f34a
Brett Hayes <bretthayes@live.ca>
2022-05-16 11:44:48 -0400

    open demio on submit

--- website/src/pages/webinars/preparing-for-the-next-log4j.tsx
+++ website/src/pages/webinars/preparing-for-the-next-log4j.tsx
@@ -41,6 +41,7 @@ export const Webinar: FunctionComponent<PageProps> = props => (
             }}
             form={{
                 formId: 'abb86998-1a6a-4cfd-9888-ffba09c850c9',
+                onFormSubmitted: () => window.open('https://my.demio.com/recording/ANM9Sjbx')
             }}
             description={
                 <section className="col-md-6 col-12">
