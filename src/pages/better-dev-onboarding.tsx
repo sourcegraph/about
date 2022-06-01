fe212791ec30e48e28b3cbef19244a215b1bf8c4
Tim Zielonko <tim.zielonko@sourcegraph.com>
2022-06-01 11:48:14 -0500

    Prettier

--- src/pages/better-dev-onboarding.tsx
+++ src/pages/better-dev-onboarding.tsx
@@ -50,93 +50,93 @@ const BetterDeveloperOnboarding: FunctionComponent = () => {
     })
     useChiliPiper()
     return (
-    <Layout
-        meta={{
-            title: 'Better Developer Onboarding | Sourcegraph',
-            description:
-                'Find your own answers with self-serve onboarding, codebase exploration, and knowledge sharing, without waiting for someone to point you to the relevant code.',
-        }}
-        className="navbar-light"
-        hero={
-            <>
-                <div className={styles.pageHeader}>
-                    <div className="container pb-4">
-                        <div className="row">
-                            <div className="col-lg-7 mb-6 mt-7">
-                                <h1 className="display-2 font-weight-bold mb-4">
-                                    Give your team a complete onboarding experience
-                                </h1>
-                                <div className="display-4 font-weight-normal mb-5">
-                                    With self-serve onboarding, codebase exploration, and knowledge sharing, developers
-                                    can find their own answers without waiting for someone to point them to the relevant
-                                    code.
-                                </div>
-                                <div className="d-flex flex-column pt-1 max-w-400">
-                                    <div id="topForm" />
+        <Layout
+            meta={{
+                title: 'Better Developer Onboarding | Sourcegraph',
+                description:
+                    'Find your own answers with self-serve onboarding, codebase exploration, and knowledge sharing, without waiting for someone to point you to the relevant code.',
+            }}
+            className="navbar-light"
+            hero={
+                <>
+                    <div className={styles.pageHeader}>
+                        <div className="container pb-4">
+                            <div className="row">
+                                <div className="col-lg-7 mb-6 mt-7">
+                                    <h1 className="display-2 font-weight-bold mb-4">
+                                        Give your team a complete onboarding experience
+                                    </h1>
+                                    <div className="display-4 font-weight-normal mb-5">
+                                        With self-serve onboarding, codebase exploration, and knowledge sharing,
+                                        developers can find their own answers without waiting for someone to point them
+                                        to the relevant code.
+                                    </div>
+                                    <div className="d-flex flex-column pt-1 max-w-400">
+                                        <div id="topForm" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </>
+            }
+        >
+            <ContentSection>
+                <div className="mt-6 brightness-0">
+                    <CustomerLogos />
                 </div>
-            </>
-        }
-    >
-        <ContentSection>
-            <div className="mt-6 brightness-0">
-                <CustomerLogos />
+            </ContentSection>
+
+            <ContentSection className="pb-8">
+                <FeatureWalkthrough />
+            </ContentSection>
+
+            <div className="bg-light-gray-3">
+                <IntegrationsSection />
             </div>
-        </ContentSection>
 
-        <ContentSection className="pb-8">
-            <FeatureWalkthrough />
-        </ContentSection>
-
-        <div className="bg-light-gray-3">
-            <IntegrationsSection />
-        </div>
-
-        <ContentSection className="py-lg-6 py-5">
-            <div className="row d-flex">
-                <div className="col-lg-6">
-                    <h1 className="mb-5 font-weight-bold">Related resources</h1>
+            <ContentSection className="py-lg-6 py-5">
+                <div className="row d-flex">
+                    <div className="col-lg-6">
+                        <h1 className="mb-5 font-weight-bold">Related resources</h1>
+                    </div>
+                    {resourceItems.map(item => (
+                        <BlogResourceItem key={item.title} blog={item} />
+                    ))}
                 </div>
-                {resourceItems.map(item => (
-                    <BlogResourceItem key={item.title} blog={item} />
-                ))}
-            </div>
-        </ContentSection>
+            </ContentSection>
 
-        <ContentSection>
-            <div className="col-3 col-md-12 text-center max-w-550 mx-auto">
-                <h1 className="font-weight-bold">Ready to accelerate developer onboarding? Let's talk.</h1>
-                <p>
-                    <Link href="/get-started" passHref={true}>
-                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a
-                            data-button-style={buttonStyle.text}
-                            data-button-location={buttonLocation.body}
-                            data-button-type="cta"
-                        >
-                            Get started
-                        </a>
-                    </Link>{' '}
-                    for free with up to 10 teammates or{' '}
-                    <Link href="/demo" passHref={true}>
-                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a
-                            data-button-style={buttonStyle.text}
-                            data-button-location={buttonLocation.body}
-                            data-button-type="cta"
-                        >
-                            request a demo
-                        </a>
-                    </Link>{' '}
-                    to learn about our enterprise plan and to see Sourcegraph in your own environment.
-                </p>
-            </div>
-        </ContentSection>
-    </Layout>
-)
+            <ContentSection>
+                <div className="col-3 col-md-12 text-center max-w-550 mx-auto">
+                    <h1 className="font-weight-bold">Ready to accelerate developer onboarding? Let's talk.</h1>
+                    <p>
+                        <Link href="/get-started" passHref={true}>
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                            <a
+                                data-button-style={buttonStyle.text}
+                                data-button-location={buttonLocation.body}
+                                data-button-type="cta"
+                            >
+                                Get started
+                            </a>
+                        </Link>{' '}
+                        for free with up to 10 teammates or{' '}
+                        <Link href="/demo" passHref={true}>
+                            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                            <a
+                                data-button-style={buttonStyle.text}
+                                data-button-location={buttonLocation.body}
+                                data-button-type="cta"
+                            >
+                                request a demo
+                            </a>
+                        </Link>{' '}
+                        to learn about our enterprise plan and to see Sourcegraph in your own environment.
+                    </p>
+                </div>
+            </ContentSection>
+        </Layout>
+    )
 }
 
 export default BetterDeveloperOnboarding
