478ae4fb2a6c8314f951edd36aa567f04ff7b50b
Tim Zielonko <tim.zielonko@sourcegraph.com>
2022-06-08 15:51:27 -0500

    reformat guides to GatedResourceLayout

---
+++ src/pages/guides/continuous-developer-onboarding.tsx
@@ -0,0 +1,58 @@
+import { FunctionComponent } from 'react'
+
+import { Layout, GatedResourceLayout } from '@components'
+
+export const Guide: FunctionComponent = () => (
+    <Layout
+        meta={{
+            title: 'Guide to Developer Onboarding | Accelerate Dev Onboarding',
+            description:
+                'Continuous onboarding shifts traditional onboarding practices to cultivate an environment that enables developers to build themselves as flexible and resilient engineers.',
+        }}
+        className="navbar-white"
+    >
+        <GatedResourceLayout
+            title="Continuous developer onboarding"
+            subtitle="A guide to cultivating a culture of professional growth in your engineering organization"
+            formLabel="Download the guide"
+            form={{
+                formId: '35e18409-5be7-4fcb-aa57-8152b34eef66',
+                onFormSubmitted: () => window.open('/guides/dev-onboarding/sg-continuous-developer-onboarding.pdf'),
+            }}
+            description={
+                <section className="col-md-6 col-12 pr-lg-6">
+                    <p>
+                        Companies often define developer onboarding as a process exclusive to new hires. But developer
+                        onboarding continues far past a developer's first few months. Engineers want to explore new
+                        parts of a codebase, learn new skills, and build new things as they switch teams, take on new
+                        features and own different parts of a codebase. Successful engineers are those who have the
+                        capacity to make these transitions in a largely self-guided way; they continuously onboard.
+                    </p>
+                    <p>
+                        How do companies set developers up to continuously onboard though? This guide describes one
+                        approach that focuses on onboarding processes and an engineering culture that nourishes
+                        engineers, and provides them with the flexible, supported room to develop their own learning
+                        capacities.
+                    </p>
+                    <p>
+                        <strong>Download this guide to learn:</strong>
+                    </p>
+                    <ul>
+                        <li className="mb-1">
+                            How to recontextualize traditional onboarding practices with long-term developer growth in
+                            mind
+                        </li>
+                        <li className="mb-1">
+                            How to cultivate an environment that enables developers to be flexible and resilient
+                        </li>
+                        <li className="mb-1">
+                            How to approach people, knowledge, and tools as fundamental aspects of engineering culture
+                        </li>
+                    </ul>
+                </section>
+            }
+        />
+    </Layout>
+)
+
+export default Guide
---
+++ src/pages/guides/dev-onboarding-how-is-it-unique.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable unicorn/prevent-abbreviations */
+import { FunctionComponent } from 'react'
+
+import { Layout, GatedResourceLayout } from '@components'
+
+export const Guide: FunctionComponent = () => (
+    <Layout
+        meta={{
+            title: 'Developer onboarding: How is it unique? | Sourcegraph',
+            description:
+                'A guide to understanding how to create a developer onboarding program that differs from general employee onboarding and creates an engaged development team.',
+        }}
+        className="navbar-white"
+    >
+        <GatedResourceLayout
+            title="Developer onboarding: What makes it unique?"
+            subtitle="A guide to creating an effective developer onboarding program"
+            formLabel="Download the guide"
+            form={{
+                formId: '25249bac-6544-4f50-9fc0-559745de5334',
+                onFormSubmitted: () =>
+                    window.open('/guides/dev-onboarding-unique/sg-dev-onboarding-what-makes-it-unique.pdf'),
+            }}
+            description={
+                <section className="col-md-6 col-12 pr-lg-6">
+                    <p>
+                        Your organization's ability to onboard new developers may be the difference between high
+                        attrition rates and an engaged, productive development team. But designing effective, delightful
+                        developer onboarding programs can be challenging.
+                    </p>
+                    <p>
+                        Why? Developer onboarding and general employee onboarding are two distinct programs. When
+                        companies don't understand how they differ, their developer onboarding experience can suffer.
+                    </p>
+                    <p>
+                        <strong>So, how can companies design a developer onboarding program that works?</strong>
+                    </p>
+                    <p>Download the developer onboarding guide to learn:</p>
+                    <ul>
+                        <li className="mb-1">
+                            How developer onboarding differs from general employee onboarding, and how to offer the best
+                            of both
+                        </li>
+                        <li className="mb-1">
+                            How to build the foundational <i>and</i> exceptional components of a developer onboarding
+                            program
+                        </li>
+                        <li className="mb-1">Examples of how 3 major tech companies onboard their developers</li>
+                        <li className="mb-1">And more!</li>
+                    </ul>
+                </section>
+            }
+        />
+    </Layout>
+)
+
+export default Guide
