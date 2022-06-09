47595ba03beb5229478408e85b6994ee6b223335
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.lan>
2022-06-09 12:58:52 -0600

    Max padding should be 96px/6rem

--- src/pages/webinars/preparing-for-the-next-log4j.tsx
+++ src/pages/webinars/preparing-for-the-next-log4j.tsx
@@ -83,7 +83,7 @@ const Webinar: FunctionComponent = () => {
                 }
                 videoSrc="https://www.youtube.com/embed/ANcbjQJ0OGI"
                 learnMoreCTA={
-                    <ContentSection className="d-flex flex-column align-items-center py-lg-8 py-7">
+                    <ContentSection className="d-flex flex-column align-items-center py-6">
                         <h1 className="font-weight-bold text-center">Interesting in learning more?</h1>
                         <Link href="/case-studies/nutanix-fixed-log4j-with-sourcegraph" passHref={true}>
                             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
