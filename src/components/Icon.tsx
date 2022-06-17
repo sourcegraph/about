1273f576c48059fcbebcde2c2432e32ca801ea58
Tim Zielonko <tim.zielonko@sourcegraph.com>
2022-06-17 13:49:17 -0500

    createRandomId fx on keys

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,24 +3,25 @@ import { FunctionComponent, SyntheticEvent } from 'react'
 import * as icons from '@mui/icons-material'
 import classNames from 'classnames'
 
+import { createRandomId } from '@util'
+
 interface Icon {
     name: keyof typeof icons
     size: number
     className?: string | undefined
     color?: string
-    key?: string
     onClick?: (event: SyntheticEvent) => void
     variant?: 'boxed'
 }
 
-export const Icon: FunctionComponent<Icon> = ({ name, size, className = '', color, key, onClick, variant }) => {
+export const Icon: FunctionComponent<Icon> = ({ name, size, className = '', color, onClick, variant }) => {
     const IconName = icons[name]
 
     return (
         <IconName
             className={classNames(className, variant === 'boxed' && 'bg-violet-mist text-vivid-violet rounded p-2')}
             style={{ fontSize: size }}
-            key={key}
+            key={createRandomId()}
             htmlColor={color}
             onClick={onClick}
         />
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,2 +1,3 @@
 export { formatDate } from './formatDate'
 export { convertExcerptMarkdown } from './convertExcerptMarkdown'
+export { createRandomId } from './createRandomId'
