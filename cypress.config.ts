629898763298e644502f9e73a0688079049b55d3
Brett Hayes <bretthayes@live.ca>
2022-06-17 14:12:37 -0400

    prettier

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    specPattern: '**/*.test.ts',
-    video: false,
-    viewportWidth: 1200,
-  }
-});
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        specPattern: '**/*.test.ts',
+        video: false,
+        viewportWidth: 1200,
+    },
+})
--- cypress/e2e/_app.test.ts
+++ cypress/e2e/_app.test.ts
@@ -10,49 +10,38 @@ beforeEach(() => {
     cy.visit('/')
 })
 
-describe('Google Analytics', () => {    
+describe('Google Analytics', () => {
     it('has a data layer and GTM loads', () => {
         cy.window().then(window => {
             assert.isDefined(window.dataLayer, 'window.dataLayer is defined')
-            assert.isDefined(window.dataLayer?.find(item => item.event === 'gtm.js'), 'GTM is loaded')
+            assert.isDefined(
+                window.dataLayer?.find(item => item.event === 'gtm.js'),
+                'GTM is loaded'
+            )
         })
     })
 
     it('renders a GA and GTM script tag and should not have a src attribute', () => {
-        cy.get('#track-ga')
-            .should('not.have.attr', 'src')
-        cy.get('#track-gtm')
-            .should('not.have.attr', 'src')
+        cy.get('#track-ga').should('not.have.attr', 'src')
+        cy.get('#track-gtm').should('not.have.attr', 'src')
     })
 })
 
 describe('Third party scripts', () => {
     it('renders Triblio web personalization script in the head', () => {
-        cy.get('#triblio-p')
-            .parent()
-            .should('have.prop', 'tagName')
-            .should('equal', 'HEAD')
+        cy.get('#triblio-p').parent().should('have.prop', 'tagName').should('equal', 'HEAD')
     })
 
     it('renders Triblio Analaytics and Overlay Cards script in the body', () => {
-        cy.get('#triblio-a')
-            .parent()
-            .should('have.prop', 'tagName')
-            .should('equal', 'BODY')
+        cy.get('#triblio-a').parent().should('have.prop', 'tagName').should('equal', 'BODY')
     })
-    
+
     it('renders Cookiebot script in the body', () => {
-        cy.get('#cookiebot')
-            .parent()
-            .should('have.prop', 'tagName')
-            .should('equal', 'BODY')
+        cy.get('#cookiebot').parent().should('have.prop', 'tagName').should('equal', 'BODY')
     })
-    
+
     it('renders Drift script in the body', () => {
-        cy.get('#drift')
-            .parent()
-            .should('have.prop', 'tagName')
-            .should('equal', 'BODY')
+        cy.get('#drift').parent().should('have.prop', 'tagName').should('equal', 'BODY')
     })
 })
 
