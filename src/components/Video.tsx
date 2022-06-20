609cee96079d1821d0260602f24bf816dcf4bd7e
Tim Zielonko <tim.zielonko@sourcegraph.com>
2022-06-20 16:45:11 -0500

    add figcaption to Video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -11,29 +11,31 @@ interface Video {
 }
 
 export const Video: FunctionComponent<Video> = ({ source, loop, caption, title }) => (
-    <video
-        className="w-100 h-auto shadow"
-        width={1280}
-        height={720}
-        autoPlay={true}
-        muted={true}
-        loop={loop}
-        playsInline={true}
-        controls={false}
-        title={title}
-        // GCS does not set cookies, so we don't want Cookiebot to block this video based on consent
-        data-cookieconsent="ignore"
-    >
-        <source
-            type="video/webm"
-            src={`https://storage.googleapis.com/sourcegraph-assets/${source.webm}.webm`}
+    <figure>
+        <video
+            className="w-100 h-auto shadow"
+            width={1280}
+            height={720}
+            autoPlay={true}
+            muted={true}
+            loop={loop}
+            playsInline={true}
+            controls={false}
+            title={title}
+            // GCS does not set cookies, so we don't want Cookiebot to block this video based on consent
             data-cookieconsent="ignore"
-        />
-        <source
-            type="video/mp4"
-            src={`https://storage.googleapis.com/sourcegraph-assets/${source.mp4}.mp4`}
-            data-cookieconsent="ignore"
-        />
-        {caption}
-    </video>
+        >
+            <source
+                type="video/webm"
+                src={`https://storage.googleapis.com/sourcegraph-assets/${source.webm}.webm`}
+                data-cookieconsent="ignore"
+            />
+            <source
+                type="video/mp4"
+                src={`https://storage.googleapis.com/sourcegraph-assets/${source.mp4}.mp4`}
+                data-cookieconsent="ignore"
+            />
+            {caption && <figcaption>{caption}</figcaption>}
+        </video>
+    </figure>
 )
