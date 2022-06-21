758902a1059a9bb690c30a30b6a755f53b46e5dc
Brett Hayes <bretthayes@live.ca>
2022-06-21 14:47:24 -0400

    simplify customer logos
    - simplify markup
    - move styles to parent element
    - add missing widths to svgs

--- src/components/CustomerLogos.tsx
+++ src/components/CustomerLogos.tsx
@@ -1,43 +1,28 @@
-import React, { FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
 
 import Link from 'next/link'
-
-interface ImageWrapper {
-    isLink: boolean
-    logo: Logo
-    children: React.ReactNode
-}
-
 interface Logo {
     name: string
     src: string
-    link: string
-}
-
-interface CustomerLogos {
-    isLink?: boolean
+    link?: string
 }
 
 const logos: Logo[] = [
     {
         name: 'Amazon',
         src: '/external-logos/amazon-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'GE',
         src: '/external-logos/general-electric-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Qualtrics',
         src: '/external-logos/qualtrics-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Dropbox',
         src: '/external-logos/dropbox-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Nutanix',
@@ -47,32 +32,26 @@ const logos: Logo[] = [
     {
         name: 'Canva',
         src: '/external-logos/canva-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Uber',
         src: '/external-logos/uber-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Plaid',
         src: '/external-logos/plaid-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Reddit',
         src: '/external-logos/reddit-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Cloudflare',
         src: '/external-logos/cloudflare-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Mercado Libre',
         src: '/external-logos/mercado-libre-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Lyft',
@@ -82,40 +61,32 @@ const logos: Logo[] = [
     {
         name: 'Databricks',
         src: '/external-logos/databricks-logo.svg',
-        link: '/case-studies',
     },
     {
         name: 'Shipt',
         src: '/external-logos/shipt-logo.svg',
-        link: '/case-studies',
     },
 ]
 
-const ImageWrapper: FunctionComponent<ImageWrapper> = ({ children, isLink, logo }) => {
-    if (isLink) {
-        return (
-            <Link href={logo.link} key={logo.name} passHref={true}>
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a className="cursor-pointer">{children}</a>
-            </Link>
-        )
-    }
-
-    return <>{children}</>
-}
-
-export const CustomerLogos: FunctionComponent<CustomerLogos> = ({ isLink = false }) => (
+export const CustomerLogos: FunctionComponent = () => (
     <div className="d-flex flex-wrap align-items-center justify-content-center max-w-1000 mx-auto user-select-none">
         {logos.map((logo: Logo) => (
-            <ImageWrapper isLink={isLink} logo={logo} key={logo.name}>
-                <img
-                    src={logo.src}
-                    alt={`${logo.name} logo`}
-                    title={`${logo.name} logo`}
-                    className="max-w-100 h-100 my-3 mx-4 flex-shrink-0"
-                    draggable={false}
-                />
-            </ImageWrapper>
+            <Link
+                key={logo.name}
+                href={logo.link ? logo.link : '/case-studies'}
+                passHref={true}
+            >
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a className="cursor-pointer max-w-100 max-h-100 my-3 mx-4 flex-shrink-0">
+                    <img
+                        src={logo.src}
+                        alt={`${logo.name} logo`}
+                        title={`${logo.name} logo`}
+                        className="w-100"
+                        draggable={false}
+                    />
+                </a>
+            </Link>
         ))}
     </div>
 )
