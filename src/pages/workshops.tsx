982ac7dfc268314b7cdec25adc04694bfa672244
Vanesa Ortiz <vanesa@sourcegraph.com>
2022-07-08 00:20:31 -0700

    first draft of wokshops page

--- src/pages/workshops.tsx
+++ src/pages/workshops.tsx
@@ -11,18 +11,27 @@ export const Workshops: FunctionComponent = () => (
     >
         <GatedResourceLayout
             title="Sourcegraph Workshops"
-            formLabel="Sign up for a workshop"
+            formLabel="Sign up"
             form={{
                 formId: '3f61adb5-bcdc-4511-83cb-9be6a2e7a947',
             }}
             description={
                 <section className="col-md-6 col-12 pr-lg-6">
                     <h3 className="font-weight-normal pb-5">
-                        Candy canes pastry chocolate bar chupa chups sweet.
+                        Coreboot + Sourcegraph Workshop:<br/>
+                        July 22 <br/>1pm PT / 2pm MT / 4pm ET / 10pm CET
                     </h3>
-                    <p>Candy canes pastry chocolate bar chupa chups sweet. Sweet roll biscuit marshmallow donut bear claw cake cake jelly. Gingerbread cotton candy topping liquorice pudding croissant. Brownie gummies croissant tiramisu icing sesame snaps.</p>
-                    <p>Carrot cake cake cupcake caramels apple pie. Sugar plum bear claw candy canes gummies chupa chups pastry lollipop. Pie oat cake bear claw bear claw jelly shortbread jujubes brownie.</p>
-                    <p>Tart donut biscuit gummies bonbon sweet caramels. Pastry danish cotton candy topping lemon drops croissant gummi bears. Cake soufflé dessert topping caramels sesame snaps. Bonbon candy canes cake oat cake chocolate cake.</p>
+                    <p>Join us for a workshop on how Sourcegraph can make contributing to Coreboot easier.</p>
+                    <p> You'll learn:</p>
+                    <ul>
+                        <li> How to get access to Sourcegraph</li>
+                        <li> Different search types</li>
+                        <li> How to solve codebase challenges</li>
+                        <li> How to use code intelligence to navigate code efficiently</li>
+                        <li> How to do large-scale changes</li>
+                        <li> Documenting on Notebooks</li>
+                        <li> Code Insights</li>
+                    </ul>
                 </section>
             }
         />
