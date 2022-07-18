36fa63ea26e42d1b5ca9636205adfd5140eb9358
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.attlocal.net>
2022-07-18 09:08:35 -0700

    Swap Amazon/Indeed logo

--- src/pages/get-started/self-hosted.tsx
+++ src/pages/get-started/self-hosted.tsx
@@ -25,7 +25,7 @@ export const SelfHostedPage: FunctionComponent = () => {
                     <h1 className="display-1 mb-2">
                         <strong>Get started</strong>
                     </h1>
-                    <p>From Amazon to Uber, the world's best developers use Sourcegraph every day.</p>
+                    <p>From GE to Uber, the world's best developers use Sourcegraph every day.</p>
                 </div>
             }
             heroAndHeaderClassName={styles.hero}
