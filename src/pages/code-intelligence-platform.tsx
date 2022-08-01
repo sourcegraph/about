461b274a08234ad528308c38655ce6652d698f49
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.lan>
2022-08-01 11:00:37 -0600

    2 col & CTA section

--- src/pages/code-intelligence-platform.tsx
+++ src/pages/code-intelligence-platform.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 
 import Link from 'next/link'
 
-import { Layout, BlogResourceItem } from '@components'
+import { Layout, BlogResourceItem, ContentSection, TwoColumnSection, Video } from '@components'
 import { buttonStyle, buttonLocation } from '@data'
 
 const blogResourceItems = [
@@ -49,7 +49,7 @@ const CodeIntelligencePlatform: FunctionComponent = () => (
                             className="btn btn-primary max-w-md-200 w-100"
                             title="Request a demo"
                             data-button-style={buttonStyle.primary}
-                            data-button-location={buttonLocation.bodyDemo}
+                            data-button-location={buttonLocation.hero}
                             data-button-type="cta"
                         >
                             Request a demo
@@ -61,20 +61,144 @@ const CodeIntelligencePlatform: FunctionComponent = () => (
     >
         {/* TODO: New carousel section */}
 
-        {/* TODO: 2 Col section */}
+        <ContentSection color="white" className="py-7">
+            <h2 className="pb-5 px-0 col-12 col-lg-6">Sourcegraph helps enterprise development teams...</h2>
+
+            <TwoColumnSection
+                centerContent={true}
+                leftColumn={
+                    <>
+                        <h2 className="mb-4">Get unblocked</h2>
+                        <ul>
+                            <li className="mb-3">
+                                Sourcegraph helps developers uncover answers, understand code, and take actions in the codebase without interrupting teammates.
+                            </li>
+                            <li className="mb-3">
+                                Developers at <i>a well-known American social news site</i> save 30 – 60 minutes a day with faster access to answers.
+                            </li>
+                        </ul>
+                    </>
+                }
+                rightColumn={
+                    <Video
+                        host="self"
+                        source={{
+                            mp4: '/animations/code-search',
+                            webm: '/animations/code-search',
+                        }}
+                        title="Sourcegraph Code Search"
+                        caption="Sourcegraph Code Search"
+                        loop={true}
+                    />
+                }
+            />
+        </ContentSection>
+
+        <ContentSection color="white">
+            <TwoColumnSection
+                centerContent={true}
+                reverseOnMobile={true}
+                leftColumn={
+                    <Video
+                        host="self"
+                        source={{
+                            mp4: '/animations/code-search',
+                            webm: '/animations/code-search',
+                        }}
+                        title="Sourcegraph Code Search"
+                        caption="Sourcegraph Code Search"
+                        loop={true}
+                    />
+                }
+                rightColumn={
+                    <>
+                        <h2 className="mb-4">Save time resolving issues</h2>
+                        <ul>
+                            <li className="mb-3">
+                                Sourcegraph helps development teams quickly identify, resolve, and proactively prevent issues from happening again.
+                            </li>
+                            <li className="mb-3">
+                                <Link href="/case-studies/indeed-accelerates-development-velocity">
+                                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                    <a
+                                        title="Indeed"
+                                        data-button-style={buttonStyle.text}
+                                        data-button-location={buttonLocation.body}
+                                        data-button-type="cta"
+                                    >
+                                        Indeed
+                                    </a>
+                                </Link>
+                                {' '}saw a 90% reduction in manual work required for large-scale code changes.
+                            </li>
+                        </ul>
+                    </>
+                }
+            />
+        </ContentSection>
+
+        <ContentSection color="white" className="py-7">
+            <TwoColumnSection
+                centerContent={true}
+                leftColumn={
+                    <>
+                        <h2 className="mb-4">Gain insights and track progress</h2>
+                        <ul>
+                            <li className="mb-3">
+                                Developers use insights to track migration and deprecation projects to completion and visualize version spread and security vulnerabilities, while leaders rely on analytics to make data-driven decisions and create clear success metrics for their teams. 
+                            </li>
+                            <li className="mb-3">
+                                A <i>top hybrid multi-cloud provider</i> resolves security vulnerabilities 4x faster and measures progress each step of the way.
+                            </li>
+                        </ul>
+                    </>
+                }
+                rightColumn={
+                    <Video
+                        host="self"
+                        source={{
+                            mp4: '/animations/code-search',
+                            webm: '/animations/code-search',
+                        }}
+                        title="Sourcegraph Code Search"
+                        caption="Sourcegraph Code Search"
+                        loop={true}
+                    />
+                }
+            />
+        </ContentSection>
 
         {/* TODO: Saturn quote carousel */}
 
-        {/* TODO: CTA */}
+        <ContentSection className="py-7">
+            <section className="max-w-800 mx-auto text-md-center">
+                <h2>Sourcegraph’s code intelligence platform is more than simply search.</h2>
 
-        <div className="container max-w-900 py-7 px-0">
+                <p className="max-w-md-450 my-5 mx-md-auto">The platform drives velocity by helping development teams quickly get unblocked, save time resolving issues, and gain insights to make better decisions.</p>
+
+                <Link href="/demo">
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a
+                            className="btn btn-primary max-w-md-200 w-100"
+                            title="Request a demo"
+                            data-button-style={buttonStyle.primary}
+                            data-button-location={buttonLocation.bodyDemo}
+                            data-button-type="cta"
+                        >
+                            Request a demo
+                        </a>
+                </Link>
+            </section>
+        </ContentSection>
+
+        <ContentSection color="white" className="container max-w-900 py-7 px-0">
             <div className="col-lg-6">
                 <h1 className="mb-5 font-weight-bold">Related resources</h1>
             </div>
             {blogResourceItems.map(item => (
                 <BlogResourceItem key={item.title} blog={item} />
             ))}
-        </div>
+        </ContentSection>
     </Layout>
 )
 
