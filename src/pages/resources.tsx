cd81287cc937c6acd9c57efc31d654635aad5a60
Brett Hayes <bretthayes@live.ca>
2022-08-15 16:21:27 -0400

    Not found content UI

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from 'react'
 
+// import AlertOutlineIcon from 'mdi-react/AlertOutlineIcon'
+
 import { Layout, Filters, Card, resourceItems, ContentSection, RequestDemoTrySourcegraph } from '@components'
 
 const Resources: FunctionComponent = () => {
@@ -37,6 +39,14 @@ const Resources: FunctionComponent = () => {
             <Filters />
 
             <ContentSection color="white">
+                {/* <div className="tw-text-center tw-max-w-xl tw-mx-auto tw-mb-3xl">
+                    <span className="tw-bg-violet-100 tw-text-violet-400 tw-w-md tw-h-md tw-p-1 tw-rounded-full tw-inline-flex tw-items-center tw-justify-center tw-mb-xxs">
+                        <AlertOutlineIcon className="tw-inline" size={18} />
+                    </span>
+                    <h4>We're stumped!</h4>
+                    <p className="tw-text-lg">We couldn't find any resource that matched all the filters chosen above — but maybe these will be of use...</p>
+                </div> */}
+
                 <div className="tw-grid md:tw-grid-cols-2 xl:tw-grid-cols-3 tw-gap-sm">
                     {topResources.map(resource => (
                         <Card key={resource.title} resource={resource} />
