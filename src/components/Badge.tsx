c7298abd88640803d362dba8bac29e2e5ec396a1
Brett Hayes <bretthayes@live.ca>
2022-08-19 18:00:56 -0400

    jsdoc

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -10,7 +10,6 @@ interface Badge {
  *
  * @param props - component props
  * @param props.text - badge text
- * @returns
  */
 export const Badge: FunctionComponent<Badge> = ({ text }) => (
     <span className="tw-text-xs tw-text-gray-500 tw-bg-gray-200 tw-px-2 tw-py-1 tw-rounded-md tw-ml-xxs tw-font-mono tw-align-middle tw-font-medium">
