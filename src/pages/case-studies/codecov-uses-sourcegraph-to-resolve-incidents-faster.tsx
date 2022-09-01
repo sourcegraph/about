sourcegraph/about

commit bb82cfb0b776679fd7dcf41f83200959f16d6f83

Starship branding on 4.0 page (#5704)
* starship hero for 4.0 page

* update gradient

* rename gradient

* feedback

* inherit background in ContentSection & starship bg

* code reuse and cleanup

* headline change

* rogue attribute

* cta section slim variant, DLS blog width alignment

* move CtaSection to slug layout

* dark nebulous gradient replacement

* modify grid/col/width/padding

src/pages/case-studies/codecov-uses-sourcegraph-to-resolve-incidents-faster.tsx
@@ -1,13 +1,12 @@
 import { FunctionComponent } from 'react'
 
 import {
-    Layout,
     AuthorBio,
-    BackButton,
-    Hero,
-    NewCaseStudyLayout,
-    ContentSection,
     Blockquote,
+    ContentSection,
+    Hero,
+    Layout,
+    NewCaseStudyLayout,
     ThreeUpText,
     UseChallengeSolutionResults,
 } from '@components'
@@ -39,7 +38,10 @@ export const CaseStudy: FunctionComponent = () => (
         }}
         hero={
             <Hero
-                backButton={<BackButton href="/case-studies" text="Case Studies" />}
+                backButton={{
+                    text: 'Case Studies',
+                    link: '/case-studies',
+                }}
                 variant="venusCode"
                 title={'Codecov uses Sourcegraph to \n resolve incidents faster'}
                 displayUnderNav={true}
@@ -47,7 +49,7 @@ export const CaseStudy: FunctionComponent = () => (
         }
     >
         <NewCaseStudyLayout customer="Codecov">
-            <ContentSection color="white">
+            <ContentSection background="white">
                 <div className="tw-max-w-4xl tw-mx-auto">
                     <Blockquote
                         quote="Sourcegraph allows us to be more efficient with our time, whether it's code review, answering security-related questions from clients, or searching for things in the code much more easily than we could through our code host's native search functionality."
@@ -103,7 +105,7 @@ export const CaseStudy: FunctionComponent = () => (
                 />
             </ContentSection>
 
-            <ContentSection color="white">
+            <ContentSection background="white">
                 <div className="tw-max-w-2xl tw-mx-auto tw-pt-5xl">
                     <p className="mt-6 mt-lg-7 mt-md-5">
                         In 2021, security engineers Mitchell Borrego and Jeff Holland joined Codecov with the goal of
@@ -233,7 +235,7 @@ export const CaseStudy: FunctionComponent = () => (
                 <ThreeUpText title="Results" items={threeUpTextItems} />
             </ContentSection>
 
-            <ContentSection color="white">
+            <ContentSection background="white">
                 <div className="tw-max-w-2xl tw-mx-auto">
                     <h3 className="mb-4 max-w-650">Sourcegraph Cloud was the right fit for a growing team</h3>
                     <p>

commit 03d388efc06a0a8b6061c2c9c479c8a03023263f

Rm unused import after broken after PR #5704 merge

src/pages/case-studies/codecov-uses-sourcegraph-to-resolve-incidents-faster.tsx
@@ -2,7 +2,6 @@ import { FunctionComponent } from 'react'
 
 import {
     StaffSpotlight,
-    BackButton,
     Hero,
     NewCaseStudyLayout,
     ContentSection,

src/pages/case-studies/hashicorp-uses-sourcegraph-to-streamline-cross-repository-code-search.tsx
@@ -3,7 +3,6 @@ import { FunctionComponent } from 'react'
 import {
     Layout,
     StaffSpotlight,
-    BackButton,
     Hero,
     NewCaseStudyLayout,
     ContentSection,