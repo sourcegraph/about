bd1ed5ec03019453471d9c0eec4b56c7cb719c07
Brett Hayes <bretthayes@live.ca>
2022-09-19 15:31:38 -0400

    nextjs bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['tsx'],
@@ -29,4 +33,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
