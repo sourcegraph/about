6f64455d9aca38abc2328cbe70dd9a46718404b6
Brett Hayes <bretthayes@live.ca>
2022-09-21 13:13:36 -0400

    post launch social meta image

--- src/pages/sourcegraph-4.tsx
+++ src/pages/sourcegraph-4.tsx
@@ -25,7 +25,7 @@ const Sourcegraph4: FunctionComponent = () => (
         meta={{
             title: 'Sourcegraph - Sourcegraph 4.0',
             description: 'Introducing Sourcegraph 4.0, the code intelligence platform for the modern development team.',
-            image: 'https://storage.googleapis.com/sourcegraph-assets/about.sourcegraph.com/meta/sourcegraph-4.png',
+            image: 'https://storage.googleapis.com/sourcegraph-assets/about.sourcegraph.com/meta/sourcegraph-4-post-launch.png',
         }}
         heroAndHeaderClassName="sg-bg-gradient-starship-large"
         className="navbar-dark"
