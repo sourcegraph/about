ff3bf1e940bf231765ce6322757c56a28ce90e1f
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.lan>
2022-10-05 10:32:41 -0600

    Tooltip arrow

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,11 +3,10 @@ import { FunctionComponent, ReactNode, useEffect, useRef, useState } from 'react
 interface Tooltip {
     text: string
     position: 'top' | 'bottom' | 'right' | 'left'
-    color?: 'light' | 'dark'
     children: ReactNode
 }
 
-export const Tooltip: FunctionComponent<Tooltip> = ({ text, position, color = 'light' , children }) => {
+export const Tooltip: FunctionComponent<Tooltip> = ({ text, position, children }) => {
     const node = useRef<HTMLDivElement | null>(null)
     const [isVisible, setVisibility] = useState(false)
 
@@ -25,21 +24,23 @@ export const Tooltip: FunctionComponent<Tooltip> = ({ text, position, color = 'l
         return () => document.removeEventListener('hover', handleHover)
     }, [])
 
-    // TODO: Style, theme color, z-index, arrow
+    // TODO: z-index, arrow position
 
+    // onMouseLeave={() => setVisibility(!isVisible)}
     return (
-        <div
-            ref={node}
-            onMouseEnter={() => setVisibility(!isVisible)}
-            onMouseLeave={() => setVisibility(!isVisible)}
-            className="tw-my-auto"
-        >
-            <div className="tw-cursor-pointer">{children}</div>
+        <>
+            <div
+                ref={node}
+                onMouseEnter={() => setVisibility(!isVisible)}
+                className="tw-cursor-pointer tw-my-auto"
+            >
+                {children}
+            </div>
             {isVisible && (
-                <div>
+                <div className="tooltip tw-relative tw-z-[9999] -tw-top-10 tw-left-10 tw-max-w-[250px] tw-p-xxs tw-shadow-lg tw-bg-gray-400 tw-white tw-opacity-100 tw-text-white tw-rounded-md">
                     {text}
                 </div>
             )}
-        </div>
+        </>
     )
 }
