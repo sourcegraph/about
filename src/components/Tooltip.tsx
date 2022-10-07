2b1a1dbf003b91bd3d12ae878e52785802395f65
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com@Beccas-MBP.lan>
2022-10-07 15:19:48 -0600

    Rename

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -7,21 +7,21 @@ interface Tooltip {
 }
 
 export const Tooltip: FunctionComponent<Tooltip> = ({ text, position = 'right', children }) => {
-    const [isVisible, setVisibility] = useState(false)
+    const [visible, setVisibility] = useState(false)
 
     return (
         <>
             <div
-                onMouseEnter={() => setVisibility(!isVisible)}
-                onMouseLeave={() => setVisibility(!isVisible)}
-                onFocus={() => setVisibility(!isVisible)}
-                onBlur={() => setVisibility(!isVisible)}
+                onMouseEnter={() => setVisibility(!visible)}
+                onMouseLeave={() => setVisibility(!visible)}
+                onFocus={() => setVisibility(!visible)}
+                onBlur={() => setVisibility(!visible)}
                 className="tw-cursor-pointer tw-my-auto tooltip-wrapper"
                 role="button"
                 tabIndex={0}
             >
                 {children}
-                {isVisible && (
+                {visible && (
                     <div
                         className={`tooltip ${position} tw-min-w-[250px] tw-p-xxs tw-shadow-lg tw-bg-gray-400 tw-white tw-opacity-100 tw-text-white tw-rounded-md`}
                     >
