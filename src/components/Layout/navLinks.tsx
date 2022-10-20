502d6590af5dd1a280b1d50d291e2379d5561a52
Quinn Slack <quinn@slack.org>
2022-10-19 22:03:35 -0700

    clean up navlinks
    - sentence case
    - remove dev tool time (not updated in ~1y)

--- src/components/Layout/navLinks.tsx
+++ src/components/Layout/navLinks.tsx
@@ -38,7 +38,7 @@ export const navLinks: NavLink[] = [
         section: 'Resources',
         items: [
             {
-                title: 'All Resources',
+                title: 'All resources',
                 href: '/resources',
             },
             {
@@ -46,48 +46,48 @@ export const navLinks: NavLink[] = [
                 href: '/blog',
             },
             {
+                title: 'Docs',
+                href: 'https://docs.sourcegraph.com',
+            },
+            {
                 title: 'Learn',
                 href: 'https://learn.sourcegraph.com/',
             },
             {
-                title: 'Dev Tool Time',
-                href: 'https://www.youtube.com/playlist?list=PL6zLuuRVa1_iDEP4EicZ8972RgyccCRGF',
-            },
-            {
-                title: 'Sourcegraph Podcast',
+                title: 'Podcast',
                 href: '/podcast',
             },
             {
-                title: 'Case Studies',
+                title: 'Case studies',
                 href: '/case-studies',
             },
         ],
     },
     {
-        section: 'Use Cases',
+        section: 'Use cases',
         items: [
             {
-                title: 'All Use Cases',
+                title: 'All use cases',
                 href: '/use-cases',
             },
             {
-                title: 'Code Security',
+                title: 'Code security',
                 href: '/use-cases/code-security',
             },
             {
-                title: 'Developer Onboarding',
+                title: 'Developer onboarding',
                 href: '/use-cases/onboarding',
             },
             {
-                title: 'Incident Response',
+                title: 'Incident response',
                 href: '/use-cases/incident-response',
             },
             {
-                title: 'Code Reuse',
+                title: 'Code reuse',
                 href: '/use-cases/code-reuse',
             },
             {
-                title: 'Code Health',
+                title: 'Code health',
                 href: '/use-cases/code-health',
             },
         ],
@@ -142,12 +142,12 @@ export const footerLinks: NavLink[] = [
                 href: '/blog',
             },
             {
-                title: 'Changelog',
-                href: 'https://sourcegraph.com/github.com/sourcegraph/sourcegraph/-/blob/CHANGELOG.md',
+                title: 'Docs',
+                href: 'https://docs.sourcegraph.com',
             },
             {
-                title: 'Documentation',
-                href: 'https://docs.sourcegraph.com',
+                title: 'Changelog',
+                href: 'https://sourcegraph.com/github.com/sourcegraph/sourcegraph/-/blob/CHANGELOG.md',
             },
             {
                 title: 'Learn',
@@ -171,7 +171,7 @@ export const footerLinks: NavLink[] = [
                 href: '/about',
             },
             {
-                title: "Careers - We're Hiring!",
+                title: 'Careers',
                 href: '/jobs',
             },
             {
@@ -208,6 +208,11 @@ export const socialLinks: NavLink = {
             icon: <TwitterIcon />,
         },
         {
+            title: 'Discord',
+            href: 'https://discord.gg/s2qDtYGnAE',
+            icon: <DiscordIcon size={24} />,
+        },
+        {
             title: 'LinkedIn',
             href: 'https://www.linkedin.com/company/4803356/',
             icon: <LinkedinIcon />,
@@ -216,11 +221,6 @@ export const socialLinks: NavLink = {
             title: 'YouTube',
             href: 'https://www.youtube.com/c/Sourcegraph/featured',
             icon: <YouTubeIcon />,
-        },
-        {
-            title: 'Discord',
-            href: 'https://discord.gg/s2qDtYGnAE',
-            icon: <DiscordIcon size={24} />,
         },
         {
             title: 'Spotify',
