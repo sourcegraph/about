26d0243e417bb9d8259c4c9e7b318170be2a5dee
Quinn Slack <quinn@slack.org>
2023-01-09 20:21:50 -0800

    refactor colors

--- src/components/Pricing/PricingPlan.tsx
+++ src/components/Pricing/PricingPlan.tsx
@@ -5,25 +5,15 @@ import CheckIcon from 'mdi-react/CheckIcon'
 import { FeatureCluster, SPOTLIGHT_FEATURE_INFO } from './data'
 import { PricingPlanFeature } from './PricingPlanFeature'
 
-const COLORS: Record<'violet' | 'vermillion', { borderClass: string; textClass: string }> = {
-    vermillion: {
-        borderClass: 'tw-border-t-vermillion-300',
-        textClass: 'tw-text-vermillion-300',
-    },
-    violet: {
-        borderClass: 'tw-border-t-violet-400',
-        textClass: 'tw-text-violet-400',
-    },
-}
-
 interface Props {
     name: string
     description: string
     price: string
-    accentColor: keyof typeof COLORS
     buttons: ReactNode
     beforeFeatures?: ReactNode
     features: FeatureCluster[]
+    borderColorClass: string
+    textColorClass: string
 }
 
 /** A pricing plan on the pricing page. */
@@ -31,13 +21,14 @@ export const PricingPlan: FunctionComponent<Props> = ({
     name,
     description,
     price,
-    accentColor,
     buttons,
     beforeFeatures,
     features,
+    borderColorClass,
+    textColorClass,
 }) => (
     <div
-        className={`h-100 sm:tw-p-md tw-p-xs tw-shadow-lg tw-border-t-16 tw-rounded tw-border-gray-200 ${COLORS[accentColor].borderClass}`}
+        className={`h-100 sm:tw-p-md tw-p-xs tw-shadow-lg tw-border-t-16 tw-rounded tw-border-gray-200 ${borderColorClass}`}
     >
         <h2 className="tw-mb-sm tw-font-semibold">{name}</h2>
         <h3 className="tw-font-normal tw-max-w-sm">{description}</h3>
@@ -49,7 +40,7 @@ export const PricingPlan: FunctionComponent<Props> = ({
             {features.map(node => (
                 <div key={node.topic} className="tw-px-0 bg-transparent border-0">
                     <div className="tw-text-xl tw-font-semibold tw-flex tw-items-center">
-                        <CheckIcon className={`mr-2 ${COLORS[accentColor].textClass}`} />
+                        <CheckIcon className={`mr-2 ${textColorClass}`} />
                         <h5 className="tw-w-full">{node.topic}</h5>
                     </div>
                     <ul className="tw-ml-2xl tw-mb-xs">
