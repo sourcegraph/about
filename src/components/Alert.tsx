c9257823f31240688a54488fa3fffb1ff99ae51b
Quinn Slack <quinn@slack.org>
2023-02-16 00:53:45 -1000

    alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,10 +1,43 @@
 import { FunctionComponent, ReactNode } from 'react'
 
+import classNames from 'classnames'
+
 interface AlertProps {
-    type?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
+    type?: 'primary' | 'secondary' | 'success' | 'warning' | 'info'
     children: ReactNode
 }
 
+const COLOR_CLASSES: Record<NonNullable<AlertProps['type']>, { backgroundColor: string; borderColor: string }> = {
+    primary: {
+        backgroundColor: 'tw-bg-blue-100',
+        borderColor: 'tw-border-blue-300',
+    },
+    success: {
+        backgroundColor: 'tw-bg-blue-100',
+        borderColor: 'tw-border-blue-300',
+    },
+    info: {
+        backgroundColor: 'tw-bg-violet-100',
+        borderColor: 'tw-border-violet-300',
+    },
+    warning: {
+        backgroundColor: 'tw-bg-lemon-100',
+        borderColor: 'tw-border-lemon-300',
+    },
+    secondary: {
+        backgroundColor: 'tw-bg-gray-100',
+        borderColor: 'tw-border-gray-300',
+    },
+}
+
 export const Alert: FunctionComponent<AlertProps> = ({ type = 'primary', children }) => (
-    <div className={`alert alert-${type} tw-my-8`}>{children}</div>
+    <div
+        className={classNames(
+            'tw-px-sm tw-py-xs tw-rounded-lg tw-border-1 tw-my-8',
+            COLOR_CLASSES[type].backgroundColor,
+            COLOR_CLASSES[type].borderColor
+        )}
+    >
+        {children}
+    </div>
 )
