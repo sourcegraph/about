f284bd40c5bd5fbb01ebb9c36b4bf9a89f3be6e8
Taylor Sperry <taylor.sperry@sourcegraph.com>
2023-03-09 17:21:59 -0700

    Add localhost:3080 (#6061)
    * Add localhost:3080
    
    * Add for linux
    
    * Update app.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -174,9 +174,12 @@ const PlatformsInstallSnippet: FunctionComponent = () => (
                                         className="-ml-2.5"
                                     />
                                 </li>
-                                <li className="ml-2.5 text-xs leading-7">Run `Sourcegraph` in your local directory</li>
+                                <li className="ml-2.5 text-xs leading-7">Run `Sourcegraph` in your local directory.</li>
                                 <li className="ml-2.5 text-xs leading-7">
-                                    The app will automatically detect code in the directory you run it in.
+                                    The app will detect code in the directory you run it in.
+                                </li>
+                                <li className="ml-2.5 text-xs leading-7">
+                                    Your browser will automatically open localhost:3080.
                                 </li>
                             </ol>
                         </div>
@@ -207,9 +210,12 @@ const PlatformsInstallSnippet: FunctionComponent = () => (
 
                                     <CodeSnippet code="dpkg -i <file.deb>" className="-ml-2.5" />
                                 </li>
-                                <li className="ml-2.5 text-xs leading-7">Run `Sourcegraph` in your local directory</li>
+                                <li className="ml-2.5 text-xs leading-7">Run `Sourcegraph` in your local directory.</li>
                                 <li className="ml-2.5 text-xs leading-7">
-                                    The app will automatically detect code in the directory you run it in.
+                                    The app will detect code in the directory you run it in.
+                                </li>
+                                <li className="ml-2.5 text-xs leading-7">
+                                    Your browser will automatically open localhost:3080.
                                 </li>
                             </ol>
                         </div>
