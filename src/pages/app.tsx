7a336e60130b1a4d8d4b3ea4df86c6ec626ac5ff
Taylor Sperry <taylor.sperry@sourcegraph.com>
2023-03-09 17:08:20 -0700

    Update app.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -212,7 +212,7 @@ const PlatformsInstallSnippet: FunctionComponent = () => (
                                 </li>
                                 <li className="ml-2.5 text-xs leading-7">Run `Sourcegraph` in your local directory.</li>
                                 <li className="ml-2.5 text-xs leading-7">
-                                    The app will automatically detect code in the directory you run it in.
+                                    The app will detect code in the directory you run it in.
                                 </li>
                                 <li className="ml-2.5 text-xs leading-7">
                                     Your browser will automatically open localhost:3080.
