39f3fa3c17f60c27adf3050ccdd0f183a0e1d9b4
Marek <marek@sourcegraph.com>
2023-03-15 11:03:50 -0400

    Add eventLogger to track download link clicks and code copy events (#6066)
    Co-authored-by: Philipp Spiess <hello@philippspiess.com>

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -4,19 +4,25 @@ import classNames from 'classnames'
 import CheckCircleIcon from 'mdi-react/CheckCircleIcon'
 import ContentCopyIcon from 'mdi-react/ContentCopyIcon'
 
+import { logCodeSnippetCopied } from '../hooks/eventLogger'
 import { copy } from '../lib/utils'
 
 interface CodeSnippetProps {
     code: string
+    snippetName?: string
     className?: string
 }
 
-export const CodeSnippet: FunctionComponent<CodeSnippetProps> = ({ code, className }) => {
+export const CodeSnippet: FunctionComponent<CodeSnippetProps> = ({ snippetName, code, className }) => {
     const [copied, setCopied] = useState(false)
     const handleCopy = (): void => {
         copy(code)
             .then(() => setCopied(true))
             .catch(() => setCopied(false))
+
+        if (snippetName) {
+            logCodeSnippetCopied(snippetName)
+        }
     }
 
     const Icon = copied ? CheckCircleIcon : ContentCopyIcon
