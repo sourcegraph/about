a43ee6e8353caf8239be39e22b74e14cf0997fd2
Marek <marek@sourcegraph.com>
2023-03-16 15:57:24 -0400

    Fix script contents

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -105,23 +105,18 @@ export default class MyDocument extends Document {
 
 \                   {/* LinkedIn Insights Tag */}
                     <Script id="linkedin-insights-tag" type="text/javascript" strategy="afterInteractive">
-                        {`<script type="text/javascript">
-                        _linkedin_partner_id = "${linkedinPartnerId}";
-                        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-                        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-                        </script><script type="text/javascript">
-                        (function(l) {
-                        if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-                        window.lintrk.q=[]}
-                        var s = document.getElementsByTagName("script")[0];
-                        var b = document.createElement("script");
-                        b.type = "text/javascript";b.async = true;
-                        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                        s.parentNode.insertBefore(b, s);})(window.lintrk);
-                        </script>
-                        <noscript>
-                        <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=${linkedinPartnerId}&fmt=gif" />
-                        </noscript>
+                        {`
+                            _linkedin_partner_id = "${linkedinPartnerId}";
+                            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                            (function(l) {
+                            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                            window.lintrk.q=[]}
+                            var s = document.getElementsByTagName("script")[0];
+                            var b = document.createElement("script");
+                            b.type = "text/javascript";b.async = true;
+                            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                            s.parentNode.insertBefore(b, s);})(window.lintrk);
                         `}
                     </Script>
 
