d6e14c24504e923f10b404cecde2adcc494217ac
gitstart-sourcegraph <sourcegraph@gitstart.com>
2023-03-20 15:43:38 +0000

    chore: adds bottom border gradient on banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'mdx/types'
 import Link from 'next/link'
 
 export const Banner: FunctionComponent<{}> = () => (
-    <div className="z-[1000] bg-black py-[13px] sg-border-gradient-banner">
+    <div className="z-[1000] bg-black py-[13px] border-b-1 sg-border-gradient-banner">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
                 Join us March 23rd for our Starship launch event
