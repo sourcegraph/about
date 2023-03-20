cf61b7351e3fb532b8b47f037e73f9abf65d2280
Alex Isken <alex.isken@sourcegraph.com>
2023-03-20 11:15:50 -0500

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="z-[1000] bg-black py-[13px] border-b-1 sg-border-gradient-banner">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
-                Join us March 23rd for our Starship launch event
+                Join us on March 23rd for our Starship launch event
             </p>
             <Link
                 href="/starship"
