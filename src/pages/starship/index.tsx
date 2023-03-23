64e9c0412dbb49f25784d615bde56b7182ff60de
Alex Isken <alex.isken@sourcegraph.com>
2023-03-23 15:25:46 -0500

    Updating the starship OG image

--- src/pages/starship/index.tsx
+++ src/pages/starship/index.tsx
@@ -62,7 +62,7 @@ const Starship: FunctionComponent = () => (
         childrenClassName="sg-bg-gradient-starship"
         displayChildrenUnderNav={true}
         meta={{
-            image: '/starship/starship_og.png',
+            image: '/starship/starship_postlaunch_og.png',
             title: 'Code intelligence + AI = 🤯',
             description:
                 'Sourcegraph 5.0, the latest release of our code intelligence platform, is now available. Watch the livestream.',
