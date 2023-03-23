acfd9f16e24887938bd688a0974961708e4450f7
Alex Isken <alex.isken@sourcegraph.com>
2023-03-23 11:58:46 -0500

    Update the site banner for Starship being live (#6102)
    * Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,14 +6,14 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="z-[1000] bg-black py-[13px] border-b-1 sg-border-gradient-banner">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
-                Join us on March 23rd for our Starship launch event
+                Learn about AI + code intelligence from leaders in the space
             </p>
             <Link
-                href="/starship"
-                title="What’s coming"
+                href="https://www.twitch.tv/sourcegraph"
+                title="Watch live on Twitch"
                 className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
             >
-                See what’s coming
+                Watch live on Twitch
                 <ChevronRightIcon className="!mb-0 ml-[18px] inline" />
             </Link>
         </div>
