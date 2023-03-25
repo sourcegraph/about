8a6bbc7315fafac7576176f982ffd8562460f830
Quinn Slack <quinn@slack.org>
2023-03-25 13:10:32 -0700

    misc post-starship updates
    - link to cody up top
    - make cody wording consistent
    - etc

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,20 +3,26 @@ import { FunctionComponent } from 'mdx/types'
 import Link from 'next/link'
 
 export const Banner: FunctionComponent<{}> = () => (
-    <div className="z-[1000] bg-black py-[13px] border-b-1 sg-border-gradient-banner">
+    <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
-
-                Sourcegraph 5.0 and Cody have arrived
+                We just shipped{' '}
+                <Link className="font-semibold text-violet-300" href="/blog/release/5.0">
+                    Sourcegraph 5.0
+                </Link>{' '}
+                and{' '}
+                <Link className="font-semibold text-violet-300" href="/cody">
+                    Cody
+                </Link>
+                .
             </p>
             <Link
                 href="/starship"
                 title="See what’s new"
-                className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
+                className="btn flex items-center bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
             >
-                See what’s new
-
-                <ChevronRightIcon className="!mb-0 ml-[18px] inline" />
+                Join upcoming dev talks
+                <ChevronRightIcon className="!mb-0 ml-2 inline" />
             </Link>
         </div>
     </div>
