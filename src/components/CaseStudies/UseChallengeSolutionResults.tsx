da385d1f08ce5832d55ed0598ac2161293e9bf88
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2023-05-01 10:52:55 -0400

    Fix formatting issues

--- src/components/CaseStudies/UseChallengeSolutionResults.tsx
+++ src/components/CaseStudies/UseChallengeSolutionResults.tsx
@@ -1,9 +1,8 @@
-import { FunctionComponent, useEffect, useRef, useState } from 'react'
+import { FunctionComponent } from 'react'
 
 import AlertIcon from 'mdi-react/AlertIcon'
 import ChartLineVariantIcon from 'mdi-react/ChartLineVariantIcon'
 import CheckCircleIcon from 'mdi-react/CheckCircleIcon'
-import ClipboardTextIcon from 'mdi-react/ClipboardTextIcon'
 import Link from 'next/link'
 
 interface TextLink {
@@ -22,90 +21,88 @@ interface ListItemProps {
     item: TextLink
 }
 
-const isString = (value: string | TextLink): boolean => typeof value === 'string'
-
 const ListItemType: FunctionComponent<ListItemProps> = ({ item }) => (
     <li className="py-1">{item?.href ? <Link href={item.href}>{item.text}</Link> : <span>{item.text}</span>}</li>
 )
 
-export const UseChallengeSolutionResults: FunctionComponent<Props> = ({ useCases, challenges, solutions, results }) => {
-    return (
-        <div className="relative grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <div className="flex items-center lg:rounded-lg lg:border lg:border-gray-300 lg:p-8">
-                <div>
-                    <h4 className="mb-2 text-2xl lg:text-3xl leading-[1]">Use case</h4>
-                    {useCases.length > 1 ? (
-                        <ul className="mb-0 ml-0 pl-sm">
-                            {useCases.map(useCase => (
-                                <ListItemType key={useCase.text} item={useCase} />
-                            ))}
-                        </ul>
-                    ) : useCases[0].href ? (
-                        <p className="mb-0 text-3xl lg:text-4xl">
-                            <Link href={useCases[0].href} className="font-semibold">{useCases[0].text}</Link>
-                        </p>
-                    ) : (
-                        <span>{useCases[0].text}</span>
-                    )}
-                </div>
-            </div>
-            <div className="flex flex-col gap-4 lg:mb-0 lg:flex-row border rounded-lg border-gray-300 p-5 md:p-8">
-                <div className="flex max-w-[50px] items-center justify-center self-start rounded bg-violet-100 p-1 text-center">
-                    <AlertIcon size={40} className="p-1 text-violet-400" />
-                </div>
-                <div>
-                    <h4>Challenge</h4>
-                    {challenges.length > 1 ? (
-                        <ul className="mb-0 ml-0 pl-sm">
-                            {challenges.map(challenge => (
-                                <ListItemType key={challenge.text} item={challenge} />
-                            ))}
-                        </ul>
-                    ) : challenges[0].href ? (
-                        <Link href={challenges[0].href}>{challenges[0].text}</Link>
-                    ) : (
-                        <span>{challenges[0].text}</span>
-                    )}
-                </div>
-            </div>
-            <div className="flex flex-col gap-4 lg:mb-0 lg:flex-row border rounded-lg border-gray-300 p-5 md:p-8">
-                <div className="flex max-w-[50px] items-center justify-center self-start rounded bg-violet-100 p-1 text-center">
-                    <CheckCircleIcon size={40} className="p-1 text-violet-400" />
-                </div>
-                <div>
-                    <h4>Solution</h4>
-                    {solutions.length > 1 ? (
-                        <ul className="mb-0 ml-0 pl-sm">
-                            {solutions.map(solution => (
-                                <ListItemType key={solution.text} item={solution} />
-                            ))}
-                        </ul>
-                    ) : solutions[0].href ? (
-                        <Link href={solutions[0].href}>{solutions[0].text}</Link>
-                    ) : (
-                        <span>{solutions[0].text}</span>
-                    )}
-                </div>
-            </div>
-            <div className="flex flex-col gap-4 lg:ml-0 lg:flex-row sg-bg-gradient-venus p-8 border rounded-lg border-gray-300 p-5 md:p-8">
-                <div className="flex max-w-[50px] items-center justify-center self-start rounded bg-violet-100 p-1 text-center">
-                    <ChartLineVariantIcon size={40} className="p-1 text-violet-400" />
-                </div>
-                <div>
-                    <h4>Results</h4>
-                    {results.length > 1 ? (
-                        <ul className="mb-0 ml-0 pl-6">
-                            {results.map(result => (
-                                <ListItemType key={result.text} item={result} />
-                            ))}
-                        </ul>
-                    ) : results[0].href ? (
-                        <Link href={results[0].href}>{results[0].text}</Link>
-                    ) : (
-                        <span>{results[0].text}</span>
-                    )}
-                </div>
+export const UseChallengeSolutionResults: FunctionComponent<Props> = ({ useCases, challenges, solutions, results }) => (
+    <div className="relative grid grid-cols-1 gap-8 lg:grid-cols-2">
+        <div className="flex items-center lg:rounded-lg lg:border lg:border-gray-300 lg:p-8">
+            <div>
+                <h4 className="mb-2 text-2xl leading-[1] lg:text-3xl">Use case</h4>
+                {useCases.length > 1 ? (
+                    <ul className="mb-0 ml-0 pl-sm">
+                        {useCases.map(useCase => (
+                            <ListItemType key={useCase.text} item={useCase} />
+                        ))}
+                    </ul>
+                ) : useCases[0].href ? (
+                    <p className="mb-0 text-3xl lg:text-4xl">
+                        <Link href={useCases[0].href} className="font-semibold">
+                            {useCases[0].text}
+                        </Link>
+                    </p>
+                ) : (
+                    <span>{useCases[0].text}</span>
+                )}
             </div>
         </div>
-    )
-}
+        <div className="flex flex-col gap-4 rounded-lg border border-gray-300 p-5 md:p-8 lg:mb-0 lg:flex-row">
+            <div className="flex max-w-[50px] items-center justify-center self-start rounded bg-violet-100 p-1 text-center">
+                <AlertIcon size={40} className="p-1 text-violet-400" />
+            </div>
+            <div>
+                <h4>Challenge</h4>
+                {challenges.length > 1 ? (
+                    <ul className="mb-0 ml-0 pl-sm">
+                        {challenges.map(challenge => (
+                            <ListItemType key={challenge.text} item={challenge} />
+                        ))}
+                    </ul>
+                ) : challenges[0].href ? (
+                    <Link href={challenges[0].href}>{challenges[0].text}</Link>
+                ) : (
+                    <span>{challenges[0].text}</span>
+                )}
+            </div>
+        </div>
+        <div className="flex flex-col gap-4 rounded-lg border border-gray-300 p-5 md:p-8 lg:mb-0 lg:flex-row">
+            <div className="flex max-w-[50px] items-center justify-center self-start rounded bg-violet-100 p-1 text-center">
+                <CheckCircleIcon size={40} className="p-1 text-violet-400" />
+            </div>
+            <div>
+                <h4>Solution</h4>
+                {solutions.length > 1 ? (
+                    <ul className="mb-0 ml-0 pl-sm">
+                        {solutions.map(solution => (
+                            <ListItemType key={solution.text} item={solution} />
+                        ))}
+                    </ul>
+                ) : solutions[0].href ? (
+                    <Link href={solutions[0].href}>{solutions[0].text}</Link>
+                ) : (
+                    <span>{solutions[0].text}</span>
+                )}
+            </div>
+        </div>
+        <div className="sg-bg-gradient-venus flex flex-col gap-4 rounded-lg border border-gray-300 p-8 p-5 md:p-8 lg:ml-0 lg:flex-row">
+            <div className="flex max-w-[50px] items-center justify-center self-start rounded bg-violet-100 p-1 text-center">
+                <ChartLineVariantIcon size={40} className="p-1 text-violet-400" />
+            </div>
+            <div>
+                <h4>Results</h4>
+                {results.length > 1 ? (
+                    <ul className="mb-0 ml-0 pl-6">
+                        {results.map(result => (
+                            <ListItemType key={result.text} item={result} />
+                        ))}
+                    </ul>
+                ) : results[0].href ? (
+                    <Link href={results[0].href}>{results[0].text}</Link>
+                ) : (
+                    <span>{results[0].text}</span>
+                )}
+            </div>
+        </div>
+    </div>
+)
