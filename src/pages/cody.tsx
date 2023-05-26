500e2c64d93cb7437eea84e838e9af4213ae042c
Eric Brody-Moore <brodymoore.eric@gmail.com>
2023-05-26 16:46:12 -0400

    Fixup to lowecase

--- src/pages/cody.tsx
+++ src/pages/cody.tsx
@@ -72,7 +72,7 @@ const codyFeatures2 = [
     {
         heading: 'Code fixup',
         description:
-            'Cody edits and improves code directly using inline instructions. Simply type what you want Cody to do above or below a block of Cody and hit the Fixup hotkey; Cody will directly edit that code within your editor, saving you the need to copy and paste code from the chat.',
+            'Cody edits and improves code directly using inline instructions. Simply type what you want Cody to do above or below a block of Cody and hit the fixup hotkey; Cody will directly edit that code within your editor, saving you the need to copy and paste code from the chat.',
     },
     {
         heading: 'Unit tests',
