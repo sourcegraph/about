79e99e05e1e010477b92b07543f0249df34d3e8e
Alex Isken <alex.isken@sourcegraph.com>
2023-06-08 10:59:25 -0500

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
-                Cody users connected to Sourcegraph.com now get 1,000 completions per day for free
+                Cody users on Sourcegraph.com now get 1,000 completions per day for free
             </p>
             <Link
                 href="/blog/increasing-the-completions-rate-limit"
