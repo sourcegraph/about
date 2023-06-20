03bc97515e7f244ddea4222663abfc41d679db0f
Taylor Sperry <taylor.sperry@sourcegraph.com>
2023-06-20 08:47:53 -0600

    Add 6-16 app link (#6321)

--- src/data/downloads.ts
+++ src/data/downloads.ts
@@ -1,4 +1,4 @@
-const appVersionString = 'v2023.6.13%2B1311.1af08ae774/Sourcegraph_2023.6.13+1311.1af08ae774_aarch64.dmg'
+const appVersionString = 'v2023.6.16%2B1314.6c2d49d47c/Cody_2023.6.16+1314.6c2d49d47c_aarch64.dmg'
 
 const appDownloadPrefix = 'https://github.com/sourcegraph/sourcegraph/releases/download/app-'
 
