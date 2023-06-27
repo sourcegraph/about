d31ddaba76b7ec88ebe69e5a7b9b434ae8b18889
Paulo Almeida <eu@paulo.digital>
2023-06-27 18:08:42 -0300

    Don't trigger every time enter viewport

--- src/hooks/useInView.ts
+++ src/hooks/useInView.ts
@@ -1,12 +1,17 @@
-import { useState, useEffect, RefObject } from 'react'
+import { useState, useEffect, useRef, RefObject } from 'react'
 
-export const useInView = (elementRef: RefObject<Element>, customThreshold?: number): boolean => {
+export const useInView = (elementRef: RefObject<HTMLElement>, customThreshold?: number): boolean => {
     const [isInView, setIsInView] = useState(false)
+    // Make sure that the animaton don't trigger every time the element enter the viewport
+    const hasAppared = useRef(false)
 
     useEffect(() => {
         const observer = new IntersectionObserver(
             ([entry]) => {
-                setIsInView(entry.isIntersecting)
+                if (!hasAppared.current && entry.isIntersecting) {
+                    setIsInView(true)
+                    hasAppared.current = true
+                }
             },
             { threshold: customThreshold || 1 }
         )
