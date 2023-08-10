2fa12fcda3d4349bb50655a8e763acb74d0a33a2
gitstart-sourcegraph <sourcegraph@gitstart.com>
2023-08-10 18:28:29 +0000

    chore: Improve cloud page formatting

--- src/pages/cloud.tsx
+++ src/pages/cloud.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from 'react'
 
+import Link from 'next/link'
 import { FaGitAlt, FaLocationArrow } from 'react-icons/fa'
 import { MdOutlineAvTimer, MdBarChart } from 'react-icons/md'
 
-import { Layout, Hero, ContentSection, CallToActionContentSection, TwoColumnSection } from '../components'
-import { StandardCallToAction } from '../components/cta/StandardCallToAction'
-import { buttonLocation } from '../data/tracking'
+import { Layout, Hero, ContentSection, TwoColumnSection, Heading } from '../components'
+import { buttonLocation, buttonStyle } from '../data/tracking'
 
 const Cloud: FunctionComponent = () => (
     <Layout
@@ -19,42 +19,67 @@ const Cloud: FunctionComponent = () => (
             <Hero
                 product="sourcegraph cloud"
                 variant="radialSpace"
+                className='md:!pt-[152px] md:h-[805px] lg:!mb-0'
                 title="Find, understand, & fix code faster than ever"
-                titleClassName="text-transparent block bg-clip-text bg-gradient-to-l from-white/10 to-violet-200"
+                titleClassName="text-transparent block bg-clip-text bg-gradient-to-l sg-bg-gradient-infrared bg-clip-text text-transparent mt-4"
                 subtitle="Sourcegraph Cloud’s dedicated, single-tenant SaaS solution is the easiest way to get Sourcegraph in the cloud. Get full code intelligence for your codebase quickly, securely, and without having to host."
                 centerContent={true}
-                cta={<StandardCallToAction buttonLocation={buttonLocation.hero} center={true} dark={true} />}
+                cta={
+                  <div className='mx-auto flex w-full flex-col items-center gap-6 md:gap-4 sm:flex sm:flex-row justify-center'>
+                    <Link 
+                      className='btn whitespace-nowrap btn-inverted-primary min-w-[135px]'
+                      href="/contact/request-info"
+                      title='Get free trial'
+                      data-button-style={buttonStyle.primary}
+                      data-button-location={buttonLocation}
+                      data-button-type="cta"
+                    >
+                      Get free trial
+                    </Link>
+                    <Link 
+                      className='btn inline-flex items-center whitespace-nowrap btn-outline-white min-w-[135px]'
+                      href="/get-started"
+                      title='Get started'
+                      data-button-style={buttonStyle.outline}
+                      data-button-location={buttonLocation.hero}
+                      data-button-type="cta"
+                    >
+                      Get started
+                    </Link>
+                  </div>
+                }
                 floatingImg="/code-intelligence-platform-illustration.svg"
                 displayUnderNav={true}
             />
         }
     >
-        <ContentSection background="white">
+        <ContentSection background="white" parentClassName='md:!pt-28 md:!pb-5xl lg:!pt-5xl'>
             <div className="flex-row lg:flex">
-                <h2 className="mb-md min-w-[400px] max-w-3xl lg:basis-1/3">
+                <h2 className="mb-10 md:min-w-[400px] max-w-3xl lg:basis-1/3 md:pr-4 md:mr-6">
                     Get in flow with a private, fully-featured Sourcegraph instance
                 </h2>
 
                 <div className="max-w-full basis-2/3">
                     <TwoColumnSection
                         blockOnMdAndDown={true}
+                        className='md:!gap-x-6 !gap-y-8'
                         leftColumn={
-                            <div className="mb-md">
-                                <div className="mb-5 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
+                            <div>
+                                <div className="mb-6 md:mb-5 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
                                     <FaGitAlt size={32} className="text-violet-400" />
                                 </div>
-                                <p className="max-w-sm">
+                                <p className="m-0">
                                     Search all your code, all in one place. Sourcegraph connects to any Git-based code
                                     host so you can view your entire codebase in a single unified view.
                                 </p>
                             </div>
                         }
                         rightColumn={
-                            <div className="mb-md">
-                                <div className="mb-5 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
+                            <div className='mb-8'>
+                                <div className="mb-6 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
                                     <FaLocationArrow size={32} className="p-1 text-violet-400" />
                                 </div>
-                                <p className="max-w-sm">
+                                <p className="m-0">
                                     Navigate your codebase with IDE-level code navigation that works across
                                     repositories, so you can track references, definitions, and implementations with
                                     perfect accuracy.
@@ -64,25 +89,26 @@ const Cloud: FunctionComponent = () => (
                     />
                     <TwoColumnSection
                         blockOnMdAndDown={true}
+                        className='md:!gap-x-6  !gap-y-8'
                         leftColumn={
                             <div>
-                                <div className="mb-5 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
+                                <div className="mb-6 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
                                     <MdOutlineAvTimer size={45} className="text-violet-400" />
                                 </div>
-                                <p className="max-w-sm">
-                                    Fix and update code across your entire codebase in hours rather than weeks with
-                                    Batch Changes.
+                                <p className="m-0">
+                                    Fix and update code across your entire codebase in hours rather than weeks with{' '}
+                                    <Link href='/batch-changes' className='text-black underline'>Batch Changes</Link>.
                                 </p>
                             </div>
                         }
                         rightColumn={
                             <div>
-                                <div className="mb-5 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
+                                <div className="mb-6 flex max-h-[48px] max-w-[48px] items-center rounded bg-violet-100 p-2.5">
                                     <MdBarChart size={32} className="text-violet-400" />
                                 </div>
-                                <p className="max-w-sm">
-                                    Track migrations, measure goals, and visualize changes in your code with Code
-                                    Insights.
+                                <p className="m-0">
+                                    Track migrations, measure goals, and visualize changes in your code with{' '}
+                                    <Link href='/code-insights' className='text-black underline'>Code Insights</Link>.
                                 </p>
                             </div>
                         }
@@ -91,43 +117,55 @@ const Cloud: FunctionComponent = () => (
             </div>
         </ContentSection>
 
-        <ContentSection background="white">
-            <h2>Start searching and stay in flow, no maintenance necessary</h2>
-            <ul className="ml-md mt-sm">
-                <li>
-                    Get a dedicated Sourcegraph Cloud instance with no manual deployment required. Simply connect to
-                    your code hosts and start searching, navigating, and automating your code.
-                </li>
-
-                <li>
-                    Spend less time maintaining your dev tools and more time shipping great code. Upgrades and scaling
-                    are fully automated.
-                </li>
-
-                <li>Reduce your total cost of ownership and increase time to value.</li>
-            </ul>
+        <ContentSection background="white" className='md:py-4' parentClassName='!py-0'>
+            <TwoColumnSection
+                className='!gap-y-16 lg:!gap-x-16'
+                leftColumn={
+                    <div className='mr-[38px] lg:mr-0'>
+                      <h2>Start searching and stay in flow, no maintenance necessary</h2>
+                      <ul className="ml-0 mt-6 list-none lg:max-w-[590px]">
+                          <li>
+                              Get a dedicated Sourcegraph Cloud instance with no manual deployment required. Simply
+                              connect to your code hosts and start searching, navigating, and automating your code.
+                          </li>
+                      </ul>
+                    </div>
+                }
+                rightColumn={
+                    <div className='mr-[38px] lg:mr-0'>
+                      <h2>Reduce your total cost of ownership and increase time to value</h2>
+                      <ul className="ml-0 mt-6 list-none lg:max-w-[590px]">
+                          <li>
+                              Spend less time maintaining your dev tools and more time shipping great code. Upgrades
+                              and scaling are fully automated.
+                          </li>
+                      </ul>
+                    </div>
+                }
+            />
         </ContentSection>
 
         <ContentSection background="white">
             <TwoColumnSection
                 reverseOnMobile={true}
                 centerContent={true}
+                className='gap-y-[101px] lg:!gap-x-[83px]'
                 leftColumn={
                     <div className="flex justify-center">
-                        <span className="mt-[40px] h-px w-1/5 bg-gray-300" />
-                        <div className="ml-sm text-center">
+                        <span className="mt-[30px] md:mt-[46px] h-px w-10 md:w-20 bg-gray-300" />
+                        <div className="mx-[33px] text-center">
                             <img src="/security/ccpa.svg" alt="CCPA badge" />
-                            <p className="mt-xxs mb-0 text-gray-400">CCPA</p>
+                            <p className="mt-xxs mb-0 text-gray-400 font-semibold">CCPA</p>
                         </div>
-                        <div className="mx-sm text-center">
+                        <div className="mr-[36.5px] text-center">
                             <img src="/security/gdpr.svg" alt="GDPR badge" />
-                            <p className="mt-xxs mb-0 text-gray-400">GDPR</p>
+                            <p className="mt-xxs mb-0 text-gray-400 font-semibold">GDPR</p>
                         </div>
-                        <div className="mr-sm text-center">
+                        <div className="mr-[36.5px] text-center">
                             <img src="/security/soc2.svg" alt="SOC2 Type 2 badge" />
-                            <p className="mt-xxs mb-0 text-gray-400">SOC2 TYPE 2</p>
+                            <p className="mt-xxs mb-0 text-gray-400 font-semibold">SOC2 TYPE 2</p>
                         </div>
-                        <span className="mt-[40px] h-px w-1/5 bg-gray-300" />
+                        <span className="mt-[30px] md:mt-[46px] h-px w-10 md:w-20 bg-gray-300" />
                     </div>
                 }
                 rightColumn={
@@ -144,9 +182,9 @@ const Cloud: FunctionComponent = () => (
                                 and restore capability.
                             </li>
 
-                            <li>
+                            <li> 
                                 Prioritize your data’s privacy: Sourcegraph Cloud has received a SOC 2 Type II report.
-                                See our security portal for more information.
+                                See our <a href='https://security.sourcegraph.com/' target="_blank" rel="noreferrer" className='text-black underline'>security portal</a> for more information.
                             </li>
                         </ul>
                     </>
@@ -154,7 +192,27 @@ const Cloud: FunctionComponent = () => (
             />
         </ContentSection>
 
-        <CallToActionContentSection />
+        <ContentSection background="white" parentClassName="!p-0 !pt-[5px] md:!pt-4 md:!pb-28">
+            <div className="sg-bg-code-search-cta mx-auto flex w-full max-w-[941px] flex-col md:items-center justify-between gap-x-8 rounded-0 bg-no-repeat py-16 px-6 shadow-cta md:rounded-2xl md:bg-[lightgray] md:bg-[url('/code-search/cta-background.png')] md:bg-cover md:bg-right md:py-24 md:px-20 lg:flex-row">
+                <div className="max-w-[516px] pb-8 md:pb-0">
+                    <Heading size="h2" className="mb-6 !text-4xl text-white">
+                        Try Sourcegraph on your code
+                    </Heading>
+                    <p className="max-w-2xl mb-0 text-lg text-gray-200">
+                        Experience code intelligence with a free trial for you and your team, or search millions of open
+                        source repositories.
+                    </p>
+                </div>
+                <div className="flex w-full flex-col items-start justify-start gap-4 md:w-fit md:flex-row md:items-center md:justify-center">
+                    <Link
+                      className='btn min-w-fit text-center px-6 lg:px-4 btn-inverted-primary text-violet-500'
+                      href="/demo"
+                    >
+                        Meet with a product expert
+                    </Link>
+                </div>
+            </div>
+        </ContentSection>
     </Layout>
 )
 
