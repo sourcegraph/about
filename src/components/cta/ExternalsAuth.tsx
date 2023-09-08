bc644572086aa37def806f7d06f2275d22918c21
rob rhyne <rob@sourcegraph.com>
2023-09-08 14:56:29 -0400

    fix logo

--- src/components/cta/ExternalsAuth.tsx
+++ src/components/cta/ExternalsAuth.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 import classNames from 'classnames'
 import Cookies from 'js-cookie'
 import GithubIcon from 'mdi-react/GithubIcon'
-import GoogleColorIcon from './GoogleColorIcon'
 import Link from 'next/link'
 
 import { EventName, getEventLogger } from '../../hooks/eventLogger'
@@ -53,7 +52,33 @@ const GitlabColorIcon: React.FunctionComponent<React.PropsWithChildren<{ classNa
 )
 
 const GoogleColorIcon: React.FunctionComponent<React.PropsWithChildren<{ className?: string }>> = ({ className }) => (
-    // need to fill this in
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="46"
+      height="46"
+      viewBox="0 0 46 46"
+    >
+      <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+        <g transform="translate(-1 -1)">
+          <g filter="url(#filter-1)" transform="translate(4 4)">
+            <g>
+              <use fill="#FFF" xlinkHref="#path-2" />
+              <use xlinkHref="#path-2" />
+              <use xlinkHref="#path-2" />
+              <use xlinkHref="#path-2" />
+            </g>
+          </g>
+          <g transform="translate(15 15)">
+            <path fill="#4285F4" d="M17.64 9.205c0-.639-.057-1.252-.164-1.841H9v3.481h4.844a4.14 4.14 0 01-1.796 2.716v2.259h2.908c1.702-1.567 2.684-3.875 2.684-6.615z" />
+            <path fill="#34A853" d="M9 18c2.43 0 4.467-.806 5.956-2.18l-2.908-2.259c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 009 18z"/>
+            <path fill="#FBBC05" d="M3.964 10.71A5.41 5.41 0 013.682 9c0-.593.102-1.17.282-1.71V4.958H.957A8.996 8.996 0 000 9c0 1.452.348 2.827.957 4.042l3.007-2.332z" />
+            <path fill="#EA4335" d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0A8.997 8.997 0 00.957 4.958L3.964 7.29C4.672 5.163 6.656 3.58 9 3.58z" />
+            <path d="M0 0h18v18H0V0z" />
+          </g>
+        </g>
+      </g>
+    </svg>
 )
 
 export const ExternalsAuth: React.FunctionComponent<ExternalsAuthProps> = ({
@@ -123,23 +148,27 @@ export const ExternalsAuth: React.FunctionComponent<ExternalsAuthProps> = ({
                 {label}
             </Link>
         );
-    } else if (authProvider === 'google') {
+    }
+
+    if (authProvider === 'google') {
         return (
             <Link
                 href="https://sourcegraph.com/.auth/openidconnect/login?pc=google"
                 className={classNames(
-                    `btn hover:sg-bg-hover-external-auth-button flex items-center px-4 hover:text-black md:h-12 md:px-6 md:text-lg 
+                    `btn hover:sg-bg-hover-external-auth-button flex items-center pl-3 pr-3 hover:text-black md:h-12 md:text-lg 
                     ${dark ? 'sg-gitlab-bg-color hover:btn-primary text-white ' : 'btn-inverted-primary text-black' }`,
                     className
                 )}
                 onClick={handleOnClick}
                 id="googleButton"
-            >
-                <GoogleColorIcon className="pr-2" />
-                {label}
+            >   
+                <GoogleColorIcon className="pr-4" />
+                <span className="pr-2">{label}</span>
             </Link>
         );
-    } else {
+    }
+
+    if (authProvider === 'gitlab') {
         return (
             <Link
                 href="https://sourcegraph.com/.auth/gitlab/login?pc=https%3A%2F%2Fgitlab.com%2F%3A%3A262309265ae76179773477bd50c93c7022007a4810c344c69a7371da11949c48&redirect=/get-cody"
@@ -157,3 +186,4 @@ export const ExternalsAuth: React.FunctionComponent<ExternalsAuthProps> = ({
             </Link>
         );
     }
+}
