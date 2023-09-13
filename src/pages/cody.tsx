f96dd708238729b0329790b1ac62b919527a66fd
rob rhyne <rob@sourcegraph.com>
2023-09-13 16:34:26 -0400

    center text vertically and section horizontally

--- src/pages/cody.tsx
+++ src/pages/cody.tsx
@@ -177,10 +177,12 @@ const CodyPage: FunctionComponent = () => {
                 parentClassName="!py-0"
                 className="flex w-full flex-col items-center gap-x-12 gap-y-12 pt-16 md:flex-row md:px-8 md:pt-0"
             >
-                <Heading size="h6" className="whitespace-nowrap !text-lg text-gray-200">
-                    Cody is available for:
-                </Heading>
-                <div className="flex w-full flex-wrap justify-center gap-x-6 gap-y-8 py-4 md:justify-start md:gap-x-8">
+                <div className="flex w-full flex-wrap justify-center gap-x-6 gap-y-8 py-4 md:gap-x-8 h-32">
+                   <div className="flex flex-col h-full items-center justify-center">
+                    <Heading size="h6" className="whitespace-nowrap !text-lg text-gray-200">
+                        Cody is available for: 
+                    </Heading>
+                    </div>
                     <div className="flex items-center gap-x-4 md:px-6">
                         <img className="" src="/icons/intelliJ.svg" alt="IntelliJ IDE marketplace" />{' '}
                         <Heading size="h4" className="!text-2xl text-gray-200">
