1ff00466421f8bebcf2e481c628cf5b06da4c78e
s.shivasurya <s.shivasurya@gmail.com>
2023-09-19 19:05:35 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,2 +1,2 @@
-const token = "sgp_918cd77fcf425af89f0b6daf7471f6f590485b5b"
+const token = "sgp_d3d9c2f1997bb5e2392a9d8780ba4663753b9da1"
 // this is purely test token for testing - please dont waste your time checking this token
