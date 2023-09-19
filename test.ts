963a725fe4290ed69a415d590e5617f10ec21897
s.shivasurya <s.shivasurya@gmail.com>
2023-09-19 18:56:14 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,2 +1,2 @@
-const token = "sgp_78eeac3fdb9726828c7e492d883d4caf6c09aeb9"
+const token = "sgp_918cd77fcf425af89f0b6daf7471f6f590485b5b"
 // this is purely test token for testing - please dont waste your time checking this token
