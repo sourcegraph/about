93a7dd14b949afe5953f055d529a03ff08685092
Alex Isken <alex.isken@sourcegraph.com>
2023-10-03 15:08:00 -0500

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
-                Join our livestream on October 4 to learn about the latest Cody features
+                Join us at 12pm PT on October 4 as we unveil Cody's latest features
             </p>
             <Link
                 href="https://www.youtube.com/watch?v=IywQJ47XPE0"
