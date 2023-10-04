7963b36d040cb8c778a2efaf79d1618c39fd411c
Alex Isken <alex.isken@sourcegraph.com>
2023-10-04 17:32:31 -0500

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,14 +6,14 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 md:flex-row">
             <p className="mb-0 font-semibold leading-[22px] text-white">
-                Join us at 12pm PT on October 4 as we unveil Cody's latest features
+                The latest Cody release improves autocomplete performance by up to 2x
             </p>
             <Link
-                href="https://www.youtube.com/watch?v=IywQJ47XPE0"
+                href="https://about.sourcegraph.com/blog/feature-release-october-2023"
                 title="RSVP to the livestream"
                 className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
             >
-                RSVP to the livestream
+                Read the release blog
                 <ChevronRightIcon className="!mb-0 ml-[18px] inline" />
             </Link>
         </div>
