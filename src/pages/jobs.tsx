a8e51a0289391dd6dc4e357ce4cdef94ed14f8a9
gitstart-sourcegraph <sourcegraph@gitstart.com>
2023-11-13 19:24:12 +0000

    chore: Updates careers page

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent } from 'react'
 
-import ArrowRightIcon from 'mdi-react/ArrowRightIcon'
+import classNames from 'classnames'
+import Ticker from 'framer-motion-ticker'
+import ChevronRightIcon from 'mdi-react/ChevronRightIcon'
 
 import { Layout, ContentSection, ThreeUpText } from '../components'
 import { buttonStyle, buttonLocation } from '../data/tracking'
@@ -8,100 +10,54 @@ import { buttonStyle, buttonLocation } from '../data/tracking'
 const CareersPage: FunctionComponent = () => {
     const valueItems = [
         {
-            value: 'High quality work',
+            value: 'Being direct & transparent',
             description:
-                'You are responsible for finding out what high-quality work looks like and producing that high-quality work iteratively.',
+                'We communicate directly and operate transparently by default, which helps us move fast and make good decisions. We proactively give and receive candid feedback out of respect for each other.',
+            img: <img className="w-full max-w-[336px]" src="/careers/BeingDirect.svg" alt="Diamond illustration" />,
+        },
+        {
+            value: 'Winning together',
+            description:
+                'Too little collaboration causes chaos and brings disappointing results. Too much collaboration crushes ownership and dulls outcomes. We speak up when something is wrong (even when it’s hard) and help create an environment where we respectfully consider every voice.',
             img: (
-                <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/1highquality.svg"
-                    alt="Diamond illustration"
-                />
+                <img className="w-full max-w-[336px]" src="/careers/WinningTogether.svg" alt="Fist bump illustration" />
             ),
         },
         {
-            value: 'Being customer-first',
-            description: 'You earn and keep the trust of our customers by putting their interests first.',
+            value: 'High-agency',
+            description:
+                "If everyone else at Sourcegraph were stranded on a desert island without internet access for a month, you'd be able to figure it out and keep Sourcegraph running. Everyone here has a high degree of autonomy, which requires hard work, responsibility, fast iteration, and optimism in the face of challenges.",
             img: (
-                <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/2customerdriven.svg"
-                    alt="Fist bump illustration"
-                />
+                <img className="w-full max-w-[336px]" src="/careers/HighAgency.svg" alt="Tandem bicycle illustration" />
             ),
         },
         {
-            value: 'Working as a team',
+            value: 'Dev-love',
             description:
-                'You work collaboratively with your peers, cross-functional teammates, and leadership to create shared success, trust, and belonging.',
+                'Everything we do is ultimately for devs (anyone who codes). All Sourcegraph teammates code to better understand our users and customers and to do their job better. Our long-term mission is to make it so everyone codes, not just the 0.1% of the world’s population who are devs.',
             img: (
                 <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/3team.svg"
-                    alt="Tandem bicycle illustration"
-                />
-            ),
-        },
-        {
-            value: 'High agency',
-            description:
-                'You have the power and the responsibility to improve Sourcegraph as a company and as a product. You deliver regardless of the circumstances.',
-            img: (
-                <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/4highagency.svg"
+                    className="w-full max-w-[336px]"
+                    src="/careers/DevLove.svg"
                     alt="Stamp labeled 'First class' illustration"
-                />
-            ),
-        },
-        {
-            value: 'Being welcoming & inclusive',
-            description:
-                'You make people from all groups and backgrounds feel comfortable belonging to our team and community.',
-            img: (
-                <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/5welcoming.svg"
-                    alt="Head illustration with a heart in the center"
-                />
-            ),
-        },
-        {
-            value: 'Being open & transparent',
-            description: 'You proactively communicate in an open and transparent way.',
-            img: (
-                <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/6open.svg"
-                    alt="X-ray vision glasses illustration"
-                />
-            ),
-        },
-        {
-            value: 'Continuous growth',
-            description:
-                'You strive to continuously grow and learn by genuinely soliciting feedback early and often, and humbly reflecting on your past mistakes.',
-            img: (
-                <img
-                    className="mt-10 w-full max-w-[500px]"
-                    src="/careers/7growth.svg"
-                    alt="Tall giraffe illustration"
                 />
             ),
         },
     ]
 
+    const carouselImages = ['/careers/Career1.jpg', '/careers/Career2.jpg', '/careers/Career3.jpg']
+
     return (
         <Layout className="jobs-page" headerColorTheme="white">
-            <ContentSection background="white">
-                <h1 className="mb-xs">Let's build the future together.</h1>
-                <h3>
-                    We're developing the world's most advanced code intelligence platform with brilliant dreamers around
-                    the globe.
+            <ContentSection background="white" className="xl:pl-6" parentClassName="!pt-8 !pb-[55px]">
+                <h1 className="mb-sm">Let's build the future together.</h1>
+                <h3 className="max-w-[820px]">
+                    We're developing the world's most advanced code AI platform with brilliant dreamers around the
+                    globe.
                 </h3>
                 <a
-                    className="btn btn-primary mt-6"
-                    href="https://grnh.se/c685479c4us"
+                    className="btn btn-primary mt-8"
+                    href="https://boards.greenhouse.io/sourcegraph91?gh_src=c685479c4us"
                     title="Explore our jobs"
                     target="_blank"
                     rel="nofollow noreferrer"
@@ -109,29 +65,29 @@ const CareersPage: FunctionComponent = () => {
                     data-button-location={buttonLocation.hero}
                     data-button-type="cta"
                 >
-                    Explore our jobs
-                    <ArrowRightIcon className="ml-4 inline" />
+                    Explore our open roles
+                    <ChevronRightIcon className="ml-2 inline" />
                 </a>
             </ContentSection>
 
-            <div className="bg-white text-center">
-                <img
-                    src="../careers/careers_hero_img.jpg"
-                    alt="Sourcegraph team members jumping"
-                    className="hidden w-full max-w-screen-2xl md:inline"
-                />
-                <img
-                    src="../careers/careers_hero_img_mobile.jpg"
-                    alt="Sourcegraph team members jumping"
-                    className="inline w-full md:hidden"
-                />
+            <div className="h-[477px]">
+                <Ticker duration={20}>
+                    {carouselImages.map((item, index) => (
+                        <img
+                            key={index}
+                            src={item}
+                            className="sg-career-carouel-image ml-[50px] h-[469px] rounded-2xl"
+                            alt="Career"
+                        />
+                    ))}
+                </Ticker>
             </div>
 
-            <ContentSection background="white">
+            <ContentSection background="white" parentClassName="!pt-[104px] !pb-16">
                 <div className="flex flex-wrap">
                     <div className="m-auto">
                         <h2 className="mx-auto mb-6 max-w-2xl text-center">Happy, healthy, and always in the flow.</h2>
-                        <p className="mx-auto text-center">
+                        <p className="mx-auto max-w-[768px] text-center">
                             Our teammates are our most valuable resource. That's why we provide total rewards that are
                             highly competitive and allow you to thrive both personally and professionally.
                         </p>
@@ -145,6 +101,7 @@ const CareersPage: FunctionComponent = () => {
                                 data-button-style={buttonStyle.text}
                                 data-button-location={buttonLocation.body}
                                 data-button-type="cta"
+                                className="underline"
                             >
                                 our handbook
                             </a>
@@ -154,41 +111,38 @@ const CareersPage: FunctionComponent = () => {
                 </div>
             </ContentSection>
 
-            <ContentSection>
+            <ContentSection parentClassName="!pt-0">
                 <ThreeUpText
-                    title="Benefits"
                     items={[
                         {
-                            icon: <img className="mb-2 sm:mx-auto" src="/careers/Remote.svg" alt="Map pin icon" />,
+                            icon: <img className="mx-auto mb-2" src="/careers/Remote.svg" alt="Map pin icon" />,
                             subtitle: 'Work fully remote',
                             description: 'Anywhere and at anytime',
                         },
                         {
-                            icon: <img className="mb-2 sm:mx-auto" src="/careers/PTO.svg" alt="Sun icon" />,
+                            icon: <img className="mx-auto mb-2" src="/careers/PTO.svg" alt="Sun icon" />,
                             subtitle: 'Unlimited PTO',
                             description: '...with a 30-day minimum',
                         },
                         {
-                            icon: <img className="mb-2 sm:mx-auto" src="/careers/Travel.svg" alt="Airplane icon" />,
+                            icon: <img className="mx-auto mb-2" src="/careers/Travel.svg" alt="Airplane icon" />,
                             subtitle: 'Generous travel budgets',
                             description: 'Meet your team across the globe',
                         },
                         {
-                            icon: <img className="mb-2 sm:mx-auto" src="/careers/Salary.svg" alt="Dollar bill icon" />,
+                            icon: <img className="mx-auto mb-2" src="/careers/Salary.svg" alt="Dollar bill icon" />,
                             subtitle: 'Competitive pay + equity',
                             description: 'So you can live your best life',
                         },
                         {
-                            icon: (
-                                <img className="mb-2 sm:mx-auto" src="/careers/Medical.svg" alt="Medical cross icon" />
-                            ),
+                            icon: <img className="mx-auto mb-2" src="/careers/Medical.svg" alt="Medical cross icon" />,
                             subtitle: 'Medical, dental, and vision',
                             description: 'Got you covered 100% (US only)',
                         },
                         {
                             icon: (
                                 <img
-                                    className="mb-2 sm:mx-auto"
+                                    className="mx-auto mb-2"
                                     src="/careers/ProfessionalDevelopment.svg"
                                     alt="Graduation cap icon"
                                 />
@@ -198,28 +152,20 @@ const CareersPage: FunctionComponent = () => {
                         },
                         {
                             icon: (
-                                <img
-                                    className="mb-2 sm:mx-auto"
-                                    src="/careers/OfficeBudget.svg"
-                                    alt="Headphones icon"
-                                />
+                                <img className="mx-auto mb-2" src="/careers/OfficeBudget.svg" alt="Headphones icon" />
                             ),
                             subtitle: 'Office budget',
                             description: 'Create a space where you thrive',
                         },
                         {
                             icon: (
-                                <img
-                                    className="mb-2 sm:mx-auto"
-                                    src="/careers/Wellness.svg"
-                                    alt="Person jogging icon"
-                                />
+                                <img className="mx-auto mb-2" src="/careers/Wellness.svg" alt="Person jogging icon" />
                             ),
                             subtitle: 'Wellness budget',
                             description: 'Take care of yourself on our dime',
                         },
                         {
-                            icon: <img className="mb-2 sm:mx-auto" src="/careers/FamilyPlanning.svg" alt="Baby icon" />,
+                            icon: <img className="mx-auto mb-2" src="/careers/FamilyPlanning.svg" alt="Baby icon" />,
                             subtitle: 'Family Planning benefits',
                             description: '$25K, because family comes first',
                         },
@@ -227,15 +173,28 @@ const CareersPage: FunctionComponent = () => {
                 />
             </ContentSection>
 
-            <ContentSection background="lightNebulousSaturn1">
-                <h2 className="mb-16 text-center">We value:</h2>
-                <div className="mx-auto grid max-w-6xl grid-cols-2 gap-lg lg:grid-cols-4">
-                    {valueItems.map(item => (
+            <div className="bg-violet-700">
+                <ContentSection parentClassName="!py-8" className="md:px-sm">
+                    <h1 className="text-white">We value...</h1>
+                </ContentSection>
+            </div>
+
+            <ContentSection parentClassName="!pt-16">
+                <div className="mx-auto flex flex-col items-center">
+                    {valueItems.map((item, index) => (
                         <div key={item.value}>
-                            <div className="flex min-h-[250px] flex-col items-center">
-                                <h4 className="">{item.value}</h4>
-                                <p className="text-center">{item.description}</p>
+                            <div
+                                className={classNames(
+                                    'flex items-center gap-x-24',
+                                    index % 2 === 0 ? 'flex-col md:flex-row' : 'flex-col md:flex-row-reverse',
+                                    index === 0 ? 'mb-16' : index === valueItems.length - 1 ? 'mb-0' : 'mb-[96px]'
+                                )}
+                            >
                                 {item.img}
+                                <div className="flex max-w-[578px] flex-col gap-6">
+                                    <h2>{item.value}</h2>
+                                    <p className="text-lg">{item.description}</p>
+                                </div>
                             </div>
                         </div>
                     ))}
@@ -243,29 +202,33 @@ const CareersPage: FunctionComponent = () => {
             </ContentSection>
 
             <ContentSection background="white">
-                <div className="mx-auto flex max-w-screen-xl flex-col items-center px-6 md:flex-row">
+                <div className="mx-auto flex max-w-screen-xl flex-col items-center px-6 md:flex-row md:gap-[113px]">
                     <div>
-                        <h2 className="mb-6">Our employees say we're a Great Place to Work&trade;</h2>
-                        <p>
+                        <h2 className="mb-[30px] md:max-w-[392px]">
+                            Our employees say we're a Great Place to Work&trade;
+                        </h2>
+                        <p className="md:max-w-[469px]">
                             Our employees have spoken! Our company culture is amazing and our Great Place to Work
                             Certification&trade; proves it.
                         </p>
                     </div>
                     <div className="flex max-h-[600px] justify-center">
                         <img
-                            className="w-full max-w-[450px]"
+                            className="w-full max-w-[577px] rounded-[5px]"
                             src="/careers/great-place-to-work.png"
-                            alt="Graphic portraying that 94% of employees at Sourcegraph say it is a great place to work compared to 57% of employees at a typical U.S.-based company."
+                            alt="Graphic portraying that 85% of employees at Sourcegraph say it is a great place to work compared to 57% of employees at a typical U.S.-based company."
                         />
                     </div>
                 </div>
             </ContentSection>
 
-            <ContentSection background="white">
+            <div className="sg-bg-careers-cta flex flex-col gap-4 py-24">
+                <h2 className="text-center text-white">Interested in working at Sourcegraph?</h2>
+                <p className="mb-0 text-center text-lg text-white">We are actively hiring for various roles.</p>
                 <div className="flex flex-col items-center text-center">
                     <a
-                        className="btn btn-primary my-2"
-                        href="https://grnh.se/c685479c4us"
+                        className="btn btn-inverted-primary"
+                        href="https://boards.greenhouse.io/sourcegraph91?gh_src=c685479c4us"
                         title="Explore our open roles"
                         target="_blank"
                         rel="nofollow noreferrer"
@@ -274,10 +237,10 @@ const CareersPage: FunctionComponent = () => {
                         data-button-type="cta"
                     >
                         Explore our open roles
-                        <ArrowRightIcon className="ml-4 inline" />
+                        <ChevronRightIcon className="ml-2 inline" />
                     </a>
                 </div>
-            </ContentSection>
+            </div>
         </Layout>
     )
 }
