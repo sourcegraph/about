5e116dc7dd2d7e2a0e8dc3ea5ae3f9e23bc02ce6
Chris Sev <chris.sev@hey.com>
2023-12-14 12:52:43 -0800

    updated cody screenshot

--- src/components/cody/CodyChat.tsx
+++ src/components/cody/CodyChat.tsx
@@ -21,12 +21,12 @@ export const CodyChat: FunctionComponent = () => {
             className="flex w-full  flex-col overflow-hidden border-gray-200 border-opacity-50 pt-[64px] md:rounded-2xl md:border md:bg-violet-700 md:pt-[72px] xl:max-w-[1280px]"
             parentClassName="!px-0 !pb-0"
         >
-            <div className="mx-6 md:ml-14 md:mb-32">
+            <div className="mx-6 md:mb-32 md:ml-14">
                 <div className="flex w-full max-w-[701px] flex-col gap-[18px]">
                     <img className="h-[48px] w-[48px]" src="/cody/chat-brand-icon.svg" alt="Cody Chat" />
                     <Heading
                         size="h2"
-                        className="m-0 text-left font-semibold leading-10 tracking-[-1px] text-white md:text-[40px] md:text-4xl"
+                        className="m-0 text-left font-semibold leading-10 tracking-[-1px] text-white md:text-4xl md:text-[40px]"
                     >
                         AI-powered chat for your code
                     </Heading>
@@ -40,26 +40,26 @@ export const CodyChat: FunctionComponent = () => {
             <div
                 className={`pb-0 pt-8 text-left md:ml-14 md:block md:flex-row md:pb-28  lg:flex lg:flex-col lg:items-center ${screenStyle.parent}`}
             >
-                <div className="mb-6 flex w-full flex-col md:mr-24">
+                <div className="mb-6 flex w-full flex-col">
                     <div className="flex max-w-[380px] flex-col justify-center gap-4 text-xl md:text-lg">
-                        <p className="px-6 md:px-[9px] text-sm font-[590] uppercase leading-[27px] text-gray-200">
+                        <p className="px-6 text-sm font-[590] uppercase leading-[27px] text-gray-200 md:px-[9px]">
                             Cody can answer questions like:
                         </p>
 
                         {answers.map(answer => (
                             <h5
                                 key={answer}
-                                className="font-lg m-0 px-6 md:px-2.5 py-2 font-[590] leading-[25px] tracking-[-0.25px]  text-white md:font-normal md:leading-[27px]"
+                                className="font-lg m-0 px-6 py-2 font-[590] leading-[25px] tracking-[-0.25px] text-white  md:px-2.5 md:font-normal md:leading-[27px]"
                             >
                                 {answer}
                             </h5>
                         ))}
                     </div>
                 </div>
-                <div className="w-full mx-6 md:mx-0">
+                <div className="mx-6 w-full md:mx-0">
                     <img
-                        className={`md:h-[522px] md:-top-16 self-stretch  -right-[128px] flex ${screenStyle.img}`}
-                        src="/cody/cody-chat-interface.svg"
+                        className={`-right-[100px] flex self-stretch shadow-md md:-top-16 md:h-[522px] xl:-right-[20px] ${screenStyle.img}`}
+                        src="/cody/cody-chat-interface-v2.png"
                         alt="Cody Chat interface"
                     />
                 </div>
