7899a45bd93ef38010085cec10621a84dd13c2f3
Alex Isken <alex.isken@sourcegraph.com>
2023-12-14 09:21:45 -0800

    Update Banner.tsx (#6636)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,14 +6,14 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 px-2 md:flex-row">
             <p className="mb-0 text-center font-semibold leading-[22px] text-white">
-                Public code search has moved! You can find it at sourcegraph.com/search.
+                Cody is now Generally Available, and Cody Pro is free until February 2024.
             </p>
             <Link
-                href="https://sourcegraph.com/search"
-                title="Public code search"
+                href="https://sourcegraph.com/blog/cody-is-generally-available"
+                title="Cody"
                 className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
             >
-                Go to public code search
+                Read the blog
                 <ChevronRightIcon className="!mb-0 ml-[18px] inline" />
             </Link>
         </div>
