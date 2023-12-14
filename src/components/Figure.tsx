377d48b5e8713d120a1ae7deacc1a6530426d115
Ado Kukic <kukicadnan@gmail.com>
2023-12-14 09:12:10 -0800

    fix lint error.

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -26,7 +26,7 @@ export const Figure: FunctionComponent<Figure> = ({
     <figure>
         <img src={src} alt={alt} title={alt} className={classNames({ 'mx-auto': centre, 'drop-shadow-xl': shadow })} />
         {caption && (
-            <figcaption className={"mt-3 " + (captionCenter ? 'text-center' : '')}>
+            <figcaption className={'mt-3 ' + (captionCenter ? 'text-center' : '')}>
                 {link ? (
                     <a href={link} className="flex not-italic">
                         {caption}
