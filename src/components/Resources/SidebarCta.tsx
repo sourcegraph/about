ffe652286a695440788d10cbb8d96d02afbbb941
Chris Sev <chris.sev@sourcegraph.com>
2024-02-13 14:44:18 -0800

    gitlab solution brief page (#6713)
    * gitlab solution brief page
    
    * sticky sidebar cta
    
    * linter fix
    
    * less chonky h2
    
    * renaming gitlab resource brief
    
    * Update copy for web form, add links
    
    ---------
    
    Co-authored-by: Alex Isken <alex.isken@sourcegraph.com>

---
+++ src/components/Resources/SidebarCta.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+import { Heading } from '../Heading'
+
+const SidebarCta: React.FC = () => (
+    <div className="sg-bg-code-search-cta mx-auto flex w-full flex-col rounded-lg py-12 px-6 text-center sm:items-center md:w-[378px] md:items-center md:text-left">
+        <Heading size="h2" className="pb-4 !text-4xl text-white ">
+            Try Sourcegraph on your code
+        </Heading>
+        <p className="pb-1 text-lg text-gray-200">
+            Experience code intelligence with a free trial for you and your team, or search millions of open source
+            repositories.
+        </p>
+        <Link
+            className="btn btn-inverted-primary flex min-w-fit self-center rounded-md bg-white py-3 px-5 text-center text-base text-violet-500 hover:!bg-violet-400 md:self-start md:py-2 md:px-6"
+            href="/demo"
+        >
+            Meet with a product expert
+        </Link>
+    </div>
+)
+
+export default SidebarCta
