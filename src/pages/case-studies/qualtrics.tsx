bcf1f79ce773ad9b11ae62b2f2acbbc82cd28511
Chris Sev <chris.sev@hey.com>
2024-02-14 13:21:35 -0800

    leidos fixes

--- src/pages/case-studies/qualtrics.tsx
+++ src/pages/case-studies/qualtrics.tsx
@@ -1,9 +1,8 @@
 import { FunctionComponent, ReactNode } from 'react'
 
-import Link from 'next/link'
-
 import { Badge, Blockquote, ContentSection, Heading, Hero, Layout } from '../../components'
 import MoreCaseStudies from '../../components/CaseStudies/MoreCaseStudies'
+import SidebarCta from '../../components/CaseStudies/SidebarCta'
 
 const Qualtrics: FunctionComponent = () => (
     <Layout
@@ -75,21 +74,7 @@ const Qualtrics: FunctionComponent = () => (
 minutes, leading to improved unit test coverage faster."
                         />
 
-                        <div className="sg-bg-code-search-cta mx-auto flex w-full flex-col rounded-lg py-12 px-6 text-center sm:items-center md:w-[378px] md:items-center md:text-left">
-                            <Heading size="h2" className="pb-4 !text-4xl text-white ">
-                                Try Sourcegraph on your code
-                            </Heading>
-                            <p className="pb-1 text-lg text-gray-200">
-                                Experience code intelligence with a free trial for you and your team, or search millions
-                                of open source repositories.
-                            </p>
-                            <Link
-                                className="btn btn-inverted-primary flex min-w-fit self-center rounded-md bg-white py-3 px-5 text-center text-base text-violet-500 hover:!bg-violet-400 md:self-start md:py-2 md:px-6"
-                                href="/demo"
-                            >
-                                Meet with a product expert
-                            </Link>
-                        </div>
+                        <SidebarCta />
                     </div>
                 </div>
 
