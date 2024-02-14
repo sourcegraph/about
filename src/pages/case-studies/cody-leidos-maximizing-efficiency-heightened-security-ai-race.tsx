cf359c6c393dee18f7fc716b97f1312f7b7954bf
Chris Sev <chris.sev@hey.com>
2024-02-14 13:16:45 -0800

    og image

--- src/pages/case-studies/cody-leidos-maximizing-efficiency-heightened-security-ai-race.tsx
+++ src/pages/case-studies/cody-leidos-maximizing-efficiency-heightened-security-ai-race.tsx
@@ -11,7 +11,7 @@ const Leidos: FunctionComponent = () => (
             title: 'Cody + Leidos: Maximizing efficiency with heightened security in the AI race',
             description:
                 "Leidos is a Fortune 500 innovation company rapidly addressing the world's most vexing challenges in national security and health. with Sourcegraph.",
-            image: '/case-studies/qualtrics-og.png',
+            image: '/case-studies/leidos-og.png',
         }}
         className="relative"
         heroAndHeaderClassName="pt-[98px] md:pt-[32px]"
