eaa91dd41102fa49564db061684c5289624e426d
Ado Kukic <kukicadnan@gmail.com>
2024-03-22 11:47:31 -0700

    add posthog to marketing site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 // Tailwind
 import '../styles/globals.css'
 
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import type { AppProps } from 'next/app'
 
@@ -10,16 +11,44 @@ import { useEventLogger, useLogAllLinkClicks } from '../hooks/eventLogger'
 import { useLandingSource } from '../hooks/landingSource'
 import 'prism-themes/themes/prism-one-light.css'
 
+// PostHog
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined') {
+  posthog.init('phc_GYC9gnJzJhbUMe7qIZPjMpTwAeF4kkC7AGAOXZgJ4pB', {
+    api_host: 'https://app.posthog.com',
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    }
+  })
+}
+
 const App = ({ Component, pageProps }: AppProps): ReactNode => {
     useEventLogger()
     useLandingSource()
     useLogAllLinkClicks()
 
+    const router = useRouter()
+
+    useEffect(() => {
+      const handleRouteChange = () => posthog?.capture('$pageview')
+      router.events.on('routeChangeComplete', handleRouteChange)
+
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }, [])
+
     return (
+      <PostHogProvider client={posthog}>
         <AuthModalProvider>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <Component {...pageProps} />
         </AuthModalProvider>
+      </PostHogProvider>
     )
 }
 
