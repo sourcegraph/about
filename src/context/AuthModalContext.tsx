7414a3eee9302b6980d4c82ca43ed103311ecd51
gitstart-sourcegraph <gitstart-sourcegraph@users.noreply.github.com>
2024-04-05 10:23:43 +0000

    refactor: Show the Authentication modal

--- src/context/AuthModalContext.tsx
+++ src/context/AuthModalContext.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactNode, createContext, useContext, useState } from 'react'
 
-import { IdeModalContent, Modal } from '../components'
+import { AuthenticateModalContent, Modal } from '../components'
 import { logAuthPopoverEvent } from '../util'
 
 interface AuthModalContextProps {
@@ -40,7 +40,7 @@ export const AuthModalProvider: FunctionComponent<{ children: ReactNode }> = ({ 
 
                 {isSignUpModalOpen && (
                     <Modal open={isSignUpModalOpen} handleClose={closeModal}>
-                        <IdeModalContent source={source} plan={plan} />
+                        <AuthenticateModalContent source={source} plan={plan} />
                     </Modal>
                 )}
             </>
