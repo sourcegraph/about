13b4faac076eab7466da2cfc780d5b4e8bcecd7b
Alex Isken <alex.isken@sourcegraph.com>
2024-04-10 17:04:52 -0500

    Back out a/b test on homepage (#6857)
    * back out a/b test on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { FunctionComponent } from 'react'
 import classNames from 'classnames'
 import ChevronRightIcon from 'mdi-react/ChevronRightIcon'
 import Link from 'next/link'
-import { useFeatureFlagVariantKey } from 'posthog-js/react'
 
 import { ContentSection, Layout, InfiniteCarousel, Heading } from '../components'
 import { useAuthModal } from '../context/AuthModalContext'
@@ -363,8 +362,6 @@ const HomeHero: FunctionComponent = () => {
 
     const { openModal } = useAuthModal()
 
-    const variant = useFeatureFlagVariantKey('grok-test')
-
     const handleOpenModal = (): void => openModal('home')
     return (
         <ContentSection className="flex items-center justify-center" parentClassName="!py-0">
@@ -374,7 +371,7 @@ const HomeHero: FunctionComponent = () => {
                         size="h1"
                         className="mb-8 w-full text-center !text-[48px] text-white md:mb-6 md:!text-[62px] lg:leading-[65px] lg:-tracking-[0.62px]"
                     >
-                        {variant === 'control' ? 'Grok' : 'Understand'} and write code blazingly fast
+                        Understand and write code blazingly fast
                     </Heading>
                     <p className="mb-10 text-2xl font-normal leading-[30px] -tracking-[0.25px] text-[#FFFFFF99] md:mb-8">
                         Sourcegraph allows developers to rapidly search, write, and understand code by bringing insights
