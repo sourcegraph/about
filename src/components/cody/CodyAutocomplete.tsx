d636a632985bceffe2b16a49be2359d6e59b36f9
Justin Dorfman <justin.dorfman@sourcegraph.com>
2024-04-10 17:13:26 -0700

    Use CDN for image (#6861)

--- src/components/cody/CodyAutocomplete.tsx
+++ src/components/cody/CodyAutocomplete.tsx
@@ -38,7 +38,7 @@ export const CodyAutocomplete: FunctionComponent<{ className?: string }> = ({ cl
             <div className="relative w-[670px] overflow-hidden">
                 <img
                     className="relative top-4 -right-4 w-[670px]"
-                    src="/cody/single-line-autocomplete_ty-arp242.svg"
+                    src="https://storage.googleapis.com/sourcegraph-assets/blog/single-line-autocomplete_ty-arp242.svg"
                     alt="Cody auto complete"
                 />
             </div>
