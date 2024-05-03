220ccef336fd40b3072727c474fa523b41eedf38
Chris Sev <chris.sev@hey.com>
2024-05-03 13:44:51 -0600

    styling

--- src/pages/webinars/index.tsx
+++ src/pages/webinars/index.tsx
@@ -9,26 +9,27 @@ const ComparePage: FunctionComponent = () => (
             description: 'Feature comparison of Sourcegraph Cody and other code AI assistants.',
         }}
         hero={
-            <div className="relative isolate bg-white px-6 pb-24 pt-24 lg:px-8">
-                <div className="mx-auto max-w-2xl text-center">
+            <div className="relative isolate bg-gray-50 px-6 pt-20 lg:px-8">
+                <div className="relative mx-auto max-w-[840px] overflow-hidden rounded-lg border border-gray-200 bg-white py-10 px-20 text-center shadow-sm">
                     <h1>Webinars</h1>
                     <p className="mt-3 text-xl leading-8 text-gray-600">
                         Join us for live events and webinars on coding with AI
                     </p>
-                </div>
 
-                <div className="absolute inset-x-0 bottom-0 w-full space-y-1">
-                    <div className="h-1.5 bg-blue-300" />
-                    <div className="h-2 bg-vermillion-300" />
-                    <div className="h-4 bg-violet-400" />
+                    <div className="absolute inset-y-0 -left-0 flex rotate-[20deg] scale-[2]">
+                        <div className="h-96 w-4 bg-blue-300" />
+                        <div className="h-96 w-4 bg-vermillion-300" />
+                        <div className="h-96 w-4 bg-violet-400" />
+                    </div>
                 </div>
             </div>
         }
         className="bg-gray-50"
     >
-        <ContentSection className="flex flex-col gap-10 md:flex-row">
-            <div className="h-full w-full">
-                <div className="h-full rounded-lg border border-gray-200 bg-white px-10 py-12">
+        <ContentSection>
+            <div className="mt-10 flex flex-col gap-10 rounded-lg border border-gray-200 bg-white px-10 py-12 md:mt-0 md:flex-row">
+                {/* left side */}
+                <div>
                     <h3 className="text-xl">May 9</h3>
                     <h2 className="mt-4">Getting Started with AI Coding</h2>
                     <p className="mt-6 text-lg text-gray-600">
@@ -42,25 +43,20 @@ const ComparePage: FunctionComponent = () => (
                     </p>
 
                     <p className="mt-4 text-lg text-gray-600">You'll learn to be a more productive developer!</p>
-
-                    {/* <button
-                        type="button"
-                        className="mt-8 rounded-lg bg-gray-700 py-3.5 px-8 font-semibold text-gray-50"
-                    >
-                        Register Now <span className="ml-2">👉</span>
-                    </button> */}
                 </div>
-            </div>
-            <div className="relative h-0 w-full overflow-hidden rounded pb-[56.25%]">
-                <iframe
-                    title="StreamYard Embed"
-                    src="https://streamyard.com/watch/xBjEG6AABB8s?embed=true"
-                    width="100%"
-                    height="100%"
-                    frameBorder="0"
-                    allow="autoplay; fullscreen"
-                    className="absolute left-0 top-0 h-full w-full overflow-hidden"
-                />
+
+                {/* right side */}
+                <div className="relative h-0 w-full overflow-hidden rounded pb-[56.25%]">
+                    <iframe
+                        title="StreamYard Embed"
+                        src="https://streamyard.com/watch/xBjEG6AABB8s?embed=true"
+                        width="100%"
+                        height="100%"
+                        frameBorder="0"
+                        allow="autoplay; fullscreen"
+                        className="absolute left-0 top-0 h-full w-full overflow-hidden"
+                    />
+                </div>
             </div>
         </ContentSection>
     </Layout>
