9e8e4701d4c50999d2e3816fdb9c95c18b9c983f
Alex Isken <alex.isken@sourcegraph.com>
2024-05-07 15:25:19 -0500

    Update Banner for the May 9 webinar (#6922)
    * Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,14 +6,14 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 px-2 md:flex-row">
             <p className="mb-0 text-center font-semibold leading-[22px] text-white">
-                Claude 3 is now available for all Cody users
+                Join us on May 9 to learn how to get started coding with AI
             </p>
             <Link
-                href="https://sourcegraph.com/blog/claude-3-now-available-in-cody"
-                title="Claude 3 in Cody"
+                href="https://sourcegraph.com/webinars"
+                title="AI Coding Webinar"
                 className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
             >
-                Read the blog
+                Register for the webinar
                 <ChevronRightIcon className="!mb-0 ml-[6px] inline" />
             </Link>
         </div>
