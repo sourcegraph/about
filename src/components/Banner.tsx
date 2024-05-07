fd97808bb15592ae84b50f3c13b11982466cb4de
Alex Isken <alex.isken@sourcegraph.com>
2024-05-07 15:19:46 -0500

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 px-2 md:flex-row">
             <p className="mb-0 text-center font-semibold leading-[22px] text-white">
-                Join us on May 9 to learn how to start coding with AI
+                Join us on May 9 to learn how to get started coding with AI
             </p>
             <Link
                 href="https://sourcegraph.com/webinars"
