14f2b4b6523adc53e89169a489dfb0144468105e
Alex Isken <alex.isken@sourcegraph.com>
2024-05-09 15:12:55 -0500

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,14 +6,14 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 px-2 md:flex-row">
             <p className="mb-0 text-center font-semibold leading-[22px] text-white">
-                Join us on May 9 to learn how to get started coding with AI
+                Join our webinar on advanced AI coding tips on May 16
             </p>
             <Link
                 href="https://sourcegraph.com/webinars"
                 title="AI Coding Webinar"
                 className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
             >
-                Register for the webinar
+                Register
                 <ChevronRightIcon className="!mb-0 ml-[6px] inline" />
             </Link>
         </div>
