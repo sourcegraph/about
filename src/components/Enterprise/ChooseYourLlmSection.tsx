fd2d0abac8aa27128060d1be3e813dc783ffa234
Alex Isken <alex.isken@sourcegraph.com>
2024-06-07 20:09:24 -0500

    Update ChooseYourLlmSection.tsx

--- src/components/Enterprise/ChooseYourLlmSection.tsx
+++ src/components/Enterprise/ChooseYourLlmSection.tsx
@@ -72,8 +72,8 @@ export const ChooseYourLlmSection: FunctionComponent<ChooseYourLlmSectionProps> 
         >
             <div
                 className={classNames(
-                    'w-full rounded-2xl border-1 border-gray-200 bg-violet-700 !py-16 px-6 text-white md:px-sm lg:px-20',
-                    authorCardClassName ?? 'w-[50%] text-white',
+                    'rounded-2xl border-1 border-gray-200 bg-violet-700 !py-16 px-6 text-white md:px-sm lg:px-20',
+                    authorCardClassName ?? 'text-white',
                     reverseQuote && 'flex h-full flex-col-reverse items-start !py-0 md:h-auto md:min-h-[554px]'
                 )}
             >
