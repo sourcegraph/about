5a9e1bfd235411d1a3608a469956ea0490f12755
Chris Sev <chris.sev@hey.com>
2024-06-21 13:13:37 -0600

    how it works

--- src/pages/autocody/index.tsx
+++ src/pages/autocody/index.tsx
@@ -1,7 +1,6 @@
 import { FunctionComponent } from 'react'
 
 import { Layout } from '../../components'
-import AutoCodyHowItWorks from '../../components/AutoCody/AutoCodyHowItWorks'
 
 const AutoCodyPage: FunctionComponent = () => (
     <Layout
@@ -18,11 +17,11 @@ const AutoCodyPage: FunctionComponent = () => (
             <VSCode />
         </div>
 
-        <div className="mt-32">
+        <div className="mt-40">
             <Principles />
         </div>
 
-        <div className="mx-auto mt-32 max-w-6xl pb-32">
+        <div className="mx-auto mt-40 max-w-6xl pb-32">
             <AutoCodyHowItWorks />
         </div>
     </Layout>
@@ -33,7 +32,7 @@ export default AutoCodyPage
 function VSCode(): JSX.Element {
     return (
         <div className="relative mx-auto max-w-5xl">
-            <div className="absolute inset-x-0 inset-y-10 rounded-xl border border-gray-200 bg-gray-100" />
+            {/* <div className="absolute inset-x-0 inset-y-10 rounded-xl border border-gray-200 bg-gray-50 " /> */}
 
             <img
                 alt="Visual Studio Code"
@@ -105,7 +104,7 @@ function VSCode(): JSX.Element {
                                     <svg
                                         stroke="currentColor"
                                         fill="none"
-                                        stroke-width="0"
+                                        strokeWidth="0"
                                         viewBox="0 0 24 24"
                                         className="h-6 w-6"
                                         height="1em"
@@ -113,9 +112,9 @@ function VSCode(): JSX.Element {
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
                                             d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
                                         />
                                     </svg>
@@ -127,7 +126,7 @@ function VSCode(): JSX.Element {
                                     <svg
                                         stroke="currentColor"
                                         fill="none"
-                                        stroke-width="0"
+                                        strokeWidth="0"
                                         viewBox="0 0 24 24"
                                         className="h-6 w-6"
                                         height="1em"
@@ -135,9 +134,9 @@ function VSCode(): JSX.Element {
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
                                             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                                         />
                                     </svg>
@@ -149,7 +148,7 @@ function VSCode(): JSX.Element {
                                     <svg
                                         stroke="currentColor"
                                         fill="none"
-                                        stroke-width="0"
+                                        strokeWidth="0"
                                         viewBox="0 0 24 24"
                                         className="ml-0.5 h-6 w-6"
                                         height="1em"
@@ -157,9 +156,9 @@ function VSCode(): JSX.Element {
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
                                             d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
                                         />
                                     </svg>
@@ -177,7 +176,7 @@ function VSCode(): JSX.Element {
                                     <svg
                                         stroke="currentColor"
                                         fill="none"
-                                        stroke-width="0"
+                                        strokeWidth="0"
                                         viewBox="0 0 24 24"
                                         className="h-6 w-6"
                                         height="1em"
@@ -185,15 +184,15 @@ function VSCode(): JSX.Element {
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
                                             d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
                                         />
                                         <path
-                                            stroke-linecap="round"
-                                            stroke-linejoin="round"
-                                            stroke-width="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
                                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                                         />
                                     </svg>
@@ -206,20 +205,30 @@ function VSCode(): JSX.Element {
                                 {/* <div className="absolute h-32 w-32 -translate-x-5 translate-y-5 -rotate-6 rounded-xl border border-black bg-gray-700" />
                         <div className="absolute h-32 w-32 translate-x-3 rotate-12 rounded-xl border border-black bg-gray-700" /> */}
                                 <div className="z-10 flex h-32 w-32 items-center justify-center rounded-full border border-black bg-gradient-to-br from-gray-700 to-black shadow-2xl">
-                                    <img src="/cody/cody-logo.svg" className="h-16 w-16" alt="Cody Logo" />
+                                    <video
+                                        autoPlay={true}
+                                        loop={false}
+                                        muted={true}
+                                        playsInline={true}
+                                        className="h-24 w-24"
+                                    >
+                                        <source src="/cody/hiCodyDark.mp4" type="video/mp4" />
+                                        Your browser does not support the video tag.
+                                    </video>
                                 </div>
                             </div>
 
                             <div className="relative h-[300px] flex-grow">
-                                <div className="flex h-full flex-col justify-center px-10 text-white lg:px-32">
-                                    <h1 className="mx-auto w-full pt-[50px] text-9xl font-semibold leading-[58px] !tracking-[-1px] text-white md:pt-0 md:leading-[62px]">
+                                <div className="flex h-full flex-col justify-center px-5 text-white">
+                                    <h1 className="text-[100px] font-semibold leading-none !tracking-[-3px] text-white">
                                         Auto
-                                        <span className="bg-gradient-to-br from-blurple-200 to-blurple-300 bg-clip-text text-transparent">
+                                        <span className="bg-gradient-to-br from-gray-100 to-gray-200 bg-clip-text text-transparent">
                                             Cody
                                         </span>
                                     </h1>
-                                    <p className="mx-auto mt-4 max-w-[25ch] text-xl leading-normal text-gray-200 lg:text-2xl">
-                                        On the road to the next generation of AI coding assistants
+                                    <p className="mx-auto mt-4 max-w-[40ch] text-xl leading-normal text-gray-300 lg:text-2xl">
+                                        We are building our autonomous coding assistant that will be able to write code
+                                        for you, understand your problems, and guide you in solving them.
                                     </p>
                                 </div>
                             </div>
@@ -235,30 +244,68 @@ function VSCode(): JSX.Element {
 function Principles(): JSX.Element {
     return (
         <>
-            <h2 className="text-center text-4xl font-semibold !tracking-[-1px] text-[#0F111A]">Core Principles</h2>
-            <h3 className="mx-auto mt-2 text-center text-xl font-semibold tracking-tight text-gray-400">
-                What's the best strategy to create autonomous coding?
-            </h3>
-
-            <div className="mx-auto mt-10 max-w-3xl rounded-xl border border-gray-200 bg-white py-12 px-10">
-                <p className="font-semibold text-gray-400">How do we get to autonomous coding?</p>
-                <h2 className="mx-auto w-full text-4xl font-semibold text-black">
-                    <span className="bg-gradient-to-br from-blurple-300 to-blurple-400 bg-clip-text text-transparent">
-                        A combination of coder and AI
-                    </span>
-                </h2>
-
-                <div className="mt-4 text-xl font-medium leading-normal text-gray-600 lg:text-2xl">
-                    If you want to create an autonomous car, you don't remove the steering wheel.
-                </div>
+            <div className="mx-auto max-w-3xl">
+                <h2 className="text-4xl font-semibold !tracking-[-1px] text-[#0F111A]">Core Principles</h2>
+                <h3 className="mx-auto mt-2 text-xl font-semibold tracking-tight text-gray-400">
+                    How we're thinking about the road to autonomous coding
+                </h3>
             </div>
 
-            <div className="mx-auto mt-4 max-w-3xl rounded-xl border border-gray-200 bg-white py-12 px-10">
-                <p className="font-semibold text-gray-400">Where can we get highest fidelity feedback from the user?</p>
+            <div className="mx-auto mt-16 max-w-3xl">
+                <p className="font-semibold text-gray-400">Principle 1: Hybrid</p>
+                <p className="text-xl font-semibold text-gray-400 lg:text-3xl lg:leading-normal">
+                    <span className="text-blurple-400">A combination of coder and AI. </span> There are agentic
+                    solutions that let the AI free to choose the correct path to solve an issue from start to finish.{' '}
+                    <a
+                        href="https://github.com/princeton-nlp/swe-agent"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:underline"
+                    >
+                        SWE-Agent
+                    </a>
+                    , the benchmark for autonomous code generation, is only able to achieve a 12.29% success rate on the
+                    full SWE-bench test set. If you want to create an autonomous car, you don't remove the steering
+                    wheel. We must allow the AI to be trained and "steered" by a user.
+                </p>
+
+                <p className="mt-5 text-xl font-semibold text-gray-400 lg:text-3xl lg:leading-normal">
+                    We believe the best place for a user to provide high fidelity feedback to AI is in a code editor.
+                </p>
+            </div>
+
+            <div className="mx-auto mt-16 max-w-3xl">
+                <p className="font-semibold text-gray-400">Principle 2: Context</p>
+                <p className="text-xl font-semibold text-gray-400 lg:text-3xl lg:leading-normal">
+                    <span className="text-blurple-400">Context should come from inside AND outside a repo. </span> We
+                    are building{' '}
+                    <a
+                        href="https://openctx.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:underline"
+                    >
+                        OpenCtx
+                    </a>
+                    , an open source tool to bring in content about your code from sources like Linear, Jira, Slack,
+                    Google Docs, and more. We believe that an LLM (and a human) can't effectively solve a problem
+                    without the context of the problem.
+                </p>
+            </div>
+
+            <div className="mx-auto mt-16 max-w-3xl">
+                <p className="font-semibold text-gray-400">Principle 3: Steerable</p>
+                <p className="text-xl font-semibold text-gray-400 lg:text-3xl lg:leading-normal">
+                    <span className="text-blurple-400">A user should be in the loop at every step. </span> In an agentic
+                    workflow, AI will have many steps to complete for the user. A user should be able to steer the
+                    workflow at every step by evaluating LLM output throughout the process.
+                </p>
+            </div>
+
+            <div className="mx-auto mt-16 max-w-3xl">
+                <p className="font-semibold text-gray-400">Principle 4: Context</p>
                 <h2 className="mx-auto w-full text-4xl font-semibold text-black">
-                    <span className="bg-gradient-to-br from-blurple-300 to-blurple-400 bg-clip-text text-transparent">
-                        The editor is the best place for user feedback
-                    </span>
+                    <span className="text-black">Context .</span>
                 </h2>
 
                 <div className="mt-4 text-xl font-medium leading-normal text-gray-600 lg:text-2xl">
@@ -267,5 +314,37 @@ function Principles(): JSX.Element {
                 </div>
             </div>
         </>
+    )
+}
+
+function AutoCodyHowItWorks(): JSX.Element {
+    return (
+        <div>
+            <h2 className="text-center text-4xl font-semibold !tracking-[-1px] text-[#0F111A]">
+                How does AutoCody work?
+            </h2>
+            <h3 className="mx-auto mt-2 text-center text-xl font-semibold tracking-tight text-gray-400">
+                A 5-step user{' '}
+                <span role="img" aria-label="handshake">
+                    🤝
+                </span>{' '}
+                AI strategy
+            </h3>
+
+            <div className="mt-14 grid grid-cols-[400px,1fr] gap-4">
+                <div className="">
+                    <div className="rounded-2xl bg-gray-200/30 p-8">
+                        <p className="text-sm font-semibold text-gray-400">Step 1</p>
+                        <h3 className="text-2xl font-semibold text-[#0F111A]">Prompting</h3>
+                        <p className="mt-4 text-lg text-gray-500">
+                            The step that we know and have done 1,000+ times in the past year
+                        </p>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="rounded-2xl bg-gray-200/30 p-8">VIDEO GOES HERE</div>
+                </div>
+            </div>
+        </div>
     )
 }
