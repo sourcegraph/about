38fc097ac1329dd7fe573f5796d57926576e4c85
Justin Dorfman <justin.dorfman@sourcegraph.com>
2024-06-24 09:48:05 -0700

    Update banner (#7035)
    * Update banner
    * Update text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,14 +6,15 @@ export const Banner: FunctionComponent<{}> = () => (
     <div className="sg-border-gradient-banner z-[1000] border-b-1 bg-black py-[13px]">
         <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-y-[9px] gap-x-12 px-2 md:flex-row">
             <p className="mb-0 text-center font-semibold leading-[22px] text-white">
-                AI Dev Tools Night: Come to our upcoming meetup in SF!
+                Join us for a live Q&A session with the co-founders on June 25th!
             </p>
             <Link
-                href="https://sourcegraph.com/blog/ai-dev-tools-night-meetup-june-24"
-                title="AI Dev Tools Night"
+                href="https://discord.gg/nyFC7Gah?event=1252715837614461012"
+                title="Join us for a live Q&A session with the co-founders on Discord!"
                 className="btn bg-transparent !px-0 !py-0 leading-[22px] text-violet-300"
+                target="_blank"
             >
-                Learn more
+                RSVP in Discord
                 <ChevronRightIcon className="!mb-0 ml-[6px] inline" />
             </Link>
         </div>
