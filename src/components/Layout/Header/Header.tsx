4b815ba180529917507ca22c9d6563dd10c5ec05
Justin Dorfman <justindorfman@gmail.com>
2024-06-26 16:12:27 -0700

    Add documentation on turning banner on and off

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -26,6 +26,9 @@ export const Header: FunctionComponent<Props> = ({ minimal, colorTheme, navRef }
     const [sticky, setSticky] = useState<boolean>(false)
     const router = useRouter()
     const { pathname } = router
+    /**
+     * Determines whether the banner component should be displayed in the header.
+     */
     const [showBanner, setShowBanner] = useState(false)
 
     const source = pathname.slice(1) || 'about-home'
