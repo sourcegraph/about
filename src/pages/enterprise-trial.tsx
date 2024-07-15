63c3888803f4a6446b70f4eaa6b0055c4ca73f6d
Alex Isken <alex.isken@sourcegraph.com>
2024-07-15 17:34:30 -0500

    Update enterprise-trial.tsx

--- src/pages/enterprise-trial.tsx
+++ src/pages/enterprise-trial.tsx
@@ -2,21 +2,10 @@ import { FunctionComponent, useEffect, useRef, useState } from 'react'
 
 import classNames from 'classnames'
 
-import { ContentSection, HubSpotForm, Layout } from '../../components'
-import { ContactUsCta } from '../../components/cta/ContactUsCta'
-import { RequestDemoCta } from '../../components/cta/RequestDemoCta'
-import { ChooseYourLlmSection } from '../../components/Enterprise/ChooseYourLlmSection'
-import { CompanyUsingCodySection } from '../../components/Enterprise/CompanyUsingCodySection'
-import { EnterpriseGradeSection } from '../../components/Enterprise/EnterpriseGradeSection'
-import { MigrationsSection } from '../../components/Enterprise/MigrationsSection'
-import { NineCaseStudySection } from '../../components/Enterprise/NineCaseStudySection'
-import { NutanixCaseStudySection } from '../../components/Enterprise/NutanixCaseStudySection'
-import { ScalableSection } from '../../components/Enterprise/ScalableSection'
-import { SecureSection } from '../../components/Enterprise/SecureSection'
-import { SecurityFeatureSection } from '../../components/Enterprise/SecurityFeatureSection'
-import { TrackVulnerabilitiesSection } from '../../components/Enterprise/TrackVulnerabilitiesSection'
-import { UniversalSection } from '../../components/Enterprise/UniversalSection'
-import { WriteCodeFasterSection } from '../../components/Enterprise/WriteCodeFasterSection'
+import { HubSpotForm, Layout } from '../components'
+import { HowCodyWorks } from '../components/cody/HowCodyWorks'
+import { CodyChooseLlmDualTheme } from '../components/cody/dual-theme/CodyChooseLlmDualTheme'
+import { EnterpriseGradeSection } from '../components/Enterprise/EnterpriseGradeSection'
 
 const Enterprise: FunctionComponent = () => {
     const formContainerRef = useRef<HTMLDivElement>(null)
@@ -32,19 +21,18 @@ const Enterprise: FunctionComponent = () => {
     return (
         <Layout
             meta={{
-                title: 'Sourcegraph - Enterprise',
+                title: 'Cody | Enterprise Trial Offer',
                 description:
-                    "Give your teams the ability to search, write, and understand massive codebases through Sourcegraph's universal and secure Code Intelligence Platform.",
+                    "Get started with a trial of Cody Enterprise to try the AI coding assistant with your team.",
             }}
             className="bg-gray-50"
             hero={
                 <div className="w-full" ref={formContainerRef}>
                     <div className="relative mx-auto flex w-full max-w-7xl flex-col gap-6 px-2 pt-[112px] md:flex-row md:px-6">
                         <div className="flex w-full flex-1 flex-col gap-6">
-                            <h1 className="md:text-6xl">Modern enterprises are powered by productive developers</h1>
+                            <h1 className="md:text-6xl">Get a free Cody Enterprise trial</h1>
                             <h3>
-                                Give your teams the ability to search, write, and understand massive codebases through
-                                Sourcegraph's universal and secure Code Intelligence Platform.{' '}
+                            Try out Cody Enterprise with your entire team. Trials are available for teams of 25 or more developers, free for 30 days.{' '}
                             </h3>
                             {!formSubmitted && (
                                 <img
@@ -57,10 +45,7 @@ const Enterprise: FunctionComponent = () => {
 
                         <div className="w-full md:w-1/2">
                             <div className="relative z-10 order-1 rounded-2xl border-1 border-gray-200 bg-white pt-6 pb-0 pl-6 pr-[1px] md:order-2 md:pt-12 md:pb-[13px] md:pl-16 md:pr-[30px]">
-                                <h2 className="mb-6 text-gray-700">Contact us for a demo</h2>
-                                <h3 className="text-[18px] font-normal text-gray-500 lg:w-[95%]">
-                                    Talk with a product specialist to learn more about Sourcegraph.
-                                </h3>
+                                <h2 className="mb-6 text-gray-700">Request your free trial</h2>
                                 <div
                                     className={classNames('mt-5', {
                                         'pb-[48px]': formSubmitted,
@@ -79,45 +64,9 @@ const Enterprise: FunctionComponent = () => {
                 </div>
             }
         >
-            <CompanyUsingCodySection />
-
-            {/* This is the ImprovedVelocitySection from the Enterprise page, edited to not include the product iconography */}
-            <ContentSection
-                className="relative z-10 flex max-w-[1232px] flex-col gap-6 md:flex-row"
-                parentClassName="!md:px-[80px] !py-0"
-            >
-                <div className="flex h-[533px] flex-col justify-between px-0 py-16 md:px-10">
-                    <div className="flex flex-col gap-4">
-                        <h2 className="text-black">Improve velocity with faster code discovery and understanding</h2>
-                        <p className="mb-0 text-lg leading-[27px] tracking-[-0.25px]">
-                            Developers can find, navigate, and share code across entire codebases in seconds, increasing
-                            development velocity and reducing time spent searching for answers.
-                        </p>
-                    </div>
-                </div>
-                <div className="h-auto rounded-2xl border-1 border-gray-200 bg-white px-6 py-16  md:max-w-[574px] md:px-20">
-                    <p className="mb-0 leading-6 tracking-[-0.25px] text-gray-700">Satish Surapaneni</p>
-                    <p className="mb-0 text-sm leading-[19.88px]">Senior Engineering Manager, F5</p>
-                    <p className="mb-0 pt-3 text-[35px] font-normal leading-[43.75px] -tracking-[2px]">
-                        "We are developing software faster than ever, with aggressive schedules, and across boundaries.
-                        Things that used to be worked out in a closed room now need to be done while teams are spread
-                        out across the globe.”
-                    </p>
-                </div>
-            </ContentSection>
-            <WriteCodeFasterSection />
-            <MigrationsSection />
-            <TrackVulnerabilitiesSection />
-            <RequestDemoCta onClickContactUs={onScrollToForm} />
-            <UniversalSection />
-            <ChooseYourLlmSection />
-            <ScalableSection />
-            <NineCaseStudySection />
-            <SecureSection />
-            <NutanixCaseStudySection />
-            <SecurityFeatureSection />
+            <HowCodyWorks isLight={true} />
+            <CodyChooseLlmDualTheme isLight={true} />
             <EnterpriseGradeSection />
-            <ContactUsCta onClickContactUs={onScrollToForm} />
         </Layout>
     )
 }
