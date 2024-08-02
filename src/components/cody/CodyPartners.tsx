10564a224536bd4294b00bf0f7317b046d62f2f8
gitstart-sourcegraph <gitstart-sourcegraph@users.noreply.github.com>
2024-08-02 05:56:35 +0000

    feat: Add enterprise  trial page

--- src/components/cody/CodyPartners.tsx
+++ src/components/cody/CodyPartners.tsx
@@ -5,41 +5,90 @@ import classNames from 'classnames'
 import { ContentSection } from '..'
 
 const darkThemeIcons = [
-    { id: 'Qualtrics Logo', src: '/qualtrics-logo.svg', className: 'block h-[51px] w-[158px]' },
-    { id: 'Drip Logo', src: '/drip-logo.svg', className: 'h-[41px] w-[111px]' },
-    { id: 'Podium Logo', src: '/podium-logo.svg', className: 'h-[36px] w-[168px]' },
-    { id: '1Password Logo', src: '/1password-logo.svg', className: 'h-[42px] w-[214px]' },
-    { id: 'Volant Partners Logo', src: '/volant-partners-logo.svg', className: 'h-[37.998px] w-[173.002px]' },
-    { id: 'mintable Logo', src: '/mintable-logo.svg', className: 'hidden h-[74px] w-[64px] md:block' },
+    {
+        id: '1Password Logo',
+        src: '/1password-logo.svg',
+        className: 'block h-[33.92px] md:mt-[7px] w-[181.636px] order-1 md:order-1',
+    },
+    {
+        id: 'Volant Partners Logo',
+        src: '/volant-partners-logo.svg',
+        className: 'h-[32.81px] w-[150px] md:mt-[7.66px] order-3 md:order-2',
+    },
+    {
+        id: 'Leidos Logo',
+        src: '/leidos-logo.svg',
+        className: 'h-[27px] md:mt-[7px] w-[117.612px] order-4 md:order-2',
+    },
+    {
+        id: 'Qualtrics Logo',
+        src: '/qualtrics-logo.svg',
+        className: 'h-12 w-[107px] order-4 md:order-3',
+    },
+    {
+        id: 'Drip Logo',
+        src: '/drip-logo.svg',
+        className: 'h-[30px] w-[81px] order-5 md:order-4 md:mt-[11px]',
+    },
+    {
+        id: 'Podium Logo',
+        src: '/podium-logo.svg',
+        className: 'h-12 w-[136px] order-2 md:order-6',
+    },
+    {
+        id: 'mintable Logo',
+        src: '/mintable-logo.svg',
+        className: 'h-12 w-[147px] md:block order-7 md:order-6',
+    },
 ]
 
 const lightThemeIcons = [
-    { id: '1Password Logo', src: '/1password-light.svg', className: 'block h-[40px] w-[208px] order-5 md:order-1' },
+    {
+        id: '1Password Logo',
+        src: '/1password-light.svg',
+        className: 'block h-[33.92px] md:mt-[7px] w-[181.636px] order-1 md:order-1',
+    },
     {
         id: 'Volant Partners Logo',
         src: '/volant-partners-light.svg',
-        className: 'h-[60.2px] w-[275px] md:h-[37.84px] md:w-[173px] order-4 md:order-2',
+        className: 'h-[32.81px] w-[150px] md:mt-[7.66px] order-3 md:order-2',
     },
-    { id: 'Qualtrics Logo', src: '/qualtrics-light.svg', className: 'h-[54.91px] w-[171.47px] order-1 md:order-3' },
-    { id: 'Drip Logo', src: '/drip-light.svg', className: 'h-[60px] w-[164px] order-2 md:order-4' },
-    { id: 'Podium Logo', src: '/podium-light.svg', className: 'h-[37px] w-[173px] order-3 md:order-5' },
+    {
+        id: 'Leidos Logo',
+        src: '/leidos-light.svg',
+        className: 'h-[27px] md:mt-[7px] w-[117.612px] order-4 md:order-2',
+    },
+    {
+        id: 'Qualtrics Logo',
+        src: '/qualtrics-light.svg',
+        className: 'h-12 w-[107px] order-4 md:order-3',
+    },
+    {
+        id: 'Drip Logo',
+        src: '/drip-light.svg',
+        className: 'h-[30px] w-[81px] order-5 md:order-4 md:mt-[11px]',
+    },
+    {
+        id: 'Podium Logo',
+        src: '/podium-light.svg',
+        className: 'h-12 w-[136px] order-2 md:order-6',
+    },
     {
         id: 'mintable Logo',
         src: '/mintable-light.svg',
-        className: 'hidden h-[74px] w-[60.5px] md:block md:order-6',
+        className: 'h-12 w-[147px] md:block order-7 md:order-6',
     },
 ]
 
-export const CodyPartners: FunctionComponent<{ isLight?: boolean }> = ({ isLight = false }) => (
+export const CodyPartners: FunctionComponent<{ isLight?: boolean; className?: string }> = ({
+    isLight = false,
+    className,
+}) => (
     <ContentSection parentClassName="!p-0 m-0">
         <div
             className={classNames(
-                'm-0 mt-16 flex w-full flex-wrap items-center justify-center p-0 pt-8 md:mt-0 md:pt-24',
-                {
-                    'gap-[24px] gap-y-[68px] py-[32px] px-[24px] md:justify-center md:gap-x-[48px] md:gap-x-[48px] md:py-0 md:px-[0px]':
-                        isLight,
-                    'gap-[68px] px-20 md:justify-between md:px-[26px]': !isLight,
-                }
+                className,
+                'm-0 mt-0 flex w-full flex-wrap items-center justify-center gap-12 gap-y-16 py-24 px-[48.5px] md:mt-0 md:items-start md:justify-center md:gap-x-[48px] md:gap-x-[48px] md:px-[0px]'
             )}
         >
             {(isLight ? lightThemeIcons : darkThemeIcons).map(icon => (
---
+++ src/pages/enterprisetrialoffer.tsx
@@ -0,0 +1,79 @@
+import { FunctionComponent, useEffect, useRef, useState } from 'react'
+
+import classNames from 'classnames'
+
+import { Layout, CodyPartners, ContentSection, HubSpotForm } from '../components'
+import { ChooseYourLlmSection } from '../components/Enterprise/ChooseYourLlmSection'
+import { EnterpriseGradeSection } from '../components/Enterprise/EnterpriseGradeSection'
+import { ProcessTable } from '../components/Enterprise/ProcessTable'
+import { UniversalSection } from '../components/Enterprise/UniversalSection'
+
+const EnterpriseTrialOffer: FunctionComponent = () => {
+    const formContainerRef = useRef<HTMLDivElement>(null)
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    useEffect(() => {
+        if (formSubmitted && formContainerRef.current) {
+            formContainerRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+        }
+    }, [formSubmitted])
+
+    return (
+        <Layout
+            meta={{
+                title: 'Cody | Enterprise Trial Offer',
+                description:
+                    'Get started with a trial of Cody Enterprise to try the AI coding assistant with your team.',
+            }}
+            className="bg-gray-50"
+            heroAndHeaderClassName="pt-20 md:pt-[57px]"
+            hero={
+                <ContentSection parentClassName="pt-11 pb-[64px] px-[24px] md:px-20">
+                    <div ref={formContainerRef}>
+                        <div className="relative mx-auto flex w-full flex-col gap-10 pt-0 md:flex-row md:gap-10">
+                            <div className="flex w-full flex-1 flex-col md:pr-10 md:pt-10 md:pr-20">
+                                <h1 className="mb-10 max-w-[533px] !text-5xl text-gray-700 md:!text-4xl md:tracking-tighter">
+                                    Productive <span className="hidden md:inline-block">dev teams</span>{' '}
+                                    <span className="md:hidden">developers</span> are powered by Cody Enterprise
+                                </h1>
+                                <h5 className="max-w-[533px] text-xl leading-[26px] text-gray-700 md:mb-10">
+                                    Start your free trial of Cody Enterprise and give your teams the ability to search,
+                                    write, and understand massive codebases through Sourcegraph's universal and secure
+                                    Code Intelligence Platform.
+                                </h5>
+                            </div>
+                            <div className="w-full md:w-1/2">
+                                <div className="overflow-hidden !rounded-2xl ">
+                                    <div className="sg-border-gradient-hubspot relative z-10 !rounded-2xl border-1 bg-white pl-12 pt-12 pb-[11px] pr-2.5">
+                                        <h2 className="mb-6 text-gray-700">Start your free trial</h2>
+                                        <div
+                                            className={classNames('mt-5', {
+                                                'pb-[48px]': formSubmitted,
+                                            })}
+                                        >
+                                            <HubSpotForm
+                                                masterFormName="contactMulti"
+                                                chiliPiper={false}
+                                                bookIt={true}
+                                                form_submission_source="request-info"
+                                                onFormSubmitted={() => setFormSubmitted(true)}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </ContentSection>
+            }
+        >
+            <ProcessTable />
+            <CodyPartners isLight={true} />
+            <UniversalSection />
+            <ChooseYourLlmSection />
+            <EnterpriseGradeSection />
+        </Layout>
+    )
+}
+
+export default EnterpriseTrialOffer
