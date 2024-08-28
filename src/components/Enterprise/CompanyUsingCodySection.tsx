3183810bf5a769238ccce1e3076c6da6f9252cef
Alex Isken <alex.isken@sourcegraph.com>
2024-08-27 21:05:28 -0500

    Updates enterprise logo pond to have a fade effect

--- src/components/Enterprise/CompanyUsingCodySection.tsx
+++ src/components/Enterprise/CompanyUsingCodySection.tsx
@@ -12,12 +12,12 @@ export const carouselImages = [
     { src: '/home/carousel/qualtrics-logo.svg', className: 'h-[55px] w-[172px] mx-[21px]' },
     { src: '/assets/icons/leidos-light.svg', className: 'h-[54px] w-[225px] mx-[21px]' },
     { src: '/home/carousel/palo-alto-logo.svg', className: ' h-[48px] w-[258px] mx-[21px]' },
+    { src: '/assets/icons/uber-logo.svg', className: ' h-[48px] w-[170px] mx-[21px]' },
 ]
-
 export const CompanyUsingCodySection: FunctionComponent = () => (
     <div className="relative mx-auto mt-0 flex flex-col items-center gap-12 py-16 text-center md:mt-[-135px] md:pt-[282px] md:pb-24">
         <ContentSection parentClassName="!py-0 md:px-20">
-            <div className="z-10 flex flex-col items-center gap-6  text-center md:w-[615px]">
+            <div className="z-10 flex flex-col items-center gap-6 text-center md:w-[615px]">
                 <h2>Used by 6 of the 10 largest software companies in the world</h2>
                 <Link
                     href="https://sourcegraph.com/case-studies"
@@ -29,6 +29,10 @@ export const CompanyUsingCodySection: FunctionComponent = () => (
                 </Link>
             </div>
         </ContentSection>
-        <InfiniteCarousel images={carouselImages} />
+        <div className="relative max-w-[900px] w-full overflow-hidden">
+            <div className="absolute left-0 top-0 bottom-0 w-16 bg-gradient-to-r from-gray-50 to-transparent z-10"/>
+            <div className="absolute right-0 top-0 bottom-0 w-16 bg-gradient-to-l from-gray-50 to-transparent z-10"/>
+            <InfiniteCarousel images={carouselImages} />
+        </div>
     </div>
 )
