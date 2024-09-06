4d86b19f992b1d7ee94629495b8ca7f0f5ed52df
Tracey Johnson <102541294+traceyljohnson@users.noreply.github.com>
2024-09-06 15:48:29 -0500

    Update _document.tsx
    Removed stray /> thingy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,8 +45,7 @@ export default class MyDocument extends Document<MyDocumentProps> {
                     {/* Google Fonts */}
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-
-                    />
+                    
                     <link
                         href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
                         rel="stylesheet"
