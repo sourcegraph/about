2067915a06b2fa3c511b316f07576e130ca84deb
Justin Dorfman <justindorfman@gmail.com>
2024-09-10 13:25:39 -0700

    Update default OG url & add new events (#7227)
    * feat: Update default OG image URL
    * feat: Add new upcoming event to EventsList
    * feat: Remove unused import from EventsList component
    * feat: Reorder upcoming events in EventsList component

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -13,6 +13,18 @@ interface Event {
 const events: { upcoming: Event[], past: Event[] } = {
     upcoming: [
         {
+            title: 'Your Cody questions answered live!',
+            location: 'StreamYard',
+            date: '2024-09-18',
+            link: 'https://streamyard.com/watch/kQC2DSFWAx6r?v=2',
+        },
+        {
+            title: 'AI Dev Tools Night',
+            location: 'San Francisco, CA',
+            date: '2024-09-30',
+            link: 'https://lu.ma/mkjng0xx',
+        },
+        {
             title: 'designFAO',
             location: 'Portugal',
             date: '2024-10-11',
