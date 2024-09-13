d143f27a0d020e1fc8d952aa3f492af0f4a41361
Chris Sev <chris.sev@sourcegraph.com>
2024-09-13 13:40:41 -0600

    openai o1-preview banner (#7245)
    * o1 banner
    
    * styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,16 +12,15 @@ import Link from 'next/link'
 export const Banner: FunctionComponent<{}> = () => (
     <div className="z-[1000] bg-black  px-4 lg:px-0">
         <Link
-            href="https://sourcegraph.com/blog/gartner-magic-quadrant-ai-code-assistants"
-            title="Sourcegraph is a Visionary in the first Gartner® Magic Quadrant™ for AI Code Assistants"
+            href="https://sourcegraph.com/blog/openai-o1-for-cody"
             className="btn btn-link-dark btn-link-icon transition-colorshover:text-white group flex w-full items-center justify-center bg-transparent !px-0 !py-3 text-sm text-gray-200 hover:no-underline focus:outline-none focus:ring-0"
             target="_blank"
         >
             <p className="mb-0 mr-2 text-center text-sm font-semibold">
-                Sourcegraph is a Visionary in the 2024 Gartner® Magic Quadrant™ for AI Code Assistants
+                Get on the waitlist for the new OpenAI o1-preview
             </p>
-            <span className="hidden text-violet-300 lg:inline-block">
-                <span>Learn more</span>
+            <span className="hidden translate-y-px text-violet-300 lg:inline-block">
+                <span>Get access</span>
                 <ChevronRightIcon className="!mb-0 ml-0.5 inline -translate-y-px transition-transform group-hover:translate-x-2" />
             </span>
         </Link>
