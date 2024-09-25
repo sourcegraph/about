892b340f7ec85e30791e960afb07a00a5fbf9339
Chris Sev <chris.sev@hey.com>
2024-09-25 16:16:45 -0600

    dev starter pack modal

--- src/pages/cody.tsx
+++ src/pages/cody.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState, useEffect } from 'react'
+import { FunctionComponent, useState, useEffect, useRef } from 'react'
 
 import classNames from 'classnames'
 import { useRouter } from 'next/router'
@@ -56,8 +56,8 @@ const CodyPage: FunctionComponent = () => {
     const router = useRouter()
     const { pathname } = router
     const { openModal } = useAuthModal()
-    const [title, setTitle ] = useState('');
-    const [titleSize, setTitleSize ] = useState('');
+    const [title, setTitle] = useState('')
+    const [titleSize, setTitleSize] = useState('')
     const [description, setDescription] = useState('')
     const [descriptionSize, setDescriptionSize] = useState('')
 
@@ -69,30 +69,38 @@ const CodyPage: FunctionComponent = () => {
 
     useEffect(() => {
         posthog.onFeatureFlags(() => {
-            const featureFlag = posthog.getFeatureFlag('cody-page-messaging-test');
+            const featureFlag = posthog.getFeatureFlag('cody-page-messaging-test')
             if (featureFlag === 'test-most-informed') {
-                setTitle('The most informed Code AI');
-                setDescription('Cody uses the latest LLMs and all your development context to help you understand, write, and fix code faster')
+                setTitle('The most informed Code AI')
+                setDescription(
+                    'Cody uses the latest LLMs and all your development context to help you understand, write, and fix code faster'
+                )
                 setTitleSize('text-[40px] text-4xl sm:text-8xl')
                 setDescriptionSize('md:text-xl')
             } else if (featureFlag === 'test-models-context') {
-                setTitle('Coding assistant with the latest AI and the most context');
-                setDescription('Cody uses context of your codebase, docs, tickets, and the web for faster development and accurate code-gen')
+                setTitle('Coding assistant with the latest AI and the most context')
+                setDescription(
+                    'Cody uses context of your codebase, docs, tickets, and the web for faster development and accurate code-gen'
+                )
                 setTitleSize('text-[40px] text-4xl sm:text-6xl')
                 setDescriptionSize('md:text-xl')
             } else if (featureFlag === 'test-multiple-contexts') {
-                setTitle('The AI assistant with context of your codebase, docs, tickets, and the web');
-                setDescription('Cody uses the latest LLMs and all your development context to help you understand, write, and fix code faster')
+                setTitle('The AI assistant with context of your codebase, docs, tickets, and the web')
+                setDescription(
+                    'Cody uses the latest LLMs and all your development context to help you understand, write, and fix code faster'
+                )
                 setTitleSize('text-[34px] text-4xl sm:text-8xl')
                 setDescriptionSize('md:text-xl')
             } else {
-                setTitle('Code more, type less');
-                setDescription('Cody is an AI coding assistant that uses advanced search and codebase context to help you understand, write, and fix code faster')
+                setTitle('Code more, type less')
+                setDescription(
+                    'Cody is an AI coding assistant that uses advanced search and codebase context to help you understand, write, and fix code faster'
+                )
                 setTitleSize('text-[52px] text-4xl sm:text-8xl')
                 setDescriptionSize('md:text-xl')
             }
-        });
-    }, []);
+        })
+    }, [])
 
     return (
         <Layout
@@ -107,7 +115,7 @@ const CodyPage: FunctionComponent = () => {
         >
             <div className="relative md:pt-10">
                 <div className="sg-bg-gradient-cody-light-mobile-hero !absolute top-[310px] z-[10] h-[650px] w-[1000px] md:relative md:hidden md:bg-none" />
-                {title && description && 
+                {title && description && (
                     <CodyIntroDualTheme
                         isLight={true}
                         title={title}
@@ -117,7 +125,7 @@ const CodyPage: FunctionComponent = () => {
                         handleOpenModal={handleOpenModal}
                         wrapperClassName="relative z-[20] md:z-0"
                     />
-                }
+                )}
 
                 <p className="pt-32 text-center text-base font-normal uppercase leading-[27px] text-gray-400">
                     Over 2.5M engineers use Sourcegraph
@@ -180,7 +188,83 @@ const CodyPage: FunctionComponent = () => {
                     </div>
                 </div>
             </Modal>
+
+            <DevStarterPackModal />
         </Layout>
+    )
+}
+
+const DevStarterPackModal: FunctionComponent = () => {
+    const router = useRouter()
+    const dialogRef = useRef<HTMLDialogElement>(null)
+
+    // Define a type for the dialog element
+    type DialogElement = HTMLDialogElement & { showModal: () => void }
+
+    useEffect(() => {
+        if (router.query.ref === 'devstarterpack' && dialogRef.current) {
+            ;(dialogRef.current as DialogElement).showModal()
+        }
+    }, [router.query])
+
+    const handleClose = (): void => {
+        if (dialogRef.current) {
+            ;(dialogRef.current as HTMLDialogElement)?.close?.()
+        }
+    }
+
+    return (
+        <dialog
+            ref={dialogRef}
+            className="relative rounded-lg bg-white px-4 pb-4 pt-5 text-left transition-all sm:my-40 sm:w-full sm:max-w-xl sm:p-16"
+        >
+            <div>
+                <div className="mx-auto flex aspect-video w-32 items-center justify-center rounded-md bg-[#0D121A]">
+                    <video autoPlay={true} loop={false} muted={true} playsInline={true} className="h-24 w-24">
+                        <source src="/assets/cody/hiCodyDark.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div className="mt-3 text-center sm:mt-7">
+                    <h2 className="text-gray-900 flex items-center justify-center gap-x-2 text-2xl font-semibold leading-6">
+                        Dev Starter Pack{' '}
+                        <span className="inline-flex items-center rounded-full bg-vermillion-100 px-2 py-1 text-xs font-medium text-vermillion-500 ring-1 ring-inset ring-vermillion-500/10">
+                            Activated!
+                        </span>
+                    </h2>
+
+                    <div className="mt-4">
+                        <p className="text-balance text-sm text-gray-500">
+                            Cody has unmatched context and the latest AI models. Supercharge your coding with AI
+                            autocomplete, chat, inline edits, Smart Apply, and more.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-5 sm:mt-6">
+                <div className="mb-4 rounded bg-gray-100 p-2 text-center font-mono text-lg tracking-tighter">
+                    1 free month of Cody Pro!
+                </div>
+            </div>
+
+            <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                <a
+                    href="https://accounts.sourcegraph.com/sign-in?redirect_to=%2Fcody%2Fsubscription%3FshowCouponCodeAtCheckout"
+                    className="inline-flex w-full justify-center rounded-md bg-blurple-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blurple-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blurple-600 sm:col-start-2"
+                >
+                    Redeem free month
+                </a>
+
+                <button
+                    type="button"
+                    onClick={handleClose}
+                    className="text-gray-900 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-200 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                >
+                    No thanks
+                </button>
+            </div>
+        </dialog>
     )
 }
 
