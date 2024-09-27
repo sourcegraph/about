1c81f1d6e89bd531febcb96f87ebf14e8d2bfa7b
Chris Sev <chris.sev@hey.com>
2024-09-27 16:11:40 -0600

    code search section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -194,7 +194,8 @@ const Home: FunctionComponent = () => {
                     </div>
                 </div>
 
-                <div className="mx-auto max-w-7xl">
+                {/* only margin top 20 here because the twocolumntestimonial card has its own padding for some reason */}
+                <div className="mx-auto mt-20 max-w-7xl">
                     <TwoColumnTestimonialCard
                         leftClientImgSrc="/home/reviews1.svg"
                         rightClientImgSrc="/home/reviews2.png"
@@ -208,106 +209,104 @@ const Home: FunctionComponent = () => {
                     />
                 </div>
 
-                <div className="relative overflow-hidden md:overflow-visible">
-                    <div className="z-10 mx-6 mb-14 flex flex-col md:mx-0 md:w-[762px]">
-                        <div className="flex items-center gap-4 pb-6">
+                {/* code search ----------------------------------------------------------------- */}
+                <div className="mt-48">
+                    <div className="mx-auto max-w-7xl">
+                        {/* header */}
+                        <div className="flex items-center justify-center gap-4">
                             <img
-                                className="h-[50px] w-[50px] rounded-t-2xl"
+                                className="h-[65px] w-[65px] rounded-t-2xl"
                                 src="/home/branded-icons/Code-Search-squircle.svg"
                                 alt="Code Search Product logo"
                             />
-                            <h1>Code Search</h1>
+                            <h2 className="text-3xl font-medium sm:text-5xl md:text-7xl">Code Search</h2>
                         </div>
-                        <h3 className="text-gray-500">
+
+                        {/* description */}
+                        <p className="text-balance mx-auto mt-4 max-w-xl text-center text-sm text-gray-500 sm:text-base md:text-lg lg:mt-8">
                             Search your entire codebase—every code host and repository, at any scale—in a single place.
                             Code Search makes it easy for developers to onboard to new codebases, understand code
                             faster, and find & fix security risks.
-                        </h3>
-                    </div>
-                    <div className="flex pb-8">
-                        {isMobile ? (
-                            <>
-                                <img
-                                    className="z-20 ml-6 mr-0 h-[279px] w-[826px]"
-                                    src="/home/code-search-mobile.svg"
-                                    alt="Cody Search"
-                                />
-                                <div className="code-search-gradient absolute right-[-700px] top-[600px] z-10 h-[1245.828px] w-full " />
-                            </>
-                        ) : (
-                            <>
-                                <img className="z-20 h-[549px] w-auto" src="/home/code-search.svg" alt="Cody Search" />
-                                <div className="code-search-gradient absolute left-[340px] top-[351px] z-10 h-[1245.828px] w-[524.518px] " />
-                            </>
-                        )}
-                    </div>
-                </div>
-
-                <div className="gap-19 z-10 mx-6 mb-8 flex justify-between rounded-2xl border-1 border-gray-200 bg-white md:mx-0">
-                    <div className=" flex max-w-[590px] flex-col py-16 pl-10 pr-[70px]">
-                        <img
-                            className="h-[32px] w-[30.316px]"
-                            src="/home/branded-icons/code-search-icon.svg"
-                            alt="Completions Brand Icon"
-                        />
-                        <h2 className="pb-4 pt-6">Find and fix code</h2>
-                        <h5 className="text-gray-500">
-                            Find what you need in milliseconds across all of your code–no matter where it lives.
-                        </h5>
-                    </div>
-                    {isDesktop && <img src="/home/code-graph-home.svg" alt="Multiline Completion" />}
-                </div>
-
-                <div className="mx-6 mb-8 flex flex-col gap-8 md:mx-0 md:flex-row md:gap-6">
-                    <div className="flex w-full flex-col rounded-2xl border-1 border-gray-200 bg-white px-10 py-16">
-                        <img
-                            className="h-[40px] w-[40px]"
-                            src="/home/branded-icons/Code-insights-icon.svg"
-                            alt="Completions Brand Icon"
-                        />
-                        <h2 className="break-words pb-4 pt-6">Track trends in your codebase</h2>
-                        <h5 className="text-gray-500">
-                            Transform your code into a queryable database to create custom dashboards in seconds.
-                        </h5>
-                    </div>
-                    <div className="flex w-full flex-col rounded-2xl border-1 border-gray-200 bg-white px-10 py-16">
-                        <img
-                            className="h-[40px] w-[40px]"
-                            src="/home/branded-icons/batch-changes-icon.svg"
-                            alt="Completions Brand Icon"
-                        />
-                        <h2 className="pb-4 pt-6">Automate large-scale code changes</h2>
-                        <h3 className="text-lg text-gray-500">
-                            Find and make changes across your codebase in one go, whether a version change or a
-                            vulnerability fix.
-                        </h3>
-                    </div>
-                </div>
-                <Link
-                    href="https://sourcegraph.com/code-search"
-                    title="Code Search"
-                    className="btn btn-link btn-link-icon mx-6 mb-28 p-0 px-5 py-3 text-center font-semibold !-tracking-[0.25px] md:mx-0 md:mb-24 md:px-0 md:pb-0 md:pt-0 md:text-left"
-                >
-                    Learn more about Code Search
-                    <ChevronRightIcon className="link-icon" />
-                </Link>
-                <div className="sg-reviews flex flex-col gap-6 rounded-none px-6 py-[73px] text-white md:flex-row md:gap-16 md:rounded-2xl md:px-20 md:py-16">
-                    <div className="flex flex-col gap-4 p-5 md:p-0">
-                        <div className="flex flex-col">
-                            <p className="mb-0 text-base">Aneesh Agrawal</p>
-                            <p className="mb-0 text-sm">Software Engineer, Lyft</p>
-                        </div>
-                        <p className="mb-0 pb-4 text-[35px] font-normal leading-[43.75px] md:pb-0">
-                            Sourcegraph makes it easy to survey and understand existing use cases to make sure we build
-                            the right thing.
                         </p>
+
+                        {/* link */}
+                        <div className="mt-4 text-center">
+                            <Link
+                                href="https://sourcegraph.com/code-search"
+                                title="Code Search"
+                                className="btn btn-link btn-link-icon"
+                            >
+                                Learn more about Code Search
+                                <ChevronRightIcon className="link-icon" />
+                            </Link>
+                        </div>
+
+                        {/* Find and fix code */}
+                        <div className="mt-10 rounded-2xl border border-gray-200 bg-white px-6 py-8 md:px-10">
+                            <img
+                                className="h-10 w-10"
+                                src="/home/branded-icons/code-search-icon.svg"
+                                alt="Code Search Icon"
+                            />
+                            <h3 className="mt-4 text-lg md:text-3xl">Find and fix code</h3>
+                            <p className="mt-2 mb-0 text-gray-500">
+                                Find what you need in milliseconds across all of your code–no matter where it lives.
+                            </p>
+                        </div>
+
+                        {/* Code Search sections */}
+                        <div className="mt-2 grid gap-2 md:grid-cols-2">
+                            {/* Automate large-scale changes */}
+                            <div className="rounded-2xl border border-gray-200 bg-white px-6 py-8 md:px-10">
+                                <img
+                                    className="h-10 w-10"
+                                    src="/home/branded-icons/batch-changes-icon.svg"
+                                    alt="Batch Changes Icon"
+                                />
+                                <h3 className="mt-4 text-lg md:text-3xl">Automate large-scale code changes</h3>
+                                <p className="mt-2 mb-0 text-gray-500">
+                                    Find and make changes across your codebase in one go, whether a version change or a
+                                    vulnerability fix.
+                                </p>
+                            </div>
+
+                            {/* Track trends */}
+                            <div className="rounded-2xl border border-gray-200 bg-white px-6 py-8 md:px-10">
+                                <img
+                                    className="h-10 w-10"
+                                    src="/home/branded-icons/Code-insights-icon.svg"
+                                    alt="Code Insights Icon"
+                                />
+                                <h3 className="mt-4 text-lg md:text-3xl">Track trends in your codebase</h3>
+                                <p className="mt-2 mb-0 text-gray-500">
+                                    Transform your code into a queryable database to create custom dashboards in
+                                    seconds.
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <ReadCaseStudyLink
-                        parentClassName="flex whitespace-nowrap px-10 md:self-end md:px-0"
-                        linkClassName="btn btn-link-dark btn-link-icon p-0"
-                        href="https://sourcegraph.com/case-studies/lyft-monolith-to-microservices"
-                    />
                 </div>
+
+                <div className="mt-48">
+                    <div className="sg-reviews flex flex-col gap-6 rounded-none px-6 py-[73px] text-white md:flex-row md:gap-16 md:rounded-2xl md:px-20 md:py-16">
+                        <div className="flex flex-col gap-4 p-5 md:p-0">
+                            <div className="flex flex-col">
+                                <p className="mb-0 text-base">Aneesh Agrawal</p>
+                                <p className="mb-0 text-sm">Software Engineer, Lyft</p>
+                            </div>
+                            <p className="mb-0 pb-4 text-[35px] font-normal leading-[43.75px] md:pb-0">
+                                Sourcegraph makes it easy to survey and understand existing use cases to make sure we
+                                build the right thing.
+                            </p>
+                        </div>
+                        <ReadCaseStudyLink
+                            parentClassName="flex whitespace-nowrap px-10 md:self-end md:px-0"
+                            linkClassName="btn btn-link-dark btn-link-icon p-0"
+                            href="https://sourcegraph.com/case-studies/lyft-monolith-to-microservices"
+                        />
+                    </div>
+                </div>
+
                 <div className="mx-6 grid grid-cols-1 gap-6 py-16 md:mx-0 md:grid-cols-2 md:py-24">
                     <div className="hover:cta-free-cody relative overflow-hidden rounded-2xl border-1 border-gray-200 bg-white">
                         <div className="cta-top-border absolute left-0 right-0 top-0 rounded-t-2xl" />
