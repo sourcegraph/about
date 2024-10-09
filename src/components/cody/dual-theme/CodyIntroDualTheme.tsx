a996cf3d284e496d1fbef7c7007e28e769102246
Alex Isken <alex.isken@sourcegraph.com>
2024-10-09 17:21:04 -0500

    Update CodyIntroDualTheme.tsx

--- src/components/cody/dual-theme/CodyIntroDualTheme.tsx
+++ src/components/cody/dual-theme/CodyIntroDualTheme.tsx
@@ -79,7 +79,7 @@ export const CodyIntroDualTheme: FunctionComponent<CodyIntroDualThemeProps> = ({
             </button>
             <Link
                 href="/enterprise-trial-offer?form_submission_source=cody-enterprise-trial"
-                title="Get a Cody Enterprise trial"
+                title="Request Cody Enterprise"
                 className={classNames('btn btn-secondary w-full px-6 py-2 lg:w-fit', isVariant && 'md:!w-fit')}
                 type="button"
                 onClick={() => captureCustomEventWithPageData('start_enterprise_trial_click')}
