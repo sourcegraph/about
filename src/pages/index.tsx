f9ae11f0275421b71118b666b53602aa0dec6c4f
Ado Kukic <kukicadnan@gmail.com>
2024-10-20 19:06:17 -0700

    feat: Update homepage CTAs
    * Removed unused `classNames` import
    * Removed the homepage CTAs section as it is no longer needed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent, ReactSVG, useState, useEffect } from 'react'
 
-import classNames from 'classnames'
 import ChevronRightIcon from 'mdi-react/ChevronRightIcon'
 import Link from 'next/link'
 import { posthog } from 'posthog-js'
@@ -353,57 +352,6 @@ const Home: FunctionComponent = () => {
 
                 {/* call to actions ------------------------------------------------------- */}
                 {/* ------------------------------------------------------------------------------- */}
-                <div className="mx-auto mt-10 max-w-6xl">
-                    <div className="mx-6 grid grid-cols-1 gap-6 py-16 md:mx-0 md:grid-cols-2 md:py-24">
-                        <div className="hover:cta-free-cody relative overflow-hidden rounded-2xl border-1 border-gray-200 bg-white">
-                            <div className="cta-top-border absolute left-0 right-0 top-0 rounded-t-2xl" />
-                            <div className=" px-14 py-16">
-                                <h2 className="mb-4 text-gray-700">Get started with Cody</h2>
-                                <p className="mb-0 text-lg text-gray-500">
-                                    Use Cody for free in your IDE, no credit card required.
-                                </p>
-                                <div className="mt-6 flex flex-wrap gap-2">
-                                    <button
-                                        type="button"
-                                        className={classNames('btn btn-primary w-full md:w-auto')}
-                                        title="free cody"
-                                        onClick={() => handleOpenModal('bottom')}
-                                    >
-                                        Download Cody for free
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="cta-home rounded-2xl px-14 py-16 text-white">
-                            <h2 className="mb-[10px]">Cody Enterprise</h2>
-                            <p className="mb-0 text-lg text-[#FFFFFFCC]">
-                                Cody Enterprise provides additional security, scalability, and control for your
-                                organization. Unlimited usage and context search for your entire codebase.
-                            </p>
-                            <div className="mt-8 flex flex-col items-center gap-4 md:flex-row">
-                                <Link
-                                    href="/contact/request-info"
-                                    title="Get Cody for Enterprise"
-                                    className="btn btn-secondary-dark w-full px-6 py-2 text-center md:w-auto"
-                                    onClick={() =>
-                                        captureCustomEventWithPageData('contact_sales_onpage_click', 'bottom')
-                                    }
-                                >
-                                    Book a demo
-                                </Link>
-                                <Link
-                                    href="/pricing"
-                                    title="See pricing"
-                                    className="btn btn-link-dark md:btn-link-icon w-full rounded-[5px] px-6 text-center md:w-auto"
-                                >
-                                    See pricing
-                                    <ChevronRightIcon className="link-icon hidden md:block" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </Layout>
     )
