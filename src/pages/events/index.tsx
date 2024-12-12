18e32fb299e04406754d63ed5c41fdbd62506830
Justin Dorfman <justindorfman@gmail.com>
2024-12-12 12:56:39 -0800

    Remove event details section

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -26,39 +26,6 @@ const EventsPage: FunctionComponent = () => (
         }
         className="bg-gray-50"
     >
-        <ContentSection>
-            <div className="mt- flex flex-col gap-10 rounded-lg border border-gray-200 bg-white px-10 py-12">
-                {/* left side */}
-                <div>
-                    <h3 className="text-xl">
-                        December 12th <span className="rounded-lg bg-violet-400 px-2 py-1 text-xs text-white">Upcoming</span>
-                    </h3>
-
-                    <div className="grid gap-y-5 gap-x-10 xl:grid-cols-2">
-                        <div>
-                            <h2 className="mt-4">Virtual Code AI Summit</h2>
-
-                            <p className="mt-4 text-lg text-gray-600">
-                            Featuring speakers from Google, Anthropic, Booking.com, Palo Alto Networks, Sourcegraph and more
-                            </p>
-                            
-                        </div>  
-                    </div>
-                </div>
-                {/* right side */}
-                <div>
-                    <a
-                        href="https://sourcegraph.registration.goldcast.io/events/b650937d-ba9f-40ce-9429-35c3539a5bb1?utm_medium=website&utm_source=website"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="inline-block rounded-lg bg-violet-600 px-4 py-2.5 text-sm font-semibold text-white hover:bg-violet-500"
-                    >
-                        Learn more
-                    </a>
-                </div>
-            </div>
-        </ContentSection>
-
         <EventsList />
     </Layout>
 )
